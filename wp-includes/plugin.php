623b85a514dda093c7c358bf36aa0d4ccb40ed72
KITE <ixkaito@gmail.com>
2014-08-25 20:13:45 +0900

    Add add_filter(), _wp_filter_build_unique_id()

--- wp-includes/plugin.php
+++ wp-includes/plugin.php
@@ -35,6 +35,60 @@ if ( ! isset( $wp_current_filter ) )
 	$wp_current_filter = array();
 
 /**
+ * Hook a function or method to a specific filter action.
+ *
+ * WordPress offers filter hooks to allow plugins to modify
+ * various types of internal data at runtime.
+ *
+ * A plugin can modify data by binding a callback to a filter hook. When the filter
+ * is later applied, each bound callback is run in order of priority, and given
+ * the opportunity to modify a value by returning a new value.
+ *
+ * The following example shows how a callback function is bound to a filter hook.
+ * Note that $example is passed to the callback, (maybe) modified, then returned:
+ *
+ * <code>
+ * function example_callback( $example ) {
+ * 	// Maybe modify $example in some way
+ * 	return $example;
+ * }
+ * add_filter( 'example_filter', 'example_callback' );
+ * </code>
+ *
+ * Since WordPress 1.5.1, bound callback can take as many arguments as are
+ * passed as parameters in the corresponding apply_filters() call. The $accepted_args
+ * parameter allows for calling functions only when the number of args match.
+ *
+ * <strong>Note:</strong> the function will return true whether or not the callback
+ * is valid. It is up to you to take care. This is done for optimization purpose,
+ * so everything is as quick as possible.
+ *
+ * @since 0.71
+ *
+ * @global array $wp_filter      A multidimensional array of all hooks and the callbacks hooked to them.
+ * @global array $merged_filters Tracks the tags that need to be merged for later. If the hook is added,
+ *                               it doesn't need to run through that process.
+ *
+ * @param string   $tag             The name of the filter to hook the $function_to_add callback to.
+ * @param callback $function_to_add The callback to be run when the filter is applied.
+ * @param int      $priority        Optional. Used to specify the order in which the functions
+ *                                  associated with a particular action are executed. Default 10.
+ *                                  Lower numbers correspond with earlier execution,
+ *                                  and functions with the same priority are executed
+ *                                  in the order in which they were added to the action.
+ * @param int      $accepted_args   Optional. The number of arguments the function accepts. Default 1.
+ * @return boolean true
+ */
+function add_filter( $tag, $function_to_add, $priority = 10, $accepted_args = 1 ) {
+	global $wp_filter, $merged_filters;
+
+	$idx = _wp_filter_build_unique_id($tag, $function_to_add, $priority);
+	$wp_filter[$tag][$priority][$idx] = array('function' => $function_to_add, 'accepted_args' => $accepted_args);
+	unset( $merged_filters[ $tag ] );
+	return true;
+}
+
+/**
  * Call the functions added to a filter hook.
  *
  * The callback functions attached to filter hook $tag are invoked by calling
@@ -216,4 +270,75 @@ function _wp_call_all_hook($args) {
 				call_user_func_array($the_['function'], $args);
 
 	} while ( next($wp_filter['all']) !== false );
+}
+
+/**
+ * Build Unique ID for storage and retrieval.
+ *
+ * The old way to serialize the callback caused issues and this function is the
+ * solution. It works by checking for objects and creating an a new property in
+ * the class to keep track of the object and new objects of the same class that
+ * need to be added.
+ *
+ * It alse allows for the removal of actions and filters for objects after they
+ * change class properties. It is possible to include the property $wp_filter_id
+ * in your class and set it to "null" or a number to bypass the workaround.
+ * However this will prevent you from adding new classes and any new classes
+ * will overwrite the previous hook by the same class.
+ *
+ * Functions and static method callbacks are just returned as strings and
+ * shouldn't have any speed penalty.
+ *
+ * @link http://trac.wordpress.org/ticket/3875
+ *
+ * @since 2.2.3
+ * @access private
+ *
+ * @global array $wp_filter Storage for all of the filters and actions.
+ *
+ * @param string   $tag      Used in counting how many hooks were applied
+ * @param callback $function Used for creating unique id
+ * @param int|bool $priority Used in counting how many hooks were applied. If === false
+ *                           and $function is an object reference, we return the unique
+ *                           id only if it already has one, false otherwise.
+ * @return string|bool Unique ID for usage as array key or false if $priority === false
+ *                     and $function is an object reference, and it does not already have
+ *                     a unique id.
+ */
+function _wp_filter_build_unique_id($tag, $funtion, $priority) {
+	global $wp_filter;
+	static $filter_id_count = 0;
+
+	if ( is_string($function) )
+		return $function;
+
+	if ( is_object($function) ) {
+		// Closures are currently implemented as objects
+		$function = array( $function, '' );
+	} else {
+		$function = (array) $function;
+	}
+
+	if (is_object($function[0]) ) {
+		// Object Class Calling
+		if ( function_exists('spl_object_hash') ) {
+			return spl_object_hash($function[0]) . $function[1];
+		} else {
+			$obj_idx = get_class($function[0]).$function[1];
+			if ( !isset($function[0]->wp_filter_id) ) {
+				if ( false === $priority )
+					return false;
+				$obj_idx .= isset($wp_filter[$tag][$priority]) ? count((array)$wp_filter[$tag][$priority]) : $filter_id_count;
+				$function[0]->wp_filter_id = $filter_id_count;
+				++$filter_id_count;
+			} else {
+				$obj_idx .= $function[0]->wp_filter_id;
+			}
+
+			return $obj_idx;
+		}
+	} else if ( is_string($function[0]) ) {
+		// static Calling
+		return $function[0] . '::' . $function[1];
+	}
 }
