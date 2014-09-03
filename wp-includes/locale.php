89f07195140189ef73c2b4b12113e701b390e136
KITE <ixkaito@gmail.com>
2014-09-04 08:32:04 +0900

    Add WP_Locale class

--- wp-includes/locale.php
+++ wp-includes/locale.php
@@ -5,3 +5,33 @@
  * @package WordPress
  * @subpackage i18n
  */
+
+/**
+ * Class that loads the calendar locale.
+ *
+ * @since 2.1.0
+ */
+class WP_Locale {
+	/**
+	 * Stores the translated strings for the full weekday names.
+	 *
+	 * @since 2.1.0
+	 * @var array
+	 * @access private
+	 */
+	var $weekday;
+
+	/**
+	 * Stores the translated strings for the one character weekday names.
+	 *
+	 * There is a hack to make sure that Tuesday and Thursday, as well
+	 * as Sunday and Saturday, don't conflict. See init() method for more.
+	 *
+	 * @see WP_Locale::init() for how to handle the hack.
+	 *
+	 * @since 2.1.0
+	 * @var array
+	 * @access private
+	 */
+	var $weekday_initial;
+}
