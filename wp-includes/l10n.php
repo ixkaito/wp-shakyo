203c21e373d369e68eae98b92553ef339f9054f5
KITE <ixkaito@gmail.com>
2014-09-10 22:02:33 +0900

    Add __()

--- wp-includes/l10n.php
+++ wp-includes/l10n.php
@@ -7,6 +7,20 @@
  */
 
 /**
+ * Retrieve the translation of $text. If there is no translation,
+ * or the text domain isn't loaded, the original text is returned.
+ *
+ * @since 2.1.0
+ *
+ * @param string $text   Text to translate.
+ * @param string $domain Optional. Text domain. Unique identifier for retrieving translated strings.
+ * @return string Translated text.
+ */
+function __( $text, $domain = 'default' ) {
+	return translate( $text, $domain );
+}
+
+/**
  * Load a .mo file into the text domain $domain.
  *
  * If the text domain already exists, the translations will be merged. If both
