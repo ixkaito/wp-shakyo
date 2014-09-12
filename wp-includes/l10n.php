cbdfd8c34eac47508bdfb27fa75e548d314cea03
KITE <ixkaito@gmail.com>
2014-09-12 15:52:57 +0900

    Add get_translations_for_domain(), _x(), class NOOP_Translations

--- wp-includes/l10n.php
+++ wp-includes/l10n.php
@@ -49,6 +49,26 @@ function __( $text, $domain = 'default' ) {
 }
 
 /**
+ * Retrieve translated string with gettext context.
+ *
+ * Quite a few times, there will be collisions with similar translatable text
+ * found in more than two places, but with different translated context.
+ *
+ * By including the context in the pot file, translators can translate the two
+ * strings differently.
+ *
+ * @since 2.8.0
+ *
+ * @param string $text    Text to translate.
+ * @param string $context Context information for the translators.
+ * @param string $domain  Optional. Text domain. Unique identifier for retrieving translated strings.
+ * @return string Translated context string without pipe.
+ */
+function _x( $text, $context, $domain = 'default' ) {
+	return translate_with_gettext_context( $text, $context, $domain );
+}
+
+/**
  * Load a .mo file into the text domain $domain.
  *
  * If the text domain already exists, the translations will be merged. If both
@@ -112,4 +132,22 @@ function load_textdomain( $domain, $mofile ) {
 	$l10n[$domain] = &$mo;
 
 	return true;
+}
+
+/**
+ * Return the Translations instance for a text domain.
+ *
+ * If there isn't one, return empty Translations instance.
+ *
+ * @since 2.8.0
+ *
+ * @param string $domain Text domain. Unique identifier for retrieving translated strings.
+ * @return Translations A Translations instance.
+ */
+function get_translations_for_domain( $domain ) {
+	global $l10n;
+	if ( !isset( $l10n[$domain] ) ) {
+		$l10n[$domain] = new NOOP_Translations;
+	}
+	return $l10n[$domain];
 }
--- wp-includes/pomo/translations.php
+++ wp-includes/pomo/translations.php
@@ -215,49 +215,49 @@ class Gettext_Translations extends Translations {
 endif;
 
 
-// if ( !class_exists( 'NOOP_Translations' ) ):
-// /**
-//  * Provides the same interface as Translations, but doesn't do anything
-//  */
-// class NOOP_Translations {
-// 	var $entries = array();
-// 	var $headers = array();
+if ( !class_exists( 'NOOP_Translations' ) ):
+/**
+ * Provides the same interface as Translations, but doesn't do anything
+ */
+class NOOP_Translations {
+	var $entries = array();
+	var $headers = array();
 
-// 	function add_entry($entry) {
-// 		return true;
-// 	}
+	function add_entry($entry) {
+		return true;
+	}
 
-// 	function set_header($header, $value) {
-// 	}
+	function set_header($header, $value) {
+	}
 
-// 	function set_headers($headers) {
-// 	}
+	function set_headers($headers) {
+	}
 
-// 	function get_header($header) {
-// 		return false;
-// 	}
+	function get_header($header) {
+		return false;
+	}
 
-// 	function translate_entry(&$entry) {
-// 		return false;
-// 	}
+	function translate_entry(&$entry) {
+		return false;
+	}
 
-// 	function translate($singular, $context=null) {
-// 		return $singular;
-// 	}
+	function translate($singular, $context=null) {
+		return $singular;
+	}
 
-// 	function select_plural_form($count) {
-// 		return 1 == $count? 0 : 1;
-// 	}
+	function select_plural_form($count) {
+		return 1 == $count? 0 : 1;
+	}
 
-// 	function get_plural_forms_count() {
-// 		return 2;
-// 	}
+	function get_plural_forms_count() {
+		return 2;
+	}
 
-// 	function translate_plural($singular, $plural, $count, $context = null) {
-// 			return 1 == $count? $singular : $plural;
-// 	}
+	function translate_plural($singular, $plural, $count, $context = null) {
+			return 1 == $count? $singular : $plural;
+	}
 
-// 	function merge_with(&$other) {
-// 	}
-// }
-// endif;
+	function merge_with(&$other) {
+	}
+}
+endif;
