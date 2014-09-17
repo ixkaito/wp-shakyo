eafa7a324c579941c339b9c58863da18e002f5af
KITE <ixkaito@gmail.com>
2014-09-17 15:02:35 +0900

    Add wp_parse_args(), did_action()

--- wp-includes/plugin.php
+++ wp-includes/plugin.php
@@ -243,6 +243,25 @@ function do_action( $tag, $arg = '' ) {
 }
 
 /**
+ * Retrieve the number of times an action is fired.
+ *
+ * @since 2.1.0
+ *
+ * @global array $wp_actions Increments the amount of times action was triggered.
+ *
+ * @param string $tag The name of the action hook.
+ * @return int The number of times action hook $tag is fired.
+ */
+function did_action($arg) {
+	global $wp_actions;
+
+	if ( ! isset( $wp_actions[ $arg ] ) )
+		return 0;
+
+	return $wp_actions[$arg];
+}
+
+/**
  * Call the 'all' hook, which will process the functions hooked into it.
  *
  * The 'all' hook passes all of the arguments or parameters that were used for
