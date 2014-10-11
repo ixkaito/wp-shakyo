e142d2303bf8fd725ef98339b729795198a8035f
KITE <ixkaito@gmail.com>
2014-10-12 03:01:04 +0900

    Add some filters. Now line 222 of default-filters.php

--- wp-includes/default-filters.php
+++ wp-includes/default-filters.php
@@ -214,3 +214,7 @@ add_action( 'template_redirect',   'wp_shortlink_header',             11, 0 );
 add_action( 'wp_print_footer_scripts', '_wp_footer_scripts'                 );
 add_action( 'init',                'check_theme_switched',            99    );
 add_action( 'after_switch_theme',  '_wp_sidebars_changed'                   );
+
+if ( isset( $_GET['replytocom'] ) )
+    add_action( 'wp_head', 'wp_no_robots' );
+
