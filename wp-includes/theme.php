20c101e79a965e74e8cb077576bb8b7c17281d34
KITE <ixkaito@gmail.com>
2014-10-27 04:42:59 +0900

    Add

--- wp-includes/theme.php
+++ wp-includes/theme.php
@@ -6,6 +6,8 @@
  * @subpackage Theme
  */
 
+add_action('setup_theme', 'preview_theme');
+
 add_action( 'wp_loaded', '_custom_header_background_just_in_time' );
 
 add_action( 'delete_attachment', '_delete_attachment_theme_mod' );
