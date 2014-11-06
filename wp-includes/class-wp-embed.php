920157557a33685f3f95dd1b2362fca117164e28
KITE <ixkaito@gmail.com>
2014-11-06 22:33:48 +0900

    Add register_handler(), unregister_handler() in class WP_Embed

--- wp-includes/class-wp-embed.php
+++ wp-includes/class-wp-embed.php
@@ -68,6 +68,53 @@ class WP_Embed {
 		return $content;
 	}
 
+	/**
+	 * If a post/page was saved, then output JavaScript to make
+	 * an AJAX request that will call WP_Embed::cache_oembed().
+	 */
+	public function maybe_run_ajax_cache() {
+		$post = get_post();
+
+		if ( ! $post || empty( $_GET['message'] ) )
+			return;
+
+?>
+<script type="text/javascript">
+/* <![CDATA[ */
+	jQuery(document).ready(function($){
+		$.get("<?php echo admin_url( 'admin-ajax.php?action=oembed-cache&post=' . $post->ID, 'relative' ); ?>");
+	});
+/* ]]> */
+</script>
+<?php
+	}
+
+	/**
+	 * Register an embed handler. Do not use this function directly, use {@link wp_embed_register_handler()} instead.
+	 * This function should probably also only be used for sites that do not support oEmbed.
+	 *
+	 * @param string $id An internal ID/name for the handler. Needs to be unique.
+	 * @param string $regex The regex that will be used to see if this handler should be used for a URL.
+	 * @param callback $callback The callback function that will be called if the regex is matched.
+	 * @param int $priority Optional. Used to specify the order in which the registered handlers will be tested (default: 10). Lower numbers correspond with earlier testing, and handlers with the same priority are tested in the order in which they were added to the action.
+	 */
+	public function register_handler( $id, $regex, $callback, $priority = 10 ) {
+		$this->handlers[$priority][$id] = array(
+			'regex'    => $regex,
+			'callback' => $callback,
+		);
+	}
+
+	/**
+	 * Unregister a previously registered embed handler. Do not use this function directly, use {@link wp_embed_unregister_handler()} instead.
+	 *
+	 * @param string $id The handler ID that should be removed.
+	 * @param int $priority Optional. The priority of the handler to be removed (default: 10).
+	 */
+	public function unregister_handler( $id, $priority = 10 ) {
+		if ( isset($this->handlers[$priority][$id]) )
+			unset($this->handlers[$priority][$id]);
+	}
 }
 
 $GLOBALS['wp_embed'] = new WP_Embed();
