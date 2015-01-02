c50c9c642e0ea5abb110ae96314066842f72a7c2
KITE <ixkaito@gmail.com>
2015-01-03 07:49:21 +0900

    Add wp-includes/vars.php

---
+++ wp-includes/vars.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Creates common globals for the rest of WordPress
+ *
+ * Sets $pagenow global which is the current page. Checks
+ * for the browser to set which one is currently being used.
+ *
+ * Detects which user environment WordPress is being used on.
+ * Only attempts to check for Apache, Nginx and IIS -- three web
+ * servers with known pretty permalink capability.
+ *
+ * Note: Though Nginx is detected, WordPress does not currently
+ * generate rewrite rules for it. See http://codex.wordpress.org/Nginx
+ *
+ * @package WordPress
+ */
