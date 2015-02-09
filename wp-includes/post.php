b2b358830e79ff879b1c114c5e02ce3495cd88bb
KITE <ixkaito@gmail.com>
2015-02-09 17:38:27 +0900

    Add `add_post_type_support()`

--- wp-includes/post.php
+++ wp-includes/post.php
@@ -607,5 +607,35 @@ function _get_custom_object_labels( $object, $nohier_vs_hier_defaults ) {
 
 add_action( 'admin_menu', '_add_post_type_submenus' );
 
+/**
+ * Register support of certain features for a post type.
+ *
+ * All core features are directly associated with a functional area of the edit
+ * screen, such as the editor or a meta box. Features include: 'title', 'editor',
+ * 'comments', 'revisions', 'trackbacks', 'author', 'excerpt', 'page-attributes',
+ * 'thumbnail', 'custom-fields', and 'post-formats'.
+ *
+ * Additionally, the 'revisions' feature dictates whether the post type will
+ * store revisions, and the 'comments' feature dictates whether the comments
+ * count will show on the edit screen.
+ *
+ * @since 3.0.0
+ *
+ * @param string       $post_type The post type for which to add the feature.
+ * @param string|array $feature   The feature being added, accepts an array of
+ *                                feature strings or a single string.
+ */
+function add_post_type_support( $post_type, $feature ) {
+	global $_wp_post_type_features;
+
+	$features = (array) $feature;
+	foreach ($features as $feature) {
+		if ( func_num_args() == 2 )
+			$_wp_post_type_features[$post_type][$feature] = true;
+		else
+			$_wp_post_type_features[$post_type][$feature] = array_slice( func_get_args(), 2 );
+	}
+}
+
 add_action( 'before_delete_post', '_reset_front_page_settings_for_post' );
 add_action( 'wp_trash_post',      '_reset_front_page_settings_for_post' );
