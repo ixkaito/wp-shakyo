3f3476ec0c349ba6bf7e6d5c788f4ada4c943870
KITE <ixkaito@gmail.com>
2015-04-19 04:14:59 +0900

    Add some variables to `WP_Query` class

--- wp-includes/query.php
+++ wp-includes/query.php
@@ -24,4 +24,49 @@
  */
 class WP_Query {
 
+	/**
+	 * Query vars set by the user
+	 *
+	 * @since 1.5.0
+	 * @access public
+	 * @var array
+	 */
+	public $query;
+
+	/**
+	 * Query vars, after parsing
+	 *
+	 * @since 1.5.0
+	 * @access public
+	 * @var array
+	 */
+	public $query_vars = array();
+
+	/**
+	 * Taxonomy query, a passed to get_tax_sql()
+	 *
+	 * @since 3.1.0
+	 * @access public
+	 * @var object WP_Tax_Query
+	 */
+	public $tax_query;
+
+	/**
+	 * Metadata query container
+	 *
+	 * @since 3.2.0
+	 * @access public
+	 * @var object WP_Meta_Query
+	 */
+	public $meta_query = false;
+
+	/**
+	 * Date query container
+	 *
+	 * @since 3.7.0
+	 * @access public
+	 * @var object WP_Date_Query
+	 */
+	public $date_query = false;
+
 }
