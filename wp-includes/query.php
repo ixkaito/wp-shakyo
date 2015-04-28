02cd1a63939b1a4a332c6059ef2a0dfadba90fe5
KITE <ixkaito@gmail.com>
2015-04-29 04:12:00 +0900

    Add `fill_query_vars()` in `WP_Query` class

--- wp-includes/query.php
+++ wp-includes/query.php
@@ -563,4 +563,69 @@ class WP_Query {
 	public function parse_query_vars() {
 		$this->parse_query();
 	}
+
+	/**
+	 * Fills in the query variables, which do not exist within the parameter.
+	 *
+	 * @since 2.1.0
+	 * @access public
+	 *
+	 * @param array $array Defined query variables.
+	 * @return array Complete query variables with undefined ones filled in empty.
+	 */
+	public function fill_query_vars($array) {
+		$keys = array(
+			'error'
+			, 'm'
+			, 'p'
+			, 'post_parent'
+			, 'subpost'
+			, 'subpost_id'
+			, 'attachment'
+			, 'attachment_id'
+			, 'name'
+			, 'static'
+			, 'pagename'
+			, 'page_id'
+			, 'second'
+			, 'minute'
+			, 'hour'
+			, 'day'
+			, 'monthnum'
+			, 'year'
+			, 'w'
+			, 'category_name'
+			, 'tag'
+			, 'cat'
+			, 'tag_id'
+			, 'author'
+			, 'author_name'
+			, 'feed'
+			, 'tb'
+			, 'paged'
+			, 'comments_popup'
+			, 'meta_key'
+			, 'meta_value'
+			, 'preview'
+			, 's'
+			, 'sentence'
+			, 'fields'
+			, 'menu_order'
+		);
+
+		foreach ( $keys as $key ) {
+			if ( !isset($array[$key]) )
+				$array[$key] = '';
+		}
+
+		$array_keys = array( 'category__in', 'category__not_in', 'category__and', 'post__in', 'post__not_in',
+			'tag__in', 'tag__not_in', 'tag__and', 'tag_slug__in', 'tag_slug__and', 'post_parent__in', 'post_parent__not_in',
+			'author__in', 'author__not_in' );
+
+		foreach ( $array_keys as $key ) {
+			if ( !isset($array[$key]) )
+				$array[$key] = array();
+		}
+		return $array;
+	}
 }
