3efaeec4e3db44beb534b6be491fac061d1693b5
Kite <ixkaito@gmail.com>
2015-06-22 01:34:56 +0900

    Fix typo

--- wp-includes/functions.php
+++ wp-includes/functions.php
@@ -1348,7 +1348,7 @@ function is_main_site( $site_id = null ) {
  *
  * @since 3.7.0
  *
- * @param int $network_id Optional. Network ID to test. Default to current network.
+ * @param int $network_id Optional. Network ID to test. Defaults to current network.
  * @return bool True if $network_id is the main network, or if not running Multisite.
  */
 function is_main_network( $network_id = null ) {
