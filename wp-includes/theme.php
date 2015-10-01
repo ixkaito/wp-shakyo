2ec4148f6285bbec4772485913dd3d3a36633200
Kite <ixkaito@gmail.com>
2015-10-02 01:50:33 +0900

    Add `preview_theme()`

--- wp-includes/theme.php
+++ wp-includes/theme.php
@@ -70,6 +70,44 @@ function get_theme_root( $stylesheet_or_template = false ) {
 	return apply_filters( 'theme_root', $theme_root );
 }
 
+/**
+ * Start preview theme output buffer.
+ *
+ * Will only perform task if the user has permissions and template and preview
+ * query variables exist.
+ *
+ * @since 2.6.0
+ */
+function preview_theme() {
+	if ( ! (isset($_GET['template']) && isset($_GET['preview'])) )
+		return;
+
+	if ( !current_user_can( 'switch_themes' ) )
+		return;
+
+	// Admin Thickbox requests
+	if ( isset( $_GET['preview_iframe'] ) )
+		show_admin_bar( false );
+
+	$_GET['template'] = preg_replace('|[^a-z0-9_./-]|i', '', $_GET['template']);
+
+	if ( validate_file($_GET['template']) )
+		return;
+
+	add_filter( 'template', '_preview_theme_template_filter' );
+
+	if ( isset($_GET['stylesheet']) ) {
+		$_GET['stylesheet'] = preg_replace('|[^a-z0-9_./-]|i', '', $_GET['stylesheet']);
+		if ( validate_file($_GET['stylesheet']) )
+			return;
+		add_filter( 'stylesheet', '_preview_theme_stylesheet_filter' );
+	}
+
+	// Prevent theme mods to current theme being used on theme being previewed
+	add_filter( 'pre_option_theme_mods_' . get_option( 'stylesheet' ), '__return_empty_array' );
+
+	ob_start( 'preview_theme_ob_filter' );
+}
 add_action('setup_theme', 'preview_theme');
 
 add_action( 'wp_loaded', '_custom_header_background_just_in_time' );
