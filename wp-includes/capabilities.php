1529b0f214701ac0733574ec2e9de6b60d1d45ae
Kite <ixkaito@gmail.com>
2015-10-02 01:38:35 +0900

    Declare variables in WP_Roles class

--- wp-includes/capabilities.php
+++ wp-includes/capabilities.php
@@ -26,4 +26,40 @@
  * @package WordPress
  * @subpackage User
  */
-class WP_Roles {}
+class WP_Roles {
+	/**
+	 * List of roles and capabilities.
+	 *
+	 * @since 2.0.0
+	 * @access public
+	 * @var array
+	 */
+	public $roles;
+
+	/**
+	 * List of the role objects.
+	 *
+	 * @since 2.0.0
+	 * @access public
+	 * @var array
+	 */
+	public $role_objects = array();
+
+	/**
+	 * List of role names.
+	 *
+	 * @since 2.0.0
+	 * @access public
+	 * @var string
+	 */
+	public $role_key;
+
+	/**
+	 * Whether to use the database for retrieval and storage.
+	 *
+	 * @since 2.1.0
+	 * @access public
+	 * @var bool
+	 */
+	public $use_db = true;
+}
