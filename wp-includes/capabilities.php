1cf776c11e26c2b95f175d8131c6cea21cd6ddc7
Kite <ixkaito@gmail.com>
2015-10-02 01:35:46 +0900

    Add WP_Roles class

--- wp-includes/capabilities.php
+++ wp-includes/capabilities.php
@@ -5,3 +5,25 @@
  * @package WordPress
  * @subpackage User
  */
+
+/**
+ * WordPress User Roles.
+ *
+ * The role option is simple, the structure is organized by role name that store
+ * the name in value of the 'name' key. The capabilities are stored as an array
+ * in the value of the 'capability' key.
+ *
+ * <code>
+ * array (
+ * 		'rolename' => array (
+ *			'name' => 'rolename',
+ *			'capabilities' => array()
+ *		)
+ * )
+ * </code>
+ *
+ * @since 2.0.0
+ * @package WordPress
+ * @subpackage User
+ */
+class WP_Roles {}
