ae1d50e3a076287a8f3656821c20cfd270107e43
Kite <ixkaito@gmail.com>
2015-10-07 19:59:03 +0900

    Add `wp_get_current_user()`

--- wp-includes/pluggable.php
+++ wp-includes/pluggable.php
@@ -5,3 +5,20 @@
  *
  * @package WordPress
  */
+
+if ( !function_exists('wp_get_current_user') ) :
+/**
+ * Retrieve the current user object.
+ *
+ * @since 2.0.3
+ *
+ * @return WP_User Current user WP_User object
+ */
+function wp_get_current_user() {
+	global $current_user;
+
+	get_currentuserinfo();
+
+	return $current_user;
+}
+endif;
