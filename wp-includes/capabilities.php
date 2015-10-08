d1f86c95c4984fa038cb2c5f6e0621fc4cbb6622
Kite <ixkaito@gmail.com>
2015-10-09 08:05:21 +0900

    Add constructor of WP_User class

--- wp-includes/capabilities.php
+++ wp-includes/capabilities.php
@@ -178,4 +178,52 @@ class WP_User {
 	var $filter = null;
 
 	private static $back_compat_keys;
+
+	/**
+	 * Constructor
+	 *
+	 * Retrieves the userdata and passes it to {@link WP_User::init()}.
+	 *
+	 * @since 2.0.0
+	 * @access public
+	 *
+	 * @param int|string|stdClass|WP_User $id User's ID, a WP_User object, or a user object from the DB.
+	 * @param string $name Optional. User's username
+	 * @param int $blog_id Optional Blog ID, defaults to current blog.
+	 * @return WP_User
+	 */
+	public function __construct( $id = 0, $name = '', $blog_id = '' ) {
+		if ( ! isset( self::$back_compat_keys ) ) {
+			$prefix = $GLOBALS['wpdb']->prefix;
+			self::$back_compat_keys = array(
+				'user_firstname' => 'first_name',
+				'user_lastname' => 'last_name',
+				'user_description' => 'description',
+				'user_level' => $prefix . 'user_level',
+				$prefix . 'usersettings' => $prefix . 'user-settings',
+				$prefix . 'usersettingstime' => $prefix . 'user-settings-time',
+			);
+		}
+
+		if ( is_a( $id, 'WP_User' ) ) {
+			$this->init( $id->data, $blog_id );
+			return;
+		} elseif ( is_object( $id ) ) {
+			$this->init( $id, $blog_id );
+			return;
+		}
+
+		if ( ! empty( $id ) && ! is_numeric( $id ) ) {
+			$name = $id;
+			$id = 0;
+		}
+
+		if ( $id )
+			$data = self::get_data_by( 'id', $id );
+		else
+			$data = self::get_data_by( 'login', $name );
+
+		if ( $data )
+			$this->init( $data, $blog_id );
+	}
 }
