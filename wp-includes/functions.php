b5eefbcb8423029f617a5ea8a6dd52c4f9a2c0d7
Kite <ixkaito@gmail.com>
2015-10-12 21:08:21 +0900

    Add `reset_mbstring_encoding()`

--- wp-includes/functions.php
+++ wp-includes/functions.php
@@ -809,3 +809,14 @@ function mbstring_binary_safe_encoding( $reset = false ) {
 		mb_internal_encoding( $encoding );
 	}
 }
+
+/**
+ * Reset the mbstring internal encoding to a users previously set encoding.
+ *
+ * @see mbstring_binary_safe_encoding()
+ *
+ * @since 3.7.0
+ */
+function reset_mbstring_encoding() {
+	mbstring_binary_safe_encoding( true );
+}
