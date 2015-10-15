fc4d1a0ed126487c927569ade672c9862e9aadfd
Kite <ixkaito@gmail.com>
2015-10-16 08:11:26 +0900

    Add `_get_cron_array()`

--- wp-includes/cron.php
+++ wp-includes/cron.php
@@ -25,3 +25,28 @@ function wp_next_scheduled( $hook, $args = array() ) {
 	}
 	return false;
 }
+
+//
+// Private functions
+//
+
+/**
+ * Retrieve cron info array option.
+ *
+ * @since 2.1.0
+ * @access private
+ *
+ * @return array CRON info array.
+ */
+function _get_cron_array()  {
+	$cron = get_option('cron');
+	if ( ! is_array($cron) )
+		return false;
+
+	if ( !isset($cron['version']) )
+		$cron = _upgrade_cron_array($cron);
+
+	unset($cron['version']);
+
+	return $cron;
+}
