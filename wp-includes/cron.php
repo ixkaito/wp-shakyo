c4187e456614eff2a4c198ade73e7a25a54d17e4
Kite <ixkaito@gmail.com>
2015-10-16 08:09:23 +0900

    Add `wp_next_scheduled()`

--- wp-includes/cron.php
+++ wp-includes/cron.php
@@ -4,3 +4,24 @@
  *
  * @package WordPress
  */
+
+/**
+ * Retrieve the next timestamp for a cron event.
+ *
+ * @since 2.1.0
+ *
+ * @param string $hook Action hook to execute when cron is run.
+ * @param array $args Optional. Arguments to pass to the hook's callback function.
+ * @return bool|int The UNIX timestamp of the next time the scheduled event will occur.
+ */
+function wp_next_scheduled( $hook, $args = array() ) {
+	$crons = _get_cron_array();
+	$key = md5(serialize($args));
+	if ( empty($crons) )
+		return false;
+	foreach ( $crons as $timestamp => $cron ) {
+		if ( isset( $cron[$hook][$key] ) )
+			return $timestamp;
+	}
+	return false;
+}
