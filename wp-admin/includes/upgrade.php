469a265e4d0b61e78ae4e607d727d6ecb627854e
Kite <ixkaito@gmail.com>
2015-10-22 17:53:56 +0900

    Include user install customize script.

---
+++ wp-admin/includes/upgrade.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * WordPress Upgrade API
+ *
+ * Most of the functions are pluggable and can be overwritten
+ *
+ * @package WordPress
+ * @subpackage Administration
+ */
+
+/** Include user install customize script. */
+if ( file_exists(WP_CONTENT_DIR . '/install.php') )
+	require (WP_CONTENT_DIR . '/install.php');
