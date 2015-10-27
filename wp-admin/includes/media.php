a3edfeca55e332b5183e0b7bd9dcceb6b2c04742
Kite <ixkaito@gmail.com>
2015-10-28 04:50:28 +0900

    Add more filters and action to wp-admin/includes/media.php

--- wp-admin/includes/media.php
+++ wp-admin/includes/media.php
@@ -15,3 +15,22 @@ add_action( 'media_buttons', 'media_buttons' );
 add_filter( 'attachment_fields_to_save', 'image_attachment_fields_to_save', 10, 2 );
 
 add_filter('media_send_to_editor', 'image_media_send_to_editor', 10, 3);
+
+add_action('post-plupload-upload-ui', 'media_upload_flash_bypass');
+
+add_action('post-html-upload-ui', 'media_upload_html_bypass');
+
+add_filter( 'async_upload_image', 'get_media_item', 10, 2 );
+add_filter( 'async_upload_audio', 'get_media_item', 10, 2 );
+add_filter( 'async_upload_video', 'get_media_item', 10, 2 );
+add_filter( 'async_upload_file',  'get_media_item', 10, 2 );
+
+add_action( 'media_upload_image', 'wp_media_upload_handler' );
+add_action( 'media_upload_audio', 'wp_media_upload_handler' );
+add_action( 'media_upload_video', 'wp_media_upload_handler' );
+add_action( 'media_upload_file',  'wp_media_upload_handler' );
+
+add_filter( 'media_upload_gallery', 'media_upload_gallery' );
+add_filter( 'media_upload_library', 'media_upload_library' );
+
+add_action( 'attachment_submitbox_misc_actions', 'attachment_submitbox_metadata' );
