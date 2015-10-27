83e2621c8b14c6d10e42f57d4c283ec32c91d13d
Kite <ixkaito@gmail.com>
2015-10-28 04:33:01 +0900

    Add some filters to wp-admin/includes/media.php

--- wp-admin/includes/media.php
+++ wp-admin/includes/media.php
@@ -5,3 +5,13 @@
  * @package WordPress
  * @subpackage Administration
  */
+
+add_filter('media_upload_tabs', 'update_gallery_tab');
+
+add_filter( 'image_send_to_editor', 'image_add_caption', 20, 8 );
+
+add_action( 'media_buttons', 'media_buttons' );
+
+add_filter( 'attachment_fields_to_save', 'image_attachment_fields_to_save', 10, 2 );
+
+add_filter('media_send_to_editor', 'image_media_send_to_editor', 10, 3);
