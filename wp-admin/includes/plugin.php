c3e0e32bb7d9de3b426dff7025c913ebb7123702
Kite <ixkaito@gmail.com>
2015-11-02 08:17:30 +0900

    wp_die if current user cannot activate plugins

--- wp-admin/includes/plugin.php
+++ wp-admin/includes/plugin.php
@@ -8,3 +8,6 @@
 
 /** WordPress Administration Bootstrap */
 require_once( dirname( __FILE__ ) . '/admin.php' );
+
+if ( ! current_user_can('activate_plugins') )
+	wp_die( __( 'You do not have sufficient permissions to manage plugins for this site.' ) );
