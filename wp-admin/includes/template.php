9fefc0df807e830f2f7042c297526f039bdfffb3
Kite <ixkaito@gmail.com>
2015-11-07 17:12:55 +0900

    Add wp-admin/includes/template.php

---
+++ wp-admin/includes/template.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Template WordPress Administration API.
+ *
+ * A Big Mess. Also some neat function that are nicely written.
+ *
+ * @package WordPress
+ * @subpackage Administration
+ */
