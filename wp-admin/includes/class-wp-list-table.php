dc91b98c395bd84b494d7db3ba5be148c21103f6
Kite <ixkaito@gmail.com>
2015-11-08 17:15:50 +0900

    Add wp-admin/includes/class-wp-list-table.php

---
+++ wp-admin/includes/class-wp-list-table.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Base class for displaying a list of items in an ajaxified HTML table.
+ *
+ * @since 3.1.0
+ * @access private
+ *
+ * @package WordPress
+ * @subpackage List_Table
+ */
