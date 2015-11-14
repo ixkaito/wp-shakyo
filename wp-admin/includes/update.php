aeab8b38b3edd04e2dc224a6ab14576105a20367
Kite <ixkaito@gmail.com>
2015-11-14 23:55:25 +0900

    Add filters & actions in wp-admin/includes/update.php

--- wp-admin/includes/update.php
+++ wp-admin/includes/update.php
@@ -5,3 +5,15 @@
  * @package WordPress
  * @subpackage Administration
  */
+
+add_filter( 'update_footer', 'core_update_footer' );
+
+add_action( 'admin_notices', 'update_nag', 3 );
+add_action( 'network_admin_notices', 'update_nag', 3 );
+
+add_action( 'admin_init', 'wp_plugin_update_rows' );
+
+add_action( 'admin_init', 'wp_theme_update_rows' );
+
+add_action( 'admin_notices', 'maintenance_nag' );
+add_action( 'network_admin_notices', 'maintenance_nag' );
