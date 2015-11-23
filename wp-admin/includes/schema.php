19188c18462fb5f6eb09f984849f7fdb08c4e68a
Kite <ixkaito@gmail.com>
2015-11-24 00:14:34 +0900

    Add `wp_get_db_schema()`

--- wp-admin/includes/schema.php
+++ wp-admin/includes/schema.php
@@ -19,5 +19,18 @@ global $wpdb, $wp_queries, $charset_collate;
  */
 $charset_collate = $wpdb->get_charset_collate();
 
+/**
+ * Retrieve the SQL for creating database tables.
+ *
+ * @since 3.3.0
+ *
+ * @param string $scope Optional. The tables for which to retrieve SQL. Can be all, global, ms_global, or blog tables. Defaults to all.
+ * @param int $blog_id Optional. The blog ID for which to retrieve SQL. Default is the current blog ID.
+ * @return string The SQL needed to create the requested tables.
+ */
+function wp_get_db_schema( $scope = 'all', $blog_id = null ) {
+
+}
+
 // Populate for back compat.
 $wp_queries = wp_get_db_schema( 'all' );
