12ce5fcc1c03826ad1f51f85a605f37607da57db
Kite <ixkaito@gmail.com>
2015-11-26 00:41:42 +0900

    Add some contents in wp-admin/includes/upgrade.php

--- wp-admin/includes/upgrade.php
+++ wp-admin/includes/upgrade.php
@@ -18,4 +18,41 @@ require_once(ABSPATH . 'wp-admin/includes/admin.php');
 /** WordPress Schema API */
 require_once(ABSPATH . 'wp-admin/includes/schema.php');
 
+if ( !function_exists('wp_install') ) :
+/**
+ * Installs the blog
+ *
+ * {@internal Missing Long Description}}
+ *
+ * @since 2.1.0
+ *
+ * @param string $blog_title Blog title.
+ * @param string $user_name User's username.
+ * @param string $user_email User's email.
+ * @param bool $public Whether blog is public.
+ * @param null $deprecated Optional. Not used.
+ * @param string $user_password Optional. User's chosen password. Will default to a random password.
+ * @param string $language Optional. Language chosen.
+ * @return array Array keys 'url', 'user_id', 'password', 'password_message'.
+ */
+endif;
+
+if ( !function_exists('wp_install_defaults') ) :
+endif;
+
+if ( !function_exists('wp_new_blog_notification') ) :
+endif;
+
+if ( !function_exists('wp_upgrade') ) :
+endif;
+
+/**
+ * Install global terms.
+ *
+ * @since 3.0.0
+ *
+ */
+if ( !function_exists( 'install_global_terms' ) ) :
+endif;
+
 var_dump( __FILE__ );
