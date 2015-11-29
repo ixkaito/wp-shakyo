49d5da189f24c32d57b9489e73f7ce4d3c17e889
Kite <ixkaito@gmail.com>
2015-11-29 11:17:04 +0900

    Execute `nocache_headers()` in setup-config.php

--- wp-admin/setup-config.php
+++ wp-admin/setup-config.php
@@ -38,4 +38,6 @@ require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
 /** Load WordPress Translation Install API */
 require_once( ABSPATH . 'wp-admin/includes/translation-install.php' );
 
+nocache_headers();
+
 var_dump( __FILE__ );
