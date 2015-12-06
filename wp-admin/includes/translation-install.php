7542c4234a20c1ef880c1f1c6044eccd875a4b17
Kite <ixkaito@gmail.com>
2015-12-07 08:35:55 +0900

    Add `wp_can_install_language_pack()`

--- wp-admin/includes/translation-install.php
+++ wp-admin/includes/translation-install.php
@@ -6,3 +6,28 @@
  * @subpackage Administration
  */
 
+/**
+ * Check if WordPress has access to the filesystem without asking for
+ * credentials.
+ *
+ * @since 4.0.0
+ *
+ * @return bool Returns true on success, false on failure.
+ */
+function wp_can_install_language_pack() {
+	if ( defined( 'DISALLOW_FILE_MODS' ) && DISALLOW_FILE_MODS ) {
+		return false;
+	}
+
+	require_once ABSPATH . 'wp-admin/includes/class-wp-upgrader.php';
+	$skin = new Automatic_Upgrader_Skin;
+	$upgrader = new Language_Pack_Upgrader( $skin );
+
+	$check = $upgrader->fs_connect( array( WP_CONTENT_DIR, WP_LANG_DIR ) );
+
+	if ( ! $check || is_wp_error( $check ) ) {
+		return false;
+	}
+
+	return true;
+}
