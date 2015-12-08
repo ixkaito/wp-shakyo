d6809f7e878e28fcfcc84fd395cedc8a4ddc56dc
Kite <ixkaito@gmail.com>
2015-12-09 08:49:40 +0900

    Add `Automatic_Upgrader_Skin` class

--- wp-admin/includes/class-wp-upgrader-skins.php
+++ wp-admin/includes/class-wp-upgrader-skins.php
@@ -6,3 +6,18 @@
  * @subpackage Upgrader
  * @since 2.8.0
  */
+
+/**
+ * Upgrader Skin for Automatic WordPress Upgrades
+ *
+ * This skin is designed to be used when no output is intended, all output
+ * is captured and stored for the caller to process and log/email/discard.
+ *
+ * @package WordPress
+ * @subpackage Upgrader
+ * @since 3.7.0
+ */
+class Automatic_Upgrader_Skin extends WP_Upgrader_Skin {
+	protected $messages = array();
+
+}
