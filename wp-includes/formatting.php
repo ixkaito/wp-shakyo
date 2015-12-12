3b353c904a1e7241a59ab1e631d1b27405cdaa48
Kite <ixkaito@gmail.com>
2015-12-12 13:17:53 +0900

    Add `esc_attr()`

--- wp-includes/formatting.php
+++ wp-includes/formatting.php
@@ -280,3 +280,28 @@ function esc_url( $url, $protocols = null, $_context = 'display' ) {
 	 */
 	return apply_filters( 'clean_url', $good_protocol_url, $original_url, $_context );
 }
+
+/**
+ * Escaping for HTML attributes.
+ *
+ * @since 2.8.0
+ *
+ * @param string $text
+ * @return string
+ */
+function esc_attr( $text ) {
+	$safe_text = wp_check_invalid_utf8( $text );
+	$safe_text = _wp_specialchars( $safe_text, ENT_QUOTES );
+	/**
+	 * FIlter a string cleaned and escaped for output in an HTML attribute.
+	 *
+	 * Text passed to esc_attr() is stripped of invalid or special characters
+	 * before output.
+	 *
+	 * @since 2.0.6
+	 *
+	 * @param string $safe_text The text after it has been escaped.
+	 * @param string $text      The text prior to being escaped.
+	 */
+	return apply_filters( 'attribute_escape', $safe_text, $text );
+}
