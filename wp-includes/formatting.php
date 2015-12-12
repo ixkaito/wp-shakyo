c6f933400d49cdeb0e5502f539ec5ca53315c080
Kite <ixkaito@gmail.com>
2015-12-12 12:47:57 +0900

    Add `untrailingslashit()`

--- wp-includes/formatting.php
+++ wp-includes/formatting.php
@@ -202,3 +202,18 @@ function sanitize_key( $key ) {
 function trailingslashit( $string ) {
 	return untrailingslashit( $string ) . '/';
 }
+
+/**
+ * Removes trailing forward slashes and backslashes if they exist.
+ *
+ * The primary use of this is for paths and thus should be used for paths. It is
+ * not restricted to paths and offers no specific path support.
+ *
+ * @since 2.2.0
+ *
+ * @param string $string What to remove the trailing slashes from.
+ * @return string String without the trailing slashes.
+ */
+function untrailingslashit( $string ) {
+	return rtrim( $string, '/\\' );
+}
