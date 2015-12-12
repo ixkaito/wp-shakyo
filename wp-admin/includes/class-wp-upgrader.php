5744b7a1b8ddc53c0ddc9c1da3814b6960c2767e
Kite <ixkaito@gmail.com>
2015-12-12 09:03:39 +0900

    Declare some variables and constructor in `WP_Upgrader` class

--- wp-admin/includes/class-wp-upgrader.php
+++ wp-admin/includes/class-wp-upgrader.php
@@ -21,7 +21,16 @@ require ABSPATH . 'wp-admin/includes/class-wp-upgrader-skins.php';
  * @since 2.8.0
  */
 class WP_Upgrader {
+	public $strings = array();
+	public $skin = null;
+	public $result = array();
 
+	public function __construct($skin = null) {
+		if ( null == $skin )
+			$this->skin = new WP_Upgrader_Skin();
+		else
+			$this->skin = $skin;
+	}
 }
 
 add_action( 'upgrader_process_complete', array( 'Language_Pack_Upgrader', 'async_upgrade' ), 20 );
