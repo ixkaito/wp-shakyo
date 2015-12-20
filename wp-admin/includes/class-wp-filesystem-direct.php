7f67a1a64f0cf42e3e9a0b772ba5b3672b1601a7
Kite <ixkaito@gmail.com>
2015-12-20 19:11:03 +0900

    Add `WP_Filesystem_Direct` class

--- wp-admin/includes/class-wp-filesystem-direct.php
+++ wp-admin/includes/class-wp-filesystem-direct.php
@@ -5,3 +5,13 @@
  * @package WordPress
  * @subpackage Filesystem
  */
+
+/**
+ * WordPress Filesystem Class for direct PHP file and folder manipulation.
+ *
+ * @since 2.5.0
+ * @package WordPress
+ * @subpackage Filesystem
+ * @uses WP_Filesystem_Base Extends class
+ */
+class WP_Filesystem_Direct extends WP_Filesystem_Base {}
