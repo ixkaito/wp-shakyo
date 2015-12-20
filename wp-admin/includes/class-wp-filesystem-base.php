572e69fe2919d7b23bfe9ac8b1e3fa0df36e0603
Kite <ixkaito@gmail.com>
2015-12-20 19:12:31 +0900

    Add `WP_Filesystem_Base` class

--- wp-admin/includes/class-wp-filesystem-base.php
+++ wp-admin/includes/class-wp-filesystem-base.php
@@ -5,3 +5,12 @@
  * @package WordPress
  * @subpackage Filesystem
  */
+
+/**
+ * Base WordPress Filesystem class for which Filesystem implementations extend
+ *
+ * @since 2.5.0
+ */
+class WP_Filesystem_Base {
+
+}
