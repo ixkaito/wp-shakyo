4b6d44b72174ed04e074c74321569c2ba4bf41c7
Kite <ixkaito@gmail.com>
2015-12-21 19:19:35 +0900

    Add `WP_Error` class

--- wp-includes/class-wp-error.php
+++ wp-includes/class-wp-error.php
@@ -7,6 +7,47 @@
  * @package WordPress
  */
 
+/**
+ * WordPress Error class.
+ *
+ * Container for checking for WordPress errors and error messages. Return
+ * WP_Error and use {@link is_wp_error()} to check if this class is returned.
+ * Many core WordPress functions pass this class in the event of an error and
+ * if not handled properly will result in code errors.
+ *
+ * @package WordPress
+ * @since 2.1.0
+ */
+class WP_Error {
+
+	/**
+	 * Initialize the error.
+	 *
+	 * If `$code` is empty, the other parameters will be ignored.
+	 * When `$code` is not empty, `$message` will be used even if
+	 * it is empty. The `$data` parameter will be used only if it
+	 * is not empty.
+	 *
+	 * Though the class is constructed with a single error code and
+	 * message, multiple codes can be added using the `add()` method.
+	 *
+	 * @since 2.1.0
+	 *
+	 * @param string|int $code Error code
+	 * @param string $message Error message
+	 * @param mixed $data Optional. Error data.
+	 * @return WP_Error
+	 */
+	public function __construct( $code = '', $message = '', $data = '' ) {
+		if ( empty($code) )
+			return;
+
+		$this->errors[$code][] = $message;
+
+		if ( ! empty($data) )
+			$this->error_data[$code] = $data;
+	}
+}
 
 /**
  * Check whether variable is a WordPress Error.
