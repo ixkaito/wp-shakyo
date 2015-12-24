83d17a850bc9992fb5bd5ace8b3bae2ebdb0b780
Kite <ixkaito@gmail.com>
2015-12-24 19:43:50 +0900

    Add `translations_api()`

--- wp-admin/includes/translation-install.php
+++ wp-admin/includes/translation-install.php
@@ -6,6 +6,83 @@
  * @subpackage Administration
  */
 
+
+/**
+ * Retrieve translations from WordPress Translation API.
+ *
+ * @since 4.0.0
+ *
+ * @param string       $type Type of translations. Accepts 'plugins', 'themes', 'core'.
+ * @param array|object $args Translation API arguments. Optional.
+ * @return object|WP_Error On success an object of translations, WP_Error on failure.
+ */
+function translations_api( $type, $args = null ) {
+	include( ABSPATH . WPINC . '/version.php' ); // include an unmodified $wp_version
+
+	if ( ! in_array( $type, array( 'plugins', 'themes', 'core' ) ) ) {
+		return	new WP_Error( 'invalid_type', __( 'Invalid translation type.' ) );
+	}
+
+	/**
+	 * Allows a plugin to override the WordPress.org Translation Install API entirely.
+	 *
+	 * @since 4.0.0
+	 *
+	 * @param bool|array  $result The result object. Default false.
+	 * @param string      $type   The type of translations being requested.
+	 * @param object      $args   Translation API arguments.
+	 */
+	$res = apply_filters( 'translations_api', false, $type, $args );
+
+	if ( false === $res ) {
+		$url = $http_url = 'http://api.wordpress.org/translations/' . $type . '/1.0/';
+		if ( $ssl = wp_http_supports( array( 'ssl' ) ) ) {
+			$url = set_url_scheme( $url, 'https' );
+		}
+
+		$options = array(
+			'timeout' => 3,
+			'body' => array(
+				'wp_version' => $wp_version,
+				'locale'     => get_locale(),
+				'version'    => $args['version'], // Version of plugin, theme or core
+			),
+		);
+
+		if ( 'core' !== $type ) {
+			$options['body']['slug'] = $args['slug']; // Plugin or theme slug
+		}
+
+		$request = wp_remote_post( $url, $options );
+
+		if ( $ssl && is_wp_error( $request ) ) {
+			trigger_error( __( 'An unexpected error occurred. Something may be wrong with WordPress.org or this server&#8217;s configuration. If you continue to have problems, please try the <a href="https://wordpress.org/support/">support forums</a>.' ) . ' ' . __( '(WordPress could not establish a secure connection to WordPress.org. Please contact your server administrator.)' ), headers_sent() || WP_DEBUG ? E_USER_WARNING : E_USER_NOTICE );
+
+			$request = wp_remote_post( $http_url, $options );
+		}
+
+		if ( is_wp_error( $request ) ) {
+			$res = new WP_Error( 'translations_api_failed', __( 'An unexpected error occurred. Something may be wrong with WordPress.org or this server&#8217;s configuration. If you continue to have problems, please try the <a href="https://wordpress.org/support/">support forums</a>.' ), $request->get_error_message() );
+		} else {
+			$res = json_decode( wp_remote_retrieve_body( $request ), true );
+			if ( ! is_object( $res ) && ! is_array( $res ) ) {
+				$res = new WP_Error( 'translations_api_failed', __( 'An unexpected error occurred. Something may be wrong with WordPress.org or this server&#8217;s configuration. If you continue to have problems, please try the <a href="https://wordpress.org/support/">support forums</a>.' ), wp_remote_retrieve_body( $request ) );
+			}
+		}
+	}
+
+	/**
+	 * Filter the Translation Install API response results.
+	 *
+	 * @since 4.0.0
+	 *
+	 * @param object|WP_Error $res  Response object or WP_Error.
+	 * @param string          $type The type of translations being requested.
+	 * @param object          $args Translation API arguments.
+	 */
+	return apply_filters( 'translations_api_result', $res, $type, $args );
+}
+
 /**
  * Get available translations from the WordPress.org API.
  *
