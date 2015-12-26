f9355b62caaeb773056d07d7eb655f8b91a26243
Kite <ixkaito@gmail.com>
2015-12-27 01:32:26 +0900

    Add `WP_Http` class

--- wp-includes/class-http.php
+++ wp-includes/class-http.php
@@ -11,3 +11,20 @@
  * @subpackage HTTP
  * @since 2.7.0
  */
+
+/**
+ * WordPress HTTP Class for managing HTTP Transports and making HTTP requests.
+ *
+ * This class is used to consistently make outgoing HTTP requests easy for developers
+ * while still being compatible with the many PHP configurations under which
+ * WordPress runs.
+ *
+ * Debugging includes several actions, which pass different variables for debugging the HTTP API.
+ *
+ * @package WordPress
+ * @subpackage HTTP
+ * @since 2.7.0
+ */
+class WP_Http {
+
+}
