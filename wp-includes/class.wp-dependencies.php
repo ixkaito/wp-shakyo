91db33ea27f74cff5d94b603b3e16e271560bc67
Kite <ixkaito@gmail.com>
2016-01-29 01:39:18 +0900

    Add `WP_Dependencies::add()`

--- wp-includes/class.wp-dependencies.php
+++ wp-includes/class.wp-dependencies.php
@@ -11,6 +11,29 @@
  * @since r74
  */
 class WP_Dependencies {
+
+	/**
+	 * Register an item.
+	 *
+	 * Registers the item if no item of that name already exists.
+	 *
+	 * @access public
+	 * @since 2.1.0
+	 *
+	 * @param string $handle Unique item name.
+	 * @param string $src    The item url.
+	 * @param array  $deps   Optional. An array of item handle strings on which this item depends.
+	 * @param string $ver    Optional. Version (used for cache busting).
+	 * @param mixed  $args   Optional. Custom property of the item. NOT the class property $args. Examples: $media, $in_footer.
+	 * @return bool True on success, false on failure.
+	 */
+	public function add( $handle, $src, $deps = array(), $ver = false, $args = null ) {
+		if ( isset($this->registered[$handle]) )
+			return false;
+		$this->registered[$handle] = new _WP_Dependency( $handle, $src, $deps, $ver, $args );
+		return true;
+	}
+
 	/**
 	 * Query list for an item.
 	 *
