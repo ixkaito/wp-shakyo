4ef8cd408e64015421eb2973ed6a72ea839f9689
Kite <ixkaito@gmail.com>
2016-01-31 01:44:21 +0900

    Add `_WP_Dependency::add_data()`

--- wp-includes/class.wp-dependencies.php
+++ wp-includes/class.wp-dependencies.php
@@ -80,6 +80,7 @@ class WP_Dependencies {
  * @since 2.6.0
  */
 class _WP_Dependency {
+
 	/**
 	 * Setup dependencies.
 	 *
@@ -90,4 +91,22 @@ class _WP_Dependency {
 		if ( ! is_array($this->deps) )
 			$this->deps = array();
 	}
-}
+
+	/**
+	 * Add handle data.
+	 *
+	 * @access public
+	 * @since 2.6.0
+	 *
+	 * @param string $name The data key to add.
+	 * @param mixed  $data The data value to add.
+	 * @return bool False if not scalar, true otherwise.
+	 */
+	public function add_data( $name, $data ) {
+		if ( !is_scalar($name) )
+			return false;
+		$this->extra[$name] = $data;
+		return true;
+	}
+
+} // _WP_Dependencies
