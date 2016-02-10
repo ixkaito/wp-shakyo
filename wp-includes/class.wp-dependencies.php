4a6e2878b9749c69b08301f46f0658bce7f9c3cd
Kite <ixkaito@gmail.com>
2016-02-10 20:06:42 +0900

    Declare variables in `WP_Dependencies` class

--- wp-includes/class.wp-dependencies.php
+++ wp-includes/class.wp-dependencies.php
@@ -11,6 +11,70 @@
  * @since r74
  */
 class WP_Dependencies {
+	/**
+	 * An array of registered handle objects.
+	 *
+	 * @access public
+	 * @since 2.6.8
+	 * @var array
+	 */
+	public $registered = array();
+
+	/**
+	 * An array of queued _WP_Dependency handle objects.
+	 *
+	 * @access public
+	 * @since 2.6.8
+	 * @var array
+	 */
+	public $queue = array();
+
+	/**
+	 * An array of _WP_Dependency handle objects to queue.
+	 *
+	 * @access public
+	 * @since 2.6.0
+	 * @var array
+	 */
+	public $to_do = array();
+
+	/**
+	 * An array of _WP_Dependency handle objects already queued.
+	 *
+	 * @access public
+	 * @since 2.6.0
+	 * @var array
+	 */
+	public $done = array();
+
+	/**
+	 * An array of additional arguments passed when a handle is registered.
+	 *
+	 * Arguments are appended to the item query string.
+	 *
+	 * @access public
+	 * @since 2.6.0
+	 * @var array
+	 */
+	public $args = array();
+
+	/**
+	 * An array of handle groups to enqueue.
+	 *
+	 * @access public
+	 * @since 2.8.0
+	 * @var array
+	 */
+	public $groups = array();
+
+	/**
+	 * A handle group to enqueue.
+	 *
+	 * @access public
+	 * @since 2.8.0
+	 * @var int
+	 */
+	public $group = 0;
 
 	/**
 	 * Process the items and dependencies.
