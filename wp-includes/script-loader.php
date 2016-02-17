da2666d042dc8b51a31d5f053ca00c8ca711b072
Kite <ixkaito@gmail.com>
2016-02-17 21:40:50 +0900

    Add `wp_just_in_time_script_localization()`

--- wp-includes/script-loader.php
+++ wp-includes/script-loader.php
@@ -152,6 +152,22 @@ function wp_default_styles( &$styles ) {
 }
 
 /**
+ * Load localized data on print rather than initialization.
+ *
+ * These localizations require information that may not be loaded even by init.
+ *
+ * @since 2.5.0
+ */
+function wp_just_in_time_script_localization() {
+
+	wp_localize_script( 'autosave', 'autosaveL10n', array(
+		'autosaveInterval' => AUTOSAVE_INTERVAL,
+		'blog_id' => get_current_blog_id(),
+	) );
+
+}
+
+/**
  * Administration Screen CSS for changing the styles.
  *
  * If installing the 'wp-admin/' directory will be replaced with './'.
