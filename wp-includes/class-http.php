31830abf5c0dd62353a124d62e07e92ad8d3f7cb
Kite <ixkaito@gmail.com>
2016-02-27 01:27:55 +0900

    Add `WP_Http::get()`

--- wp-includes/class-http.php
+++ wp-includes/class-http.php
@@ -402,6 +402,24 @@ class WP_Http {
 	}
 
 	/**
+	 * Uses the GET HTTP method.
+	 *
+	 * Used for sending data that is expected to be in the body.
+	 *
+	 * @access public
+	 * @since 2.7.0
+	 *
+	 * @param string $url The request URL.
+	 * @param string|array $args Optional. Override the defaults.
+	 * @return array|WP_Error Array containing 'headers', 'body', 'response', 'cookies', 'filename'. A WP_Error instance upon error
+	 */
+	public function get($url, $args = array()) {
+		$defaults = array('method' => 'GET');
+		$r = wp_parse_args( $args, $defaults );
+		return $this->request($url, $r);
+	}
+
+	/**
 	 * Transform header string into an array.
 	 *
 	 * If an array is given then it is assumed to be raw header data with numeric keys with the
