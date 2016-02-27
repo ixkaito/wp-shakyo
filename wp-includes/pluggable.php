dc580427e446829f97f3b8673e1de7c0e5b31a28
Kite <ixkaito@gmail.com>
2016-02-28 01:31:44 +0900

    Add `wp_generate_password()`

--- wp-includes/pluggable.php
+++ wp-includes/pluggable.php
@@ -322,3 +322,38 @@ function wp_parse_auth_cookie($cookie = '', $scheme = '') {
 	return compact( 'username', 'expiration', 'token', 'hmac', 'scheme' );
 }
 endif;
+
+if ( !function_exists('wp_generate_password') ) :
+/**
+ * Generates a random password drawn from the defined set of characters.
+ *
+ * @since 2.5.0
+ *
+ * @param int $length The length of password to generate
+ * @param bool $special_chars Whether to include standard special characters. Default true.
+ * @param bool $extra_special_chars Whether to include other special characters. Used when
+ *   generating secret keys and salts. Default false.
+ * @return string The random password
+ **/
+function wp_generate_password( $length = 12, $special_chars = true, $extra_special_chars = false ) {
+	$chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+	if ( $special_chars )
+		$chars .= '!@#$%^&*()';
+	if ( $extra_special_chars )
+		$chars .= '-_ []{}<>~`+=,.;:/?|';
+
+	$password = '';
+	for ( $i = 0; $i < $length; $i++ ) {
+		$password .= substr($chars, wp_rand(0, strlen($chars) - 1), 1);
+	}
+
+	/**
+	 * Filter the randomly-generated password.
+	 *
+	 * @since 3.0.0
+	 *
+	 * @param string $password The generated password.
+	 */
+	return apply_filters( 'random_password', $password );
+}
+endif;
