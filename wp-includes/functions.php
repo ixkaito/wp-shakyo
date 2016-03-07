d744a50c549412631452fe629b3b782877287590
Kite <ixkaito@gmail.com>
2016-03-08 04:11:52 +0900

    Add `_config_wp_siteurl()`

--- wp-includes/functions.php
+++ wp-includes/functions.php
@@ -754,6 +754,27 @@ function _default_wp_die_handler( $message, $title = '', $args = array() ) {
 }
 
 /**
+ * Retrieve the WordPress site URL.
+ *
+ * If the constant named 'WP_SITEURL' is defined, then the value in that
+ * constant will always be returned. This can be used for debugging a site
+ * on your localhost while not having to change the database to your URL.
+ *
+ * @since 2.2.0
+ * @access private
+ *
+ * @see WP_SITEURL
+ *
+ * @param string $url URL to set the WordPress site location.
+ * @return string The WordPress Site URL.
+ */
+function _config_wp_siteurl( $url = '' ) {
+	if ( defined( 'WP_SITEURL' ) )
+		return untrailingslashit( WP_SITEURL );
+	return $url;
+}
+
+/**
  * Convert smiley code to the icon graphic file equivalent.
  *
  * You can turn off smilies, by going to the write setting screen and unchecking
