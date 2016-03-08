57b47154843026a94ef4063da2dd3cc7591d2461
Kite <ixkaito@gmail.com>
2016-03-09 04:13:07 +0900

    Add `_config_wp_home()`

--- wp-includes/functions.php
+++ wp-includes/functions.php
@@ -754,6 +754,27 @@ function _default_wp_die_handler( $message, $title = '', $args = array() ) {
 }
 
 /**
+ * Retrieve the WordPress home page URL.
+ *
+ * If the constant named 'WP_HOME' exists, then it will be used and returned
+ * by the function. This can be used to counter the redirection on your local
+ * development environment.
+ *
+ * @since 2.2.0
+ * @access private
+ *
+ * @see WP_HOME
+ *
+ * @param string $url URL for the home location.
+ * @return string Homepage location.
+ */
+function _config_wp_home( $url = '' ) {
+	if ( defined( 'WP_HOME' ) )
+		return untrailingslashit( WP_HOME );
+	return $url;
+}
+
+/**
  * Retrieve the WordPress site URL.
  *
  * If the constant named 'WP_SITEURL' is defined, then the value in that
