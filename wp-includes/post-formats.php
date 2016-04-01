436d9a214f3098584cf5f029695eca08329f718c
Kite <ixkaito@gmail.com>
2016-04-02 01:23:05 +0900

    Add `_post_format_request()`

--- wp-includes/post-formats.php
+++ wp-includes/post-formats.php
@@ -6,6 +6,23 @@
  * @subpackage Post
  */
 
+/**
+ * Filters the request to allow for the format prefix.
+ *
+ * @access private
+ * @since 3.1.0
+ */
+function _post_format_request( $qvs ) {
+	if ( ! isset( $qvs['post_format'] ) )
+		return $qvs;
+	$slugs = get_post_format_slugs();
+	if ( isset( $slugs[ $qvs['post_format'] ] ) )
+		$qvs['post_format'] = 'post-format-' . $slugs[ $qvs['post_format'] ];
+	$tax = get_taxonomy( 'post_format' );
+	if ( ! is_admin() )
+		$qvs['post_type'] = $tax->object_type;
+	return $qvs;
+}
 add_filter( 'request', '_post_format_request' );
 
 add_filter( 'term_link', '_post_format_link', 10, 3 );
