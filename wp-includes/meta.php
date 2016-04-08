3681864a20f1b6fed6bf22ca6d271935a8582af3
Kite <ixkaito@gmail.com>
2016-04-08 21:29:04 +0900

    Add class `WP_Meta_Query`

--- wp-includes/meta.php
+++ wp-includes/meta.php
@@ -10,3 +10,62 @@
  * @subpackage Meta
  * @since 2.9.0
  */
+
+/**
+ * Container class for a multiple metadata query
+ *
+ * @since 3.2.0
+ */
+class WP_Meta_Query {
+	/**
+	* List of metadata queries. A single query is an associative array:
+	* - 'key' string The meta key
+	* - 'value' string|array The meta value
+	* - 'compare' (optional) string How to compare the key to the value.
+	*              Possible values: '=', '!=', '>', '>=', '<', '<=', 'LIKE', 'NOT LIKE', 'IN', 'NOT IN',
+	*              'BETWEEN', 'NOT BETWEEN', 'REGEXP', 'NOT REGEXP', 'RLIKE'.
+	*              Default: '='
+	* - 'type' string (optional) The type of the value.
+	*              Possible values: 'NUMERIC', 'BINARY', 'CHAR', 'DATE', 'DATETIME', 'DECIMAL', 'SIGNED', 'TIME', 'UNSIGNED'.
+	*              Default: 'CHAR'
+	*
+	* @since 3.2.0
+	* @access public
+	* @var array
+	*/
+	public $queries = array();
+
+	/**
+	 * The relation between the queries. Can be one of 'AND' or 'OR'.
+	 *
+	 * @since 3.2.0
+	 * @access public
+	 * @var string
+	 */
+	public $relation;
+
+	/**
+	 * Constructor
+	 *
+	 * @param array $meta_query (optional) A meta query
+	 */
+	public function __construct( $meta_query = false ) {
+		if ( !$meta_query )
+			return;
+
+		if ( isset( $meta_query['relation'] ) && strtoupper( $meta_query['relation'] ) == 'OR' ) {
+			$this->relation = 'OR';
+		} else {
+			$this->relation = 'AND';
+		}
+
+		$this->queries = array();
+
+		foreach ( $meta_query as $key => $query ) {
+			if ( ! is_array( $query ) )
+				continue;
+
+			$this->queries[] = $query;
+		}
+	}
+}
