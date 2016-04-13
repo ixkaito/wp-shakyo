dafc4d6197dc781f4c94118cd201bf66f7ce8cbc
Kite <ixkaito@gmail.com>
2016-04-13 11:46:19 +0900

    Add `wp_strip_all_tags()`

--- wp-includes/formatting.php
+++ wp-includes/formatting.php
@@ -769,6 +769,29 @@ function wp_parse_str( $string, &$array ) {
 }
 
 /**
+ * Properly strip all HTML tags including script and style
+ *
+ * This differs from strip_tags() because it removes the contents of
+ * the <script> and <style> tags. E.g. strip_tags( '<script>something</script>' )
+ * will return 'something'. wp_strip_all_tags will return ''
+ *
+ * @since 2.9.0
+ *
+ * @param string $string String containing HTML tags
+ * @param bool $remove_breaks optional Whether to remove left over line breaks and white space chars
+ * @return string The processed string.
+ */
+function wp_strip_all_tags($string, $remove_breaks = false) {
+	$string = preg_replace( '@<(script|style)[^>]*?>.*?</\\1>@si', '', $string );
+	$string = strip_tags($string);
+
+	if ( $remove_breaks )
+		$string = preg_replace('/[\r\n\t ]+/', ' ', $string);
+
+	return trim( $string );
+}
+
+/**
  * Remove slashes from a string or array of strings.
  *
  * This should be used to remove slashes from data passed to core API that
