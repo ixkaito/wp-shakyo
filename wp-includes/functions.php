fa9d7c8c2a6fc2d762b236807c75dbfcb9948109
Kite <ixkaito@gmail.com>
2016-04-15 04:35:33 +0900

    Add `_get_non_cached_ids()`

--- wp-includes/functions.php
+++ wp-includes/functions.php
@@ -1446,6 +1446,29 @@ function wp_debug_backtrace_summary( $ignore_class = null, $skip_frames = 0, $pr
 }
 
 /**
+ * Retrieve ids that are not already present in the cache.
+ *
+ * @since 3.4.0
+ * @access private
+ *
+ * @param array  $object_ids ID list.
+ * @param string $cache_key  The cache bucket to check against.
+ *
+ * @return array List of ids not present in the cache.
+ */
+function _get_non_cached_ids( $object_ids, $cache_key ) {
+	$clean = array();
+	foreach ( $object_ids as $id ) {
+		$id = (int) $id;
+		if ( !wp_cache_get( $id, $cache_key ) ) {
+			$clean[] = $id;
+		}
+	}
+
+	return $clean;
+}
+
+/**
  * Retrieve a canonical form of the provided charset appropriate for passing to PHP
  * functions such as htmlspecialchars() and charset html attributes.
  *
