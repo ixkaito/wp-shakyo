39168a89e4941bd737816287c9a8ade32a5cf7ab
Kite <ixkaito@gmail.com>
2016-04-24 17:34:20 +0900

    Fix typo

--- wp-includes/link-template.php
+++ wp-includes/link-template.php
@@ -253,15 +253,15 @@ function set_url_scheme( $url, $scheme = null ) {
  * Return a shortlink for a post, page, attachment, or blog.
  *
  * This function exists to provide a shortlink tag that all themes and plugins can target. A plugin must hook in to
- * porvide the actual shortlinks. Default shortlink support is limited to providing ?p= style links for posts.
+ * provide the actual shortlinks. Default shortlink support is limited to providing ?p= style links for posts.
  * Plugins can short-circuit this function via the pre_get_shortlink filter or filter the output
  * via the get_shortlink filter.
  *
- * @since 3.0.0
+ * @since 3.0.0.
  *
  * @param int $id A post or blog id. Default is 0, which means the current post or blog.
  * @param string $context Whether the id is a 'blog' id, 'post' id, or 'media' id. If 'post', the post_type of the post is consulted. If 'query', the current query is consulted to determine the id and context. Default is 'post'.
- * @param bool $allow_slugs Wheterh to allow post slugs in the shortlink. It is up to the plugin how and whetehr to honor this.
+ * @param bool $allow_slugs Whether to allow post slugs in the shortlink. It is up to the plugin how and whether to honor this.
  * @return string A shortlink or an empty string if no shortlink exists for the requested resource or if shortlinks are not enabled.
  */
 function wp_get_shortlink($id = 0, $context = 'post', $allow_slugs = true) {
