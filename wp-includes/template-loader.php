c3680e04d62b7870371c0cb0b63a14b31b5a0062
Kite <ixkaito@gmail.com>
2016-04-25 15:18:34 +0900

    Process feeds and trackbacks even if not using themes.

--- wp-includes/template-loader.php
+++ wp-includes/template-loader.php
@@ -24,4 +24,21 @@ if ( defined('WP_USE_THEMES') && WP_USE_THEMES )
 if ( 'HEAD' === $_SERVER['REQUEST_METHOD'] && apply_filters( 'exit_on_http_head', true ) )
 	exit();
 
+// Process feeds and trackbacks even if not using themes.
+if ( is_robots() ) :
+	/**
+	 * Fired when the template loader determines a robots.txt request.
+	 *
+	 * @since 2.1.0
+	 */
+	do_action( 'do_robots' );
+	return;
+elseif ( is_feed() ) :
+	do_feed();
+	return;
+elseif ( is_trackback() ) :
+	include( ABSPATH . 'wp-trackback.php' );
+	return;
+endif;
+
 var_dump(__FILE__);
