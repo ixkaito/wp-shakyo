61c1b02de894ee2dd70cef02e0f370e8905329fb
Kite <ixkaito@gmail.com>
2016-04-26 06:32:02 +0900

    Add `get_front_page_template()`

--- wp-includes/template.php
+++ wp-includes/template.php
@@ -5,3 +5,21 @@
  * @package WordPress
  * @subpackage Template
  */
+
+/**
+ * Retrieve path of front-page template in current or parent template.
+ *
+ * Looks for 'front-page.php'. The template path is filterable via the
+ * 'front_page_template' hook.
+ *
+ * @since 3.0.0
+ *
+ * @see get_query_template()
+ *
+ * @return string Full path to front page template file.
+ */
+function get_front_page_template() {
+	$templates = array('front-page.php');
+
+	return get_query_template( 'front_page', $templates );
+}
