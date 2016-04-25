2d6ee6321969914e44cc8f8de91d60875117945c
Kite <ixkaito@gmail.com>
2016-04-25 15:15:33 +0900

    Filter whether to allow 'HEAD' requests to generate content.

--- wp-includes/template-loader.php
+++ wp-includes/template-loader.php
@@ -11,4 +11,17 @@ if ( defined('WP_USE_THEMES') && WP_USE_THEMES )
 	 */
 	do_action( 'template_redirect' );
 
+/**
+ * Filter whether to allow 'HEAD' requests to generate content.
+ *
+ * Provides a significant performance bump by exiting before the page
+ * content loads for 'HEAD' requests. See #14348.
+ *
+ * @since 3.5.0
+ *
+ * @param bool $exit Whether to exit without generating any content for 'HEAD' requests. Default true.
+ */
+if ( 'HEAD' === $_SERVER['REQUEST_METHOD'] && apply_filters( 'exit_on_http_head', true ) )
+	exit();
+
 var_dump(__FILE__);
