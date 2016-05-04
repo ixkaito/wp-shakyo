4007bb1b74ce8adf1fa526156d87122e6a00d0e3
Kite <ixkaito@gmail.com>
2016-05-05 07:30:12 +0900

    Add `wp_slash()`

--- wp-includes/formatting.php
+++ wp-includes/formatting.php
@@ -1207,6 +1207,33 @@ function wp_strip_all_tags($string, $remove_breaks = false) {
 }
 
 /**
+ * Add slashes to a string or array of strings.
+ *
+ * This should be used when preparing data for core API that expects slashed data.
+ * This should not be used to escape data going directly into an SQL query.
+ *
+ * @since 3.6.0
+ *
+ * @param string|array $value String or array of strings to slash.
+ * @return string|array Slashed $value
+ */
+function wp_slash( $value ) {
+	if ( is_array( $value ) ) {
+		foreach ( $value as $k => $v ) {
+			if ( is_array( $v ) ) {
+				$value[$k] = wp_slash( $v );
+			} else {
+				$value[$k] = addslashes( $v );
+			}
+		}
+	} else {
+		$value = addslashes( $value );
+	}
+
+	return $value;
+}
+
+/**
  * Remove slashes from a string or array of strings.
  *
  * This should be used to remove slashes from data passed to core API that
