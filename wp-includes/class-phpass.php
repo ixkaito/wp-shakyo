0b5f0cb8b95ed2e12aace1fa636f3f0d5d388488
Kite <ixkaito@gmail.com>
2016-06-04 05:24:57 +0900

    Add the comment of phpass

--- wp-includes/class-phpass.php
+++ wp-includes/class-phpass.php
@@ -6,3 +6,21 @@
  * @version 0.3 / WordPress
  * @link http://www.openwall.com/phpass/
  */
+
+#
+# Written by Solar Designer <solar at openwall.com> in 2004-2006 and placed in
+# the public domain.  Revised in subsequent years, still public domain.
+#
+# There's absolutely no warranty.
+#
+# Please be sure to update the Version line if you edit this file in any way.
+# It is suggested that you leave the main version number intact, but indicate
+# your project name (after the slash) and add your own revision information.
+#
+# Please do not change the "private" password hashing method implemented in
+# here, thereby making your hashes incompatible.  However, if you must, please
+# change the hash type identifier (the "$P$") to something different.
+#
+# Obviously, since this code is in the public domain, the above are not
+# requirements (there can be none), but merely suggestions.
+#
