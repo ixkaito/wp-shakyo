83dc35793494ec925dd03c8b6e375ab32dcc09bd
Kite <ixkaito@gmail.com>
2016-06-08 16:11:54 +0900

    Add `email_exists()`

--- wp-includes/user.php
+++ wp-includes/user.php
@@ -145,6 +145,21 @@ function username_exists( $username ) {
 }
 
 /**
+ * Checks whether the given email exists.
+ *
+ * @since 2.1.0
+ *
+ * @param string $email Email.
+ * @return bool|int The user's ID on success, and false on failure.
+ */
+function email_exists( $email ) {
+	if ( $user = get_user_by('email', $email) )
+		return $user->ID;
+
+	return false;
+}
+
+/**
  * Insert an user into the database.
  *
  * Most of the $userdata array fields have filters associated with the values.
