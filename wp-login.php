561aa7bc13226e0fe5e0c5d61daad63f652f6f68
Kite <ixkaito@gmail.com>
2016-06-26 06:53:38 +0200

    validate action so as to default to the login screen

--- wp-login.php
+++ wp-login.php
@@ -21,3 +21,17 @@ if ( force_ssl_admin() && ! is_ssl() ) {
 		exit();
 	}
 }
+
+//
+// Main
+//
+
+$action = isset($_REQUEST['action']) ? $_REQUEST['action'] : 'login';
+$errors = new WP_Error();
+
+if ( isset($_GET['key']) )
+	$action = 'resetpass';
+
+// validate action so as to default to the login screen
+if ( !in_array( $action, array( 'postpass', 'logout', 'lostpassword', 'retrievepassword', 'resetpass', 'rp', 'register', 'login' ), true ) && false === has_filter( 'login_form_' . $action ) )
+	$action = 'login';
