b2e1186b00c56c12e9c8e0a37f172d202a9380d9
Kite <ixkaito@gmail.com>
2016-07-09 08:36:10 +0900

    Add `sanitize_html_class()`

--- wp-includes/formatting.php
+++ wp-includes/formatting.php
@@ -692,6 +692,43 @@ function sanitize_title_with_dashes( $title, $raw_title = '', $context = 'displa
 }
 
 /**
+ * Sanitizes an HTML classname to ensure it only contains valid characters.
+ *
+ * Strips the string down to A-Z,a-z,0-9,_,-. If this results in an empty
+ * string then it will return the alternative value supplied.
+ *
+ * @todo Expand to support the full range of CDATA that a class attribute can contain.
+ *
+ * @since 2.8.0
+ *
+ * @param string $class The classname to be sanitized
+ * @param string $fallback Optional. The value to return if the sanitization ends up as an empty string.
+ * 	Defaults to an empty string.
+ * @return string The sanitized value
+ */
+function sanitize_html_class( $class, $fallback = '' ) {
+	//Strip out any % encoded octets
+	$sanitized = preg_replace( '|%[a-fA-F0-9][a-fA-F0-9]|', '', $class );
+
+	//Limit to A-Z,a-z,0-9,_,-
+	$sanitized = preg_replace( '/[^A-Za-z0-9_-]/', '', $sanitized );
+
+	if ( '' == $sanitized )
+		$sanitized = $fallback;
+
+	/**
+	 * Filter a sanitized HTML class string.
+	 *
+	 * @since 2.8.0
+	 *
+	 * @param string $sanitized The sanitized HTML class.
+	 * @param string $class     HTML class before sanitization.
+	 * @param string $fallback  The fallback string.
+	 */
+	return apply_filters( 'sanitize_html_class', $sanitized, $class, $fallback );
+}
+
+/**
  * Appends a trailing slash.
  *
  * Will remove trailing forward and backslashes if it exists already before adding
