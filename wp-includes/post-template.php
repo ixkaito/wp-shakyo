cd5a42189eccb4513a2d6e878ceb6eff3f157653
Kite <ixkaito@gmail.com>
2016-07-30 05:35:18 +0900

    Add `body_class()`

--- wp-includes/post-template.php
+++ wp-includes/post-template.php
@@ -8,3 +8,14 @@
  * @subpackage Template
  */
 
+/**
+ * Display the classes for the body element.
+ *
+ * @since 2.8.0
+ *
+ * @param string|array $class One or more classes to add to the class list.
+ */
+function body_class( $class = '' ) {
+	// Separates classes with a single space, collates classes for body element
+	echo 'class="' . join( ' ', get_body_class( $class ) ) . '"';
+}
