5a656fb5322aead10184c65bae6a56e033c5289f
Kite <ixkaito@gmail.com>
2016-08-01 07:42:55 +0900

    Add `is_multi_author()`

--- wp-includes/author-template.php
+++ wp-includes/author-template.php
@@ -9,3 +9,30 @@
  * @package WordPress
  * @subpackage Template
  */
+
+/**
+ * Does this site have more than one author
+ *
+ * Checks to see if more than one author has published posts.
+ *
+ * @since 3.2.0
+ * @return bool Whether or not we have more than one author
+ */
+function is_multi_author() {
+	global $wpdb;
+
+	if ( false === ( $is_multi_author = get_transient( 'is_multi_author' ) ) ) {
+		$rows = (array) $wpdb->get_col("SELECT DISTINCT post_author FROM $wpdb->posts WHERE post_type = 'post' AND post_status = 'publish' LIMIT 2");
+		$is_multi_author = 1 < count( $rows ) ? 1 : 0;
+		set_transient( 'is_multi_author', $is_multi_author );
+	}
+
+	/**
+	 * Filter whether the site has more than one author with published posts.
+	 *
+	 * @since 3.2.0
+	 *
+	 * @param bool $is_multi_author Whether $is_multi_author should evaluate as true.
+	 */
+	return apply_filters( 'is_multi_author', (bool) $is_multi_author );
+}
