f825e5930740595418780251dd42c56514e01f70
Kite <ixkaito@gmail.com>
2016-08-08 14:00:13 +0900

    Add `wp_get_nav_menu_object()`

--- wp-includes/nav-menu.php
+++ wp-includes/nav-menu.php
@@ -6,3 +6,32 @@
  * @subpackage Nav_Menus
  * @since 3.0.0
  */
+
+/**
+ * Returns a navigation menu object.
+ *
+ * @since 3.0.0
+ *
+ * @uses get_term
+ * @uses get_term_by
+ *
+ * @param string $menu Menu ID, slug, or name.
+ * @return mixed false if $menu param isn't supplied or term does not exist, menu object if successful.
+ */
+function wp_get_nav_menu_object( $menu ) {
+	if ( ! $menu )
+		return false;
+
+	$menu_obj = get_term( $menu, 'nav_menu' );
+
+	if ( ! $menu_obj )
+		$menu_obj = get_term_by( 'slug', $menu, 'nav_menu' );
+
+	if ( ! $menu_obj )
+		$menu_obj = get_term_by( 'name', $menu, 'nav_menu' );
+
+	if ( ! $menu_obj )
+		$menu_obj = false;
+
+	return $menu_obj;
+}
