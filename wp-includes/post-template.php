110fb076364fadc967e15a440425dce1f98b41e3
Kite <ixkaito@gmail.com>
2016-08-17 03:09:11 +0900

    Fix typo

--- wp-includes/post-template.php
+++ wp-includes/post-template.php
@@ -345,7 +345,7 @@ function get_body_class( $class = '' ) {
  *
  * @since 2.7.0
  *
- * @param int|WP_Post $post An optional post. Global $psot used if not provided.
+ * @param int|WP_Post $post An optional post. Global $post used if not provided.
  * @return bool false if a password is not required or the correct password cookie is present, true otherwise.
  */
 function post_password_required( $post = null ) {
