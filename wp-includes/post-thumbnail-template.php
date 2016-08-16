65c7be1da5dd2f0ccd49a09f8d114fc73772ebff
Kite <ixkaito@gmail.com>
2016-08-17 03:09:56 +0900

    Add `has_post_thumbnail()`

--- wp-includes/post-thumbnail-template.php
+++ wp-includes/post-thumbnail-template.php
@@ -8,3 +8,15 @@
  * @package WordPress
  * @subpackage Template
  */
+
+/**
+ * Check if post has an image attached.
+ *
+ * @since 2.9.0
+ *
+ * @param int $post_id Optional. Post ID.
+ * @return bool Whether post has an image attached.
+ */
+function has_post_thumbnail( $post_id = null ) {
+	return (bool) get_post_thumbnail_id( $post_id );
+}
