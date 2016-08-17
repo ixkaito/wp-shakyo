3b654f901cfc789a6fc58031bdfcfcaba091ac88
Kite <ixkaito@gmail.com>
2016-08-18 03:58:14 +0900

    Add `get_post_type()`

--- wp-includes/post.php
+++ wp-includes/post.php
@@ -593,6 +593,21 @@ function get_post_stati( $args = array(), $output = 'names', $operator = 'and' )
 }
 
 /**
+ * Retrieve the post type of the current post or of a given post.
+ *
+ * @since 2.1.0
+ *
+ * @param int|WP_Post $post Optional. Post ID or post object. Default is global $post.
+ * @return string|bool Post type on success, false on failure.
+ */
+function get_post_type( $post = null ) {
+	if ( $post = get_post( $post ) )
+		return $post->post_type;
+
+	return false;
+}
+
+/**
  * Get a list of all registered post type objects.
  *
  * @since 2.9.0
