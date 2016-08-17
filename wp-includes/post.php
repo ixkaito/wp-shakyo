4c82f7684cfd825c5366f0598d923f8551ba6b27
Kite <ixkaito@gmail.com>
2016-08-18 03:55:13 +0900

    Add `get_post_meta()`

--- wp-includes/post.php
+++ wp-includes/post.php
@@ -1158,6 +1158,22 @@ function add_post_type_support( $post_type, $feature ) {
 	}
 }
 
+/**
+ * Retrieve post meta field for a post.
+ *
+ * @since 1.5.0
+ *
+ * @param int    $post_id Post ID.
+ * @param string $key     Optional. The meta key to retrieve. By default, returns
+ *                        data for all keys. Default empty.
+ * @param bool   $single  Optional. Whether to return a single value. Default false.
+ * @return mixed Will be an array if $single is false. Will be value of meta data
+ *               field if $single is true.
+ */
+function get_post_meta( $post_id, $key = '', $single = false ) {
+	return get_metadata('post', $post_id, $key, $single);
+}
+
 add_action( 'before_delete_post', '_reset_front_page_settings_for_post' );
 add_action( 'wp_trash_post',      '_reset_front_page_settings_for_post' );
 
