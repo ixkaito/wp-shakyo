58a57dc22976374aec2820b52bd771b895d28b81
Kite <ixkaito@gmail.com>
2016-08-20 08:27:36 +0900

    Add `comments_open()`

--- wp-includes/comment-template.php
+++ wp-includes/comment-template.php
@@ -7,3 +7,28 @@
  * @package WordPress
  * @subpackage Template
  */
+
+/**
+ * Whether the current post is open for comments.
+ *
+ * @since 1.5.0
+ *
+ * @param int|WP_Post $post_id Post Id or WP_Post object. Default current post.
+ * @return bool True if the comments are open.
+ */
+function comments_open( $post_id = null ) {
+
+	$_post = get_post($post_id);
+
+	$open = ( 'open' == $_post->comment_status );
+
+	/**
+	 * Filter whether the current post is open for comments.
+	 *
+	 * @since 2.5.0
+	 *
+	 * @param bool        $open    Whether the current post is open for comments.
+	 * @param int|WP_Post $post_id The post ID or WP_Post object.
+	 */
+	return apply_filters( 'comments_open', $open, $post_id );
+}
