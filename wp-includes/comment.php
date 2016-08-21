c8d82d5425acc630320c1b9468404d9115a24227
Kite <ixkaito@gmail.com>
2016-08-21 23:16:03 +0900

    Add `_close_comments_for_old_post()`

--- wp-includes/comment.php
+++ wp-includes/comment.php
@@ -106,3 +106,37 @@ function _close_comments_for_old_posts( $posts, $query ) {
 
 	return $posts;
 }
+
+/**
+ * Close comments on an old post. Hooked to comments_open and pings_open.
+ *
+ * @access private
+ * @since 2.7.0
+ *
+ * @param bool $open Comments open or closed
+ * @param int $post_id Post ID
+ * @return bool $open
+ */
+function _close_comments_for_old_post( $open, $post_id ) {
+	if ( ! $open )
+		return $open;
+
+	if ( !get_option('close_comments_for_old_posts') )
+		return $open;
+
+	$days_old = (int) get_option('close_comments_days_old');
+	if ( !$days_old )
+		return $open;
+
+	$post = get_post($post_id);
+
+	/** This filter is documented in wp-includes/comment.php */
+	$post_types = apply_filters( 'close_comments_for_post_types', array( 'post' ) );
+	if ( ! in_array( $post->post_type, $post_types ) )
+		return $open;
+
+	if ( time() - strtotime( $post->post_date_gmt ) > ( $days_old * DAY_IN_SECONDS ) )
+		return false;
+
+	return $open;
+}
