83454fb36fd6148d52a48ad6ccf16e3f44e28baf
Kite <ixkaito@gmail.com>
2016-08-24 01:10:22 +0900

    Add `the_tags()`

--- wp-includes/category-template.php
+++ wp-includes/category-template.php
@@ -5,3 +5,18 @@
  * @package WordPress
  * @subpackage Template
  */
+
+/**
+ * Retrieve the tags for a post.
+ *
+ * @since 2.3.0
+ *
+ * @param string $before Optional. Before list.
+ * @param string $sep Optional. Separate items using this.
+ * @param string $after Optional. After list.
+ */
+function the_tags( $before = null, $sep = ', ', $after = '' ) {
+	if ( null === $before )
+		$before = __('Tags: ');
+	echo get_the_tag_list($before, $sep, $after);
+}
