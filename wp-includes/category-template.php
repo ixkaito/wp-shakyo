0c57f95e8caf707ab3dc1de5f87b957febb764f5
Kite <ixkaito@gmail.com>
2016-08-24 01:13:01 +0900

    Add `get_the_tag_list()`

--- wp-includes/category-template.php
+++ wp-includes/category-template.php
@@ -7,6 +7,33 @@
  */
 
 /**
+ * Retrieve the tags for a post formatted as a string.
+ *
+ * @since 2.3.0
+ *
+ * @param string $before Optional. Before tags.
+ * @param string $sep Optional. Between tags.
+ * @param string $after Optional. After tags.
+ * @param int $id Optional. Post ID. Defaults to the current post.
+ * @return string|bool|WP_Error A list of tags on success, false if there are no terms, WP_Error on failure.
+ */
+function get_the_tag_list( $before = '', $sep = '', $after = '', $id = 0 ) {
+
+	/**
+	 * Filter the tags list for a given post.
+	 *
+	 * @since 2.3.0
+	 *
+	 * @param string $tag_list List of tags.
+	 * @param string $before   String to use before tags.
+	 * @param string $sep      String to use between the tags.
+	 * @param string $after    String to use after tags.
+	 * @param int    $id       Post ID.
+	 */
+	return apply_filters( 'the_tags', get_the_term_list( $id, 'post_tag', $before, $sep, $after ), $before, $sep, $after, $id );
+}
+
+/**
  * Retrieve the tags for a post.
  *
  * @since 2.3.0
