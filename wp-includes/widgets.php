f5ae2ed8ef5a19fcd1452bdf3c06bf0cab2f8184
Kite <ixkaito@gmail.com>
2016-08-26 05:32:16 +0900

    Fix typo

--- wp-includes/widgets.php
+++ wp-includes/widgets.php
@@ -88,13 +88,13 @@ $GLOBALS['_wp_deprecated_widgets_callbacks'] = array(
 /**
  * Display dynamic sidebar.
  *
- * By default this display the default sidebar or 'sidebar-1'. If your theme specifies the 'id' or
- * 'name' parameter for ists registered sidebars you can pass an id or name as the $index parameter.
+ * By default this displays the default sidebar or 'sidebar-1'. If your theme specifies the 'id' or
+ * 'name' parameter for its registered sidebars you can pass an id or name as the $index parameter.
  * Otherwise, you can pass in a numerical index to display the sidebar at that index.
  *
  * @since 2.2.0
  *
- * @param int|string $index Optional, default is 1. Index, name or ID or dynamic sidebar.
+ * @param int|string $index Optional, default is 1. Index, name or ID of dynamic sidebar.
  * @return bool True, if widget sidebar was found and called. False if not found or not called.
  */
 function dynamic_sidebar($index = 1) {
