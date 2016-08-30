7c11f7a9190603b7fa8e1d484e1dcbe826ebf2b4
Kite <ixkaito@gmail.com>
2016-08-31 05:33:41 +0900

    Fix indent

--- wp-includes/default-filters.php
+++ wp-includes/default-filters.php
@@ -216,7 +216,7 @@ add_action( 'init',                'check_theme_switched',            99    );
 add_action( 'after_switch_theme',  '_wp_sidebars_changed'                   );
 
 if ( isset( $_GET['replytocom'] ) )
-	add_action( 'wp_head', 'wp_no_robots' );
+    add_action( 'wp_head', 'wp_no_robots' );
 
 // Login actions
 add_action( 'login_head',          'wp_print_head_scripts',         9     );
