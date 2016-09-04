6616d392b47912292e08133d890589c5b4327039
Kite <ixkaito@gmail.com>
2016-09-05 03:34:53 +0900

    Add Class `WP_Session_Tokens`

--- wp-includes/session.php
+++ wp-includes/session.php
@@ -4,3 +4,25 @@
  *
  * @since 4.0.0
  */
+abstract class WP_Session_Tokens {
+
+	/**
+	 * User ID.
+	 *
+	 * @since 4.0.0
+	 * @access protected
+	 * @var int User ID.
+	 */
+	protected $user_id;
+
+	/**
+	 * Protected constructor.
+	 *
+	 * @since 4.0.0
+	 *
+	 * @param int $user_id User whose session to manage.
+	 */
+	protected function __construct( $user_id ) {
+		$this->user_id = $user_id;
+	}
+}
