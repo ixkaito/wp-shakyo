d092a3c87dfb1300cd7b504a34febb2156e9a7da
Kite <ixkaito@gmail.com>
2016-09-05 03:37:16 +0900

    Add `WP_Session_Tokens::get_instance()`

--- wp-includes/session.php
+++ wp-includes/session.php
@@ -25,4 +25,29 @@ abstract class WP_Session_Tokens {
 	protected function __construct( $user_id ) {
 		$this->user_id = $user_id;
 	}
+
+	/**
+	 * Get a session token manager instance for a user.
+	 *
+	 * This method contains a filter that allows a plugin to swap out
+	 * the session manager for a subclass of WP_Session_Tokens.
+	 *
+	 * @since 4.0.0
+	 * @access public
+	 * @static
+	 *
+	 * @param int $user_id User whose session to manage.
+	 */
+	final public static function get_instance( $user_id ) {
+		/**
+		 * Filter the session token manager used.
+		 *
+		 * @since 4.0.0
+		 *
+		 * @param string $session Name of class to use as the manager.
+		 *                        Default 'WP_User_Meta_Session_Tokens'.
+		 */
+		$manager = apply_filters( 'session_token_manager', 'WP_User_Meta_Session_Tokens' );
+		return new $manager( $user_id );
+	}
 }
