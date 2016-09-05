0a9ea6f65c9cd451452093cdbbb355a40e3f1da7
Kite <ixkaito@gmail.com>
2016-09-06 05:38:29 +0900

    Add `wp_generate_auth_cookie()`

--- wp-includes/pluggable.php
+++ wp-includes/pluggable.php
@@ -669,6 +669,54 @@ function wp_validate_auth_cookie($cookie = '', $scheme = '') {
 }
 endif;
 
+if ( !function_exists('wp_generate_auth_cookie') ) :
+/**
+ * Generate authentication cookie contents.
+ *
+ * @since 2.5.0
+ *
+ * @param int $user_id User ID
+ * @param int $expiration Cookie expiration in seconds
+ * @param string $scheme Optional. The cookie scheme to use: auth, secure_auth, or logged_in
+ * @param string $token User's session token to use for this cookie
+ * @return string Authentication cookie contents. Empty string if user does not exist.
+ */
+function wp_generate_auth_cookie( $user_id, $expiration, $scheme = 'auth', $token = '' ) {
+	$user = get_userdata($user_id);
+	if ( ! $user ) {
+		return '';
+	}
+
+	if ( ! $token ) {
+		$manager = WP_Session_Tokens::get_instance( $user_id );
+		$token = $manager->create( $expiration );
+	}
+
+	$pass_frag = substr($user->user_pass, 8, 4);
+
+	$key = wp_hash( $user->user_login . '|' . $pass_frag . '|' . $expiration . '|' . $token, $scheme );
+
+	// If ext/hash is not present, compat.php's hash_hmac() does not support sha256.
+	$algo = function_exists( 'hash' ) ? 'sha256' : 'sha1';
+	$hash = hash_hmac( $algo, $user->user_login . '|' . $expiration . '|' . $token, $key );
+
+	$cookie = $user->user_login . '|' . $expiration . '|' . $token . '|' . $hash;
+
+	/**
+	 * Filter the authentication cookie.
+	 *
+	 * @since 2.5.0
+	 *
+	 * @param string $cookie     Authentication cookie.
+	 * @param int    $user_id    User ID.
+	 * @param int    $expiration Authentication cookie expiration in seconds.
+	 * @param string $scheme     Cookie scheme used. Accepts 'auth', 'secure_auth', or 'logged_in'.
+	 * @param string $token      User's session token used.
+	 */
+	return apply_filters( 'auth_cookie', $cookie, $user_id, $expiration, $scheme, $token );
+}
+endif;
+
 if ( !function_exists('wp_parse_auth_cookie') ) :
 /**
  * Parse a cookie into its components
