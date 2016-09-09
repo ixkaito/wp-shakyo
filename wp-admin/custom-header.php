03071db1ece151af82d9049b29ce743abe32a292
Kite <ixkaito@gmail.com>
2016-09-10 06:32:59 +0900

    Add class `Custom_Image_Header`

--- wp-admin/custom-header.php
+++ wp-admin/custom-header.php
@@ -5,3 +5,14 @@
  * @package WordPress
  * @subpackage Administration
  */
+
+/**
+ * The custom header image class.
+ *
+ * @since 2.1.0
+ * @package WordPress
+ * @subpackage Administration
+ */
+class Custom_Image_Header {
+
+}
