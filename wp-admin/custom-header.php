e27f83ea81c583c6588aaa87c75f19383d08daae
Kite <ixkaito@gmail.com>
2016-09-10 06:33:39 +0900

    Declare variables in class `Custom_Image_Header`

--- wp-admin/custom-header.php
+++ wp-admin/custom-header.php
@@ -15,4 +15,48 @@
  */
 class Custom_Image_Header {
 
+	/**
+	 * Callback for administration header.
+	 *
+	 * @var callback
+	 * @since 2.1.0
+	 * @access private
+	 */
+	private $admin_header_callback;
+
+	/**
+	 * Callback for header div.
+	 *
+	 * @var callback
+	 * @since 3.0.0
+	 * @access private
+	 */
+	private $admin_image_div_callback;
+
+	/**
+	 * Holds default headers.
+	 *
+	 * @var array
+	 * @since 3.0.0
+	 * @access private
+	 */
+	private $default_headers = array();
+
+	/**
+	 * Holds custom headers uploaded by the user.
+	 *
+	 * @var array
+	 * @since 3.2.0
+	 * @access private
+	 */
+	private $uploaded_headers = array();
+
+	/**
+	 * Holds the page menu hook.
+	 *
+	 * @var string
+	 * @since 3.0.0
+	 * @access private
+	 */
+	private $page = '';
 }
