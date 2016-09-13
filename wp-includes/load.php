f2eb4e4714bfc2d55047ddb8331b7927c5fcd6ed
Kite <ixkaito@gmail.com>
2016-09-13 19:00:23 +0900

    Fix typos

--- wp-includes/load.php
+++ wp-includes/load.php
@@ -703,11 +703,12 @@ function is_user_admin() {
  *
  * @since 3.0.0
  *
- * @return bool True if Multisite is enabled, false oterwise.
+ * @return bool True if Multisite is enabled, false otherwise.
  */
 function is_multisite() {
 	if ( defined( 'MULTISITE' ) )
-		return  MULTISITE;
+		return MULTISITE;
+
 	if ( defined( 'SUBDOMAIN_INSTALL' ) || defined( 'VHOST' ) || defined( 'SUNRISE' ) )
 		return true;
 
