8dfd62b687c4652a8577162b3a69de6b21d65676
Kite <ixkaito@gmail.com>
2016-09-13 20:14:50 +0900

    Debuging

--- wp-blog-header.php
+++ wp-blog-header.php
@@ -5,14 +5,15 @@
  * @package WordPress
  */
 
+
 if ( !isset($wp_did_header) ) {
 
 	$wp_did_header = true;
 
 	require_once( dirname(__FILE__) . '/wp-load.php' );
 
-	wp();
+// 	wp();
 
-	require_once( ABSPATH . WPINC . '/template-loader.php' );
+// 	require_once( ABSPATH . WPINC . '/template-loader.php' );
 
 }
--- wp-includes/default-constants.php
+++ wp-includes/default-constants.php
@@ -89,235 +89,235 @@ function wp_initial_constants() {
 	define( 'YEAR_IN_SECONDS',  365 * DAY_IN_SECONDS    );
 }
 
-/**
- * Defines plugin directory WordPress constants
- *
- * Defines must-use plugin directory constants, which may be overridden in the sunrise.php drop-in
- *
- * @since 3.0.0
- */
-function wp_plugin_directory_constants() {
-	if ( !defined('WP_CONTENT_URL') )
-		define( 'WP_CONTENT_URL', get_option('siteurl') . '/wp-content'); // full url - WP_CONTENT_DIR is defined further up
+// /**
+//  * Defines plugin directory WordPress constants
+//  *
+//  * Defines must-use plugin directory constants, which may be overridden in the sunrise.php drop-in
+//  *
+//  * @since 3.0.0
+//  */
+// function wp_plugin_directory_constants() {
+// 	if ( !defined('WP_CONTENT_URL') )
+// 		define( 'WP_CONTENT_URL', get_option('siteurl') . '/wp-content'); // full url - WP_CONTENT_DIR is defined further up
 
-	/**
-	 * Allows for the plugins directory to be moved from the default location.
-	 *
-	 * @since 2.6.0
-	 */
-	if ( !defined('WP_PLUGIN_DIR') )
-		define( 'WP_PLUGIN_DIR', WP_CONTENT_DIR . '/plugins' ); // full path, no trailing slash
+// 	/**
+// 	 * Allows for the plugins directory to be moved from the default location.
+// 	 *
+// 	 * @since 2.6.0
+// 	 */
+// 	if ( !defined('WP_PLUGIN_DIR') )
+// 		define( 'WP_PLUGIN_DIR', WP_CONTENT_DIR . '/plugins' ); // full path, no trailing slash
 
-	/**
-	 * Allows for the plugins directory to be moved from the default location.
-	 *
-	 * @since 2.6.0
-	 */
-	if ( !defined('WP_PLUGIN_URL') )
-		define( 'WP_PLUGIN_URL', WP_CONTENT_URL . '/plugins' ); // full url, no trailing slash
+// 	/**
+// 	 * Allows for the plugins directory to be moved from the default location.
+// 	 *
+// 	 * @since 2.6.0
+// 	 */
+// 	if ( !defined('WP_PLUGIN_URL') )
+// 		define( 'WP_PLUGIN_URL', WP_CONTENT_URL . '/plugins' ); // full url, no trailing slash
 
-	/**
-	 * Allows for the plugins directory to be moved from the default location.
-	 *
-	 * @since 2.1.0
-	 * @deprecated
-	 */
-	if ( !defined('PLUGINDIR') )
-		define( 'PLUGINDIR', 'wp-content/plugins' ); // Relative to ABSPATH. For back compat.
+// 	/**
+// 	 * Allows for the plugins directory to be moved from the default location.
+// 	 *
+// 	 * @since 2.1.0
+// 	 * @deprecated
+// 	 */
+// 	if ( !defined('PLUGINDIR') )
+// 		define( 'PLUGINDIR', 'wp-content/plugins' ); // Relative to ABSPATH. For back compat.
 
-	/**
-	 * Allows for the mu-plugins directory to be moved from the default location.
-	 *
-	 * @since 2.8.0
-	 */
-	if ( !defined('WPMU_PLUGIN_DIR') )
-		define( 'WPMU_PLUGIN_DIR', WP_CONTENT_DIR . '/mu-plugins' ); // full path, no trailing slash
+// 	/**
+// 	 * Allows for the mu-plugins directory to be moved from the default location.
+// 	 *
+// 	 * @since 2.8.0
+// 	 */
+// 	if ( !defined('WPMU_PLUGIN_DIR') )
+// 		define( 'WPMU_PLUGIN_DIR', WP_CONTENT_DIR . '/mu-plugins' ); // full path, no trailing slash
 
-	/**
-	 * Allows for the mu-plugins directory to be moved from the default location.
-	 *
-	 * @since 2.8.0
-	 */
-	if ( !defined('WPMU_PLUGIN_URL') )
-		define( 'WPMU_PLUGIN_URL', WP_CONTENT_URL . '/mu-plugins' ); // full url, no trailing slash
+// 	/**
+// 	 * Allows for the mu-plugins directory to be moved from the default location.
+// 	 *
+// 	 * @since 2.8.0
+// 	 */
+// 	if ( !defined('WPMU_PLUGIN_URL') )
+// 		define( 'WPMU_PLUGIN_URL', WP_CONTENT_URL . '/mu-plugins' ); // full url, no trailing slash
 
-	/**
-	 * Allows for the mu-plugins directory to be moved from the default location.
-	 *
-	 * @since 2.8.0
-	 * @deprecated
-	 */
-	if ( !defined( 'MUPLUGINDIR' ) )
-		define( 'MUPLUGINDIR', 'wp-content/mu-plugins' ); // Relative to ABSPATH. For back compat.
-}
+// 	/**
+// 	 * Allows for the mu-plugins directory to be moved from the default location.
+// 	 *
+// 	 * @since 2.8.0
+// 	 * @deprecated
+// 	 */
+// 	if ( !defined( 'MUPLUGINDIR' ) )
+// 		define( 'MUPLUGINDIR', 'wp-content/mu-plugins' ); // Relative to ABSPATH. For back compat.
+// }
 
-/**
- * Defines cookie related WordPress constants
- *
- * Defines constants after multisite is loaded. Cookie-related constants may be overridden in ms_network_cookies().
- * @since 3.0.0
- */
-function wp_cookie_constants() {
-	/**
-	 * Used to guarantee unique hash cookies
-	 *
-	 * @since 1.5.0
-	 */
-	if ( !defined( 'COOKIEHASH' ) ) {
-		$siteurl = get_site_option( 'siteurl' );
-		if ( $siteurl )
-			define( 'COOKIEHASH', md5( $siteurl ) );
-		else
-			define( 'COOKIEHASH', '' );
-	}
+// /**
+//  * Defines cookie related WordPress constants
+//  *
+//  * Defines constants after multisite is loaded. Cookie-related constants may be overridden in ms_network_cookies().
+//  * @since 3.0.0
+//  */
+// function wp_cookie_constants() {
+// 	/**
+// 	 * Used to guarantee unique hash cookies
+// 	 *
+// 	 * @since 1.5.0
+// 	 */
+// 	if ( !defined( 'COOKIEHASH' ) ) {
+// 		$siteurl = get_site_option( 'siteurl' );
+// 		if ( $siteurl )
+// 			define( 'COOKIEHASH', md5( $siteurl ) );
+// 		else
+// 			define( 'COOKIEHASH', '' );
+// 	}
 
-	/**
-	 * @since 2.0.0
-	 */
-	if ( !defined('USER_COOKIE') )
-		define('USER_COOKIE', 'wordpressuser_' . COOKIEHASH);
+// 	/**
+// 	 * @since 2.0.0
+// 	 */
+// 	if ( !defined('USER_COOKIE') )
+// 		define('USER_COOKIE', 'wordpressuser_' . COOKIEHASH);
 
-	/**
-	 * @since 2.0.0
-	 */
-	if ( !defined('PASS_COOKIE') )
-		define('PASS_COOKIE', 'wordpresspass_' . COOKIEHASH);
+// 	/**
+// 	 * @since 2.0.0
+// 	 */
+// 	if ( !defined('PASS_COOKIE') )
+// 		define('PASS_COOKIE', 'wordpresspass_' . COOKIEHASH);
 
-	/**
-	 * @since 2.5.0
-	 */
-	if ( !defined('AUTH_COOKIE') )
-		define('AUTH_COOKIE', 'wordpress_' . COOKIEHASH);
+// 	/**
+// 	 * @since 2.5.0
+// 	 */
+// 	if ( !defined('AUTH_COOKIE') )
+// 		define('AUTH_COOKIE', 'wordpress_' . COOKIEHASH);
 
-	/**
-	 * @since 2.6.0
-	 */
-	if ( !defined('SECURE_AUTH_COOKIE') )
-		define('SECURE_AUTH_COOKIE', 'wordpress_sec_' . COOKIEHASH);
+// 	/**
+// 	 * @since 2.6.0
+// 	 */
+// 	if ( !defined('SECURE_AUTH_COOKIE') )
+// 		define('SECURE_AUTH_COOKIE', 'wordpress_sec_' . COOKIEHASH);
 
-	/**
-	 * @since 2.6.0
-	 */
-	if ( !defined('LOGGED_IN_COOKIE') )
-		define('LOGGED_IN_COOKIE', 'wordpress_logged_in_' . COOKIEHASH);
+// 	/**
+// 	 * @since 2.6.0
+// 	 */
+// 	if ( !defined('LOGGED_IN_COOKIE') )
+// 		define('LOGGED_IN_COOKIE', 'wordpress_logged_in_' . COOKIEHASH);
 
-	/**
-	 * @since 2.3.0
-	 */
-	if ( !defined('TEST_COOKIE') )
-		define('TEST_COOKIE', 'wordpress_test_cookie');
+// 	/**
+// 	 * @since 2.3.0
+// 	 */
+// 	if ( !defined('TEST_COOKIE') )
+// 		define('TEST_COOKIE', 'wordpress_test_cookie');
 
-	/**
-	 * @since 1.2.0
-	 */
-	if ( !defined('COOKIEPATH') )
-		define('COOKIEPATH', preg_replace('|https?://[^/]+|i', '', get_option('home') . '/' ) );
+// 	/**
+// 	 * @since 1.2.0
+// 	 */
+// 	if ( !defined('COOKIEPATH') )
+// 		define('COOKIEPATH', preg_replace('|https?://[^/]+|i', '', get_option('home') . '/' ) );
 
-	/**
-	 * @since 1.5.0
-	 */
-	if ( !defined('SITECOOKIEPATH') )
-		define('SITECOOKIEPATH', preg_replace('|https?://[^/]+|i', '', get_option('siteurl') . '/' ) );
+// 	/**
+// 	 * @since 1.5.0
+// 	 */
+// 	if ( !defined('SITECOOKIEPATH') )
+// 		define('SITECOOKIEPATH', preg_replace('|https?://[^/]+|i', '', get_option('siteurl') . '/' ) );
 
-	/**
-	 * @since 2.6.0
-	 */
-	if ( !defined('ADMIN_COOKIE_PATH') )
-		define( 'ADMIN_COOKIE_PATH', SITECOOKIEPATH . 'wp-admin' );
+// 	/**
+// 	 * @since 2.6.0
+// 	 */
+// 	if ( !defined('ADMIN_COOKIE_PATH') )
+// 		define( 'ADMIN_COOKIE_PATH', SITECOOKIEPATH . 'wp-admin' );
 
-	/**
-	 * @since 2.6.0
-	 */
-	if ( !defined('PLUGINS_COOKIE_PATH') )
-		define( 'PLUGINS_COOKIE_PATH', preg_replace('|https?://[^/]+|i', '', WP_PLUGIN_URL)  );
+// 	/**
+// 	 * @since 2.6.0
+// 	 */
+// 	if ( !defined('PLUGINS_COOKIE_PATH') )
+// 		define( 'PLUGINS_COOKIE_PATH', preg_replace('|https?://[^/]+|i', '', WP_PLUGIN_URL)  );
 
-	/**
-	 * @since 2.0.0
-	 */
-	if ( !defined('COOKIE_DOMAIN') )
-		define('COOKIE_DOMAIN', false);
-}
+// 	/**
+// 	 * @since 2.0.0
+// 	 */
+// 	if ( !defined('COOKIE_DOMAIN') )
+// 		define('COOKIE_DOMAIN', false);
+// }
 
-/**
- * Defines cookie related WordPress constants
- *
- * @since 3.0.0
- */
-function wp_ssl_constants() {
-	/**
-	 * @since 2.6.0
-	 */
-	if ( !defined( 'FORCE_SSL_ADMIN' ) ) {
-		if ( 'https' === parse_url( get_option( 'siteurl' ), PHP_URL_SCHEME ) ) {
-			define( 'FORCE_SSL_ADMIN', true );
-		} else {
-			define( 'FORCE_SSL_ADMIN', false );
-		}
-	}
-	force_ssl_admin( FORCE_SSL_ADMIN );
+// /**
+//  * Defines cookie related WordPress constants
+//  *
+//  * @since 3.0.0
+//  */
+// function wp_ssl_constants() {
+// 	/**
+// 	 * @since 2.6.0
+// 	 */
+// 	if ( !defined( 'FORCE_SSL_ADMIN' ) ) {
+// 		if ( 'https' === parse_url( get_option( 'siteurl' ), PHP_URL_SCHEME ) ) {
+// 			define( 'FORCE_SSL_ADMIN', true );
+// 		} else {
+// 			define( 'FORCE_SSL_ADMIN', false );
+// 		}
+// 	}
+// 	force_ssl_admin( FORCE_SSL_ADMIN );
 
-	/**
-	 * @since 2.6.0
-	 * @deprecated 4.0.0
-	 */
-	if ( defined( 'FORCE_SSL_LOGIN' ) && FORCE_SSL_LOGIN ) {
-		force_ssl_admin( true );
-	}
-}
+// 	/**
+// 	 * @since 2.6.0
+// 	 * @deprecated 4.0.0
+// 	 */
+// 	if ( defined( 'FORCE_SSL_LOGIN' ) && FORCE_SSL_LOGIN ) {
+// 		force_ssl_admin( true );
+// 	}
+// }
 
-/**
- * Defines functionality related WordPress constants
- *
- * @since 3.0.0
- */
-function wp_functionality_constants() {
-	/**
-	 * @since 2.5.0
-	 */
-	if ( !defined( 'AUTOSAVE_INTERVAL' ) )
-		define( 'AUTOSAVE_INTERVAL', 60 );
+// /**
+//  * Defines functionality related WordPress constants
+//  *
+//  * @since 3.0.0
+//  */
+// function wp_functionality_constants() {
+// 	/**
+// 	 * @since 2.5.0
+// 	 */
+// 	if ( !defined( 'AUTOSAVE_INTERVAL' ) )
+// 		define( 'AUTOSAVE_INTERVAL', 60 );
 
-	/**
-	 * @since 2.9.0
-	 */
-	if ( !defined( 'EMPTY_TRASH_DAYS' ) )
-		define( 'EMPTY_TRASH_DAYS', 30 );
+// 	/**
+// 	 * @since 2.9.0
+// 	 */
+// 	if ( !defined( 'EMPTY_TRASH_DAYS' ) )
+// 		define( 'EMPTY_TRASH_DAYS', 30 );
 
-	if ( !defined('WP_POST_REVISIONS') )
-		define('WP_POST_REVISIONS', true);
+// 	if ( !defined('WP_POST_REVISIONS') )
+// 		define('WP_POST_REVISIONS', true);
 
-	/**
-	 * @since 3.3.0
-	 */
-	if ( !defined( 'WP_CRON_LOCK_TIMEOUT' ) )
-		define('WP_CRON_LOCK_TIMEOUT', 60);  // In seconds
-}
+// 	/**
+// 	 * @since 3.3.0
+// 	 */
+// 	if ( !defined( 'WP_CRON_LOCK_TIMEOUT' ) )
+// 		define('WP_CRON_LOCK_TIMEOUT', 60);  // In seconds
+// }
 
-/**
- * Defines templating related WordPress constants
- *
- * @since 3.0.0
- */
-function wp_templating_constants() {
-	/**
-	 * Filesystem path to the current active template directory
-	 * @since 1.5.0
-	 */
-	define('TEMPLATEPATH', get_template_directory());
+// /**
+//  * Defines templating related WordPress constants
+//  *
+//  * @since 3.0.0
+//  */
+// function wp_templating_constants() {
+// 	/**
+// 	 * Filesystem path to the current active template directory
+// 	 * @since 1.5.0
+// 	 */
+// 	define('TEMPLATEPATH', get_template_directory());
 
-	/**
-	 * Filesystem path to the current active template stylesheet directory
-	 * @since 2.1.0
-	 */
-	define('STYLESHEETPATH', get_stylesheet_directory());
+// 	/**
+// 	 * Filesystem path to the current active template stylesheet directory
+// 	 * @since 2.1.0
+// 	 */
+// 	define('STYLESHEETPATH', get_stylesheet_directory());
 
-	/**
-	 * Slug of the default theme for this install.
-	 * Used as the default theme when installing new sites.
-	 * Will be used as the fallback if the current theme doesn't exist.
-	 * @since 3.0.0
-	 */
-	if ( !defined('WP_DEFAULT_THEME') )
-		define( 'WP_DEFAULT_THEME', 'twentyfourteen' );
+// 	/**
+// 	 * Slug of the default theme for this install.
+// 	 * Used as the default theme when installing new sites.
+// 	 * Will be used as the fallback if the current theme doesn't exist.
+// 	 * @since 3.0.0
+// 	 */
+// 	if ( !defined('WP_DEFAULT_THEME') )
+// 		define( 'WP_DEFAULT_THEME', 'twentyfourteen' );
 
-}
+// }
