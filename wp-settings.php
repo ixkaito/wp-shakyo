f266cf6347e936cee7b876d9eb9a8451fd940c82
Kite <ixkaito@gmail.com>
2016-09-14 11:01:31 +0900

    Add wp-settings.php

---
+++ wp-settings.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Used to set up and fix common variables and include
+ * the WordPress procedural and class library.
+ *
+ * Allows for some configuration in wp-config.php (see default-constants.php)
+ *
+ * @internal This file must be parsable by PHP4.
+ *
+ * @package WordPress
+ */
+
+/**
+ * Stores the location of the WordPress directory of functions, classes, and core content.
+ *
+ * @since 1.0.0
+ */
+define( 'WPINC', 'wp-includes' );
+
+var_dump(__FILE__);
