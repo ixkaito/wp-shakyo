0b0899c826e7369f062c51aa82f7edfc00909378
Kite <ixkaito@gmail.com>
2016-09-15 19:09:52 +0900

    Finish wp-load.php

--- wp-load.php
+++ wp-load.php
@@ -27,6 +27,47 @@ if ( file_exists( ABSPATH . 'wp-config.php') ) {
 
 	/** The config file resides in ABSPATH */
 	require_once( ABSPATH . 'wp-config.php' );
-}
 
-var_dump(__FILE__);
+} elseif ( file_exists( dirname(ABSPATH) . '/wp-config.php' ) && ! file_exists( dirname(ABSPATH) . '/wp-settings.php' ) ) {
+
+	/** The config file resides one level above ABSPATH but is not part of another install */
+	require_once( dirname(ABSPATH) . '/wp-config.php' );
+
+} else {
+
+	// A config file doesn't exist
+
+	define( 'WPINC', 'wp-includes' );
+	require_once( ABSPATH . WPINC . '/load.php' );
+
+	// Standardize $_SERVER variables across setups.
+	wp_fix_server_vars();
+
+	require_once( ABSPATH . WPINC . '/functions.php' );
+
+	$path = wp_guess_url() . '/wp-admin/setup-config.php';
+
+	/*
+	 * We're going to redirect to setup-config.php. While this shouldn't result
+	 * in an infinite loop, that's a silly thing to assume, don't you think? If
+	 * we're traveling in circles, our last-ditch effort is "Need more help?"
+	 */
+	if ( false === strpos( $_SERVER['REQUEST_URI'], 'setup-config' ) ) {
+		header( 'Location: ' . $path );
+		exit;
+	}
+
+	define( 'WP_CONTENT_DIR', ABSPATH . 'wp-content' );
+	require_once( ABSPATH . WPINC . '/version.php' );
+
+	wp_check_php_mysql_versions();
+	wp_load_translations_early();
+
+	// Die with an error message
+	$die  = __( "There doesn't seem to be a <code>wp-config.php</code> file. I need this before we can get started." ) . '</p>';
+	$die .= '<p>' . __( "Need more help? <a href='http://codex.wordpress.org/Editing_wp-config.php'>We got it</a>." ) . '</p>';
+	$die .= '<p>' . __( "You can create a <code>wp-config.php</code> file through a web interface, but this doesn't work for all server setups. The safest way is to manually create the file." ) . '</p>';
+	$die .= '<p><a href="' . $path . '" class="button button-large">' . __( "Create a Configuration File" ) . '</a>';
+
+	wp_die( $die, __( 'WordPress &rsaquo; Error' ) );
+}
