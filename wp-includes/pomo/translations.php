31dbd65d9f4462363337196df7c2a69a4cccac54
Kite <ixkaito@gmail.com>
2016-09-17 02:37:33 +0900

    Add wp-includes/pomo/translations.php

---
+++ wp-includes/pomo/translations.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Class for a set of entries for translation and their associated headers
+ *
+ * @version $Id: translations.php 718 2012-10-31 00:32:02Z nbachiyski $
+ * @package pomo
+ * @subpackage translations
+ */
