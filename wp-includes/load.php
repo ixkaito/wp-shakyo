8fa8ad22c05999059d35db06c353ed5c318d50ed
Kite <ixkaito@gmail.com>
2016-09-16 13:58:41 +0900

    Add `is_multisite()`

--- wp-includes/load.php
+++ wp-includes/load.php
@@ -6,3 +6,20 @@
  *
  * @package WordPress
  */
+
+/**
+ * If Multisite is enabled.
+ *
+ * @since 3.0.0
+ *
+ * @return bool True if Multisite is enabled, false otherwise.
+ */
+function is_multisite() {
+	if ( defined( 'MULTISITE' ) )
+		return MULTISITE;
+
+	if ( defined( 'SUBDOMAIN_INSTALL' ) || defined( 'VHOST' ) || defined( 'SUNRISE' ) )
+		return true;
+
+	return false;
+}
