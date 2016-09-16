9df10f14044b9214899782e0fd675784008badad
Kite <ixkaito@gmail.com>
2016-09-17 01:35:48 +0900

    Add `wp_set_lang_dir()`

--- wp-includes/load.php
+++ wp-includes/load.php
@@ -263,6 +263,51 @@ function wp_debug_mode() {
 }
 
 /**
+ * Set the location of the language directory.
+ *
+ * To set directory manually, define the `WP_LANG_DIR` constant
+ * in wp-config.php.
+ *
+ * If the language directory exists within `WP_CONTENT_DIR`, it
+ * is used. Otherwise the language directory is assumed to live
+ * in `WPINC`.
+ *
+ * @since 3.0.0
+ * @access private
+ */
+function wp_set_lang_dir() {
+	if ( !defined( 'WP_LANG_DIR' ) ) {
+		if ( file_exists( WP_CONTENT_DIR . '/languages' ) && @is_dir( WP_CONTENT_DIR . '/languages' ) || !@is_dir(ABSPATH . WPINC . '/languages') ) {
+			/**
+			 * Server path of the language directory.
+			 *
+			 * No leading slash, no trailing slash, full path, not relative to ABSPATH
+			 *
+			 * @since 2.1.0
+			 */
+			define( 'WP_LANG_DIR', WP_CONTENT_DIR . '/languages' );
+			if ( !defined( 'LANGDIR' ) ) {
+				// Old static relative path maintained for limited backwards compatibility - won't work in some cases
+				define( 'LANGDIR', 'wp-content/languages' );
+			}
+		} else {
+			/**
+			 * Server path of the language directory.
+			 *
+			 * No leading slash, no trailing slash, full path, not relative to `ABSPATH`.
+			 *
+			 * @since 2.1.0
+			 */
+			define( 'WP_LANG_DIR', ABSPATH . WPINC . '/languages' );
+			if ( !defined( 'LANGDIR' ) ) {
+				// Old relative path maintained for backwards compatibility
+				define( 'LANGDIR', WPINC . '/languages' );
+			}
+		}
+	}
+}
+
+/**
  * If Multisite is enabled.
  *
  * @since 3.0.0
