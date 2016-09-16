9bde72c5979f4e895f773a5b7395d8eb664589fd
Kite <ixkaito@gmail.com>
2016-09-16 14:27:31 +0900

    Add `wp_favicon_request()`

--- wp-includes/load.php
+++ wp-includes/load.php
@@ -125,6 +125,21 @@ function wp_check_php_mysql_versions() {
 }
 
 /**
+ * Don't load all of WordPress when handling a favicon.ico request.
+ *
+ * Instead, send the headers for a zero-length favicon and bail.
+ *
+ * @since 3.0.0
+ */
+function wp_favicon_request() {
+	if ( '/favicon.ico' == $_SERVER['REQUEST_URI'] ) {
+		header('Content-Type: image/vnd.microsoft.icon');
+		header('Content-Length: 0');
+		exit;
+	}
+}
+
+/**
  * If Multisite is enabled.
  *
  * @since 3.0.0
