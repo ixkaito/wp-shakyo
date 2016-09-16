a2ca075084062339d93ffb4a39bb272990213dc1
Kite <ixkaito@gmail.com>
2016-09-17 01:39:51 +0900

    If gettext isn't available

--- wp-includes/compat.php
+++ wp-includes/compat.php
@@ -5,3 +5,16 @@
  * @package PHP
  * @access private
  */
+
+// If gettext isn't available
+if ( !function_exists('_') ) {
+	function _($string) {
+		return $string;
+	}
+}
+
+if ( !function_exists('mb_substr') ):
+	function mb_substr( $str, $start, $length=null, $encoding=null ) {
+		return _mb_substr($str, $start, $length, $encoding);
+	}
+endif;
