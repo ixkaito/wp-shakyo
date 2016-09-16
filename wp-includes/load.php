32bd46a18d0a0402ef1e407dd41b091ce447cf8b
Kite <ixkaito@gmail.com>
2016-09-16 14:20:12 +0900

    Add `wp_unregister_GLOBALS()`

--- wp-includes/load.php
+++ wp-includes/load.php
@@ -8,6 +8,31 @@
  */
 
 /**
+ * Turn register globals off.
+ *
+ * @since 2.1.0
+ * @access private
+ *
+ * @return null Will return null if register_globals PHP directive was disabled.
+ */
+function wp_unregister_GLOBALS() {
+	if ( !ini_get( 'register_globals' ) )
+		return;
+
+	if ( isset( $_REQUEST['GLOBALS'] ) )
+		die( 'GLOBALS overwrite attempt detected' );
+
+	// Variables that shouldn't be unset
+	$no_unset = array( 'GLOBALS', '_GET', '_POST', '_COOKIE', '_REQUEST', '_SERVER', '_ENV', '_FILES', 'table_prefix' );
+
+	$input = array_merge( $_GET, $_POST, $_COOKIE, $_SERVER, $_ENV, $_FILES, isset( $_SESSION ) && is_array( $_SESSION ) ? $_SESSION : array() );
+	foreach ( $input as $k => $v )
+		if ( !in_array( $k, $no_unset ) && isset( $GLOBALS[$k] ) ) {
+			unset( $GLOBALS[$k] );
+		}
+}
+
+/**
  * Check for the required PHP version, and the MySQL extension or
  * a database drop-in.
  *
