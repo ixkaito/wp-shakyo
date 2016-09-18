8bec34b3b61ad9d99cf014a69b13630363305bd0
Kite <ixkaito@gmail.com>
2016-09-18 12:44:52 +0900

    Add `wpdb` class

--- wp-includes/wp-db.php
+++ wp-includes/wp-db.php
@@ -34,3 +34,19 @@ define( 'ARRAY_A', 'ARRAY_A' );
  * @since 0.71
  */
 define( 'ARRAY_N', 'ARRAY_N' );
+
+/**
+ * WordPress Database Access Abstraction Object
+ *
+ * It is possible to replace this class with your own
+ * by setting the $wpdb global variable in wp-content/db.php
+ * file to your class. The wpdb class will still be included,
+ * so you can extend it or simply use your own.
+ *
+ * @link http://codex.wordpress.org/Function_Reference/wpdb_Class
+ *
+ * @package WordPress
+ * @subpackage Database
+ * @since 0.71
+ */
+class wpdb {}
