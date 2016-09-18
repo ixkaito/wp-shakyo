4777679aa5556eef900100f8455b37ced7835834
Kite <ixkaito@gmail.com>
2016-09-18 12:42:50 +0900

    Define some constants in wp-includes/wp-db.php

--- wp-includes/wp-db.php
+++ wp-includes/wp-db.php
@@ -8,3 +8,29 @@
  * @subpackage Database
  * @since 0.71
  */
+
+/**
+ * @since 0.71
+ */
+define( 'EZSQL_VERSION', 'WP1.25' );
+
+/**
+ * @since 0.71
+ */
+define( 'OBJECT', 'OBJECT' );
+define( 'object', 'OBJECT' ); // Back compat.
+
+/**
+ * @since 2.5.0
+ */
+define( 'OBJECT_K', 'OBJECT_K' );
+
+/**
+ * @since 0.71
+ */
+define( 'ARRAY_A', 'ARRAY_A' );
+
+/**
+ * @since 0.71
+ */
+define( 'ARRAY_N', 'ARRAY_N' );
