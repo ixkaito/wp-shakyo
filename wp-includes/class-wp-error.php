ec091735a97258cf3511d1379d258cd613d2e42d
Kite <ixkaito@gmail.com>
2016-09-19 17:44:36 +0900

    Add `is_wp_error()`

--- wp-includes/class-wp-error.php
+++ wp-includes/class-wp-error.php
@@ -6,3 +6,19 @@
  *
  * @package WordPress
  */
+
+/**
+ * Check whether variable is a WordPress Error.
+ *
+ * Returns true if $thing is an object of the WP_Error class.
+ *
+ * @since 2.1.0
+ *
+ * @param mixed $thing Check if unknown variable is a WP_Error object.
+ * @return bool True, if WP_Error. False, if not WP_Error.
+ */
+function is_wp_error($thing) {
+	if ( is_object($thing) && is_a($thing, 'WP_Error') )
+		return true;
+	return false;
+}
