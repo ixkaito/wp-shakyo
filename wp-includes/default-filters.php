a1f0acc2e8d982f8a61cdd3815d088d556dd5140
Kite <ixkaito@gmail.com>
2016-09-21 00:29:29 +0900

    Add wp-includes/default-filters.php

---
+++ wp-includes/default-filters.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Sets up the default filters and actions for most
+ * of the WordPress hooks.
+ *
+ * If you need to remove a default hook, this file will
+ * give you the priority for which to use to remove the
+ * hook.
+ *
+ * Not all of the default hooks are found in default-filters.php
+ *
+ * @package WordPress
+ */
