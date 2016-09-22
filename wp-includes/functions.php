fbf58a8d27204d2cfdd77046b80fc3a8338fc7ee
Kite <ixkaito@gmail.com>
2016-09-22 17:35:26 +0900

    Add `wp_ob_end_flush_all()`

--- wp-includes/functions.php
+++ wp-includes/functions.php
@@ -6,3 +6,16 @@
  */
 
 require( ABSPATH . WPINC . '/option.php' );
+
+/**
+ * Flush all output buffers for PHP 5.2.
+ *
+ * Make sure all output buffers are flushed before our singletons are destroyed.
+ *
+ * @since 2.2.0
+ */
+function wp_ob_end_flush_all() {
+	$levels = ob_get_level();
+	for ($i=0; $i<$levels; $i++)
+		ob_end_flush();
+}
