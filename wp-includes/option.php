edf73aa2b3f4404243baa771e52e100e8b64277c
Kite <ixkaito@gmail.com>
2016-09-27 00:22:54 +0900

    Add `wp_load_alloptions()`

--- wp-includes/option.php
+++ wp-includes/option.php
@@ -5,3 +5,34 @@
  * @package WordPress
  * @subpackage Option
  */
+
+/**
+ * Loads and caches all autoloaded options, if available or all options.
+ *
+ * @since 2.2.0
+ *
+ * @return array List of all options.
+ */
+function wp_load_alloptions() {
+	global $wpdb;
+
+	if ( !defined( 'WP_INSTALLING' ) || !is_multisite() )
+		$alloptions = wp_cache_get( 'alloptions', 'options' );
+	else
+		$alloptions = false;
+
+	if ( !$alloptions ) {
+		$suppress = $wpdb->suppress_errors();
+		if ( !$alloptions_db = $wpdb->get_results( "SELECT option_name, option_value FROM $wpdb->options WHERE autoload = 'yes'" ) )
+			$alloptions_db = $wpdb->get_results( "SELECT option_name, option_value FROM $wpdb->options" );
+		$wpdb->suppress_errors($suppress);
+		$alloptions = array();
+		foreach ( (array) $alloptions_db as $o ) {
+			$alloptions[$o->option_name] = $o->option_value;
+		}
+		if ( !defined( 'WP_INSTALLING' ) || !is_multisite() )
+			wp_cache_add( 'alloptions', $alloptions, 'options' );
+	}
+
+	return $alloptions;
+}
