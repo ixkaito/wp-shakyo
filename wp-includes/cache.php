59ca60b2f70ddddf655e15f3c308d1715e8ac4bf
Kite <ixkaito@gmail.com>
2016-09-28 02:37:53 +0900

    `wp_cache_add()`

--- wp-includes/cache.php
+++ wp-includes/cache.php
@@ -9,6 +9,25 @@
  */
 
 /**
+ * Adds data to the cache, if the cache key doesn't already exist.
+ *
+ * @since 2.0.0
+ * @uses $wp_object_cache Object Cache Class
+ * @see WP_Object_Cache::add()
+ *
+ * @param int|string $key The cache key to use for retrieval later
+ * @param mixed $data The data to add to the cache store
+ * @param string $group The group to add the cache to
+ * @param int $expire When the cache data should be expired
+ * @return bool False if cache key and group already exist, true on success
+ */
+function wp_cache_add( $key, $data, $group = '', $expire = 0 ) {
+	global $wp_object_cache;
+
+	return $wp_object_cache->add( $key, $data, $group, (int) $expire );
+}
+
+/**
  * Closes the cache.
  *
  * This function has ceased to do anything since WordPress 2.5. The
