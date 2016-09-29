64db57a270790efab0cb176660afe37784c94a0c
Kite <ixkaito@gmail.com>
2016-09-29 17:00:54 +0900

    Add `is_ssl()`

--- wp-includes/functions.php
+++ wp-includes/functions.php
@@ -174,6 +174,25 @@ function wp_ob_end_flush_all() {
 }
 
 /**
+ * Determine if SSL is used.
+ *
+ * @since 2.6.0
+ *
+ * @return bool True if SSL, false if not used.
+ */
+function is_ssl() {
+	if ( isset($_SERVER['HTTPS']) ) {
+		if ( 'on' == strtolower($_SERVER['HTTPS']) )
+			return true;
+		if ( '1' == $_SERVER['HTTPS'] )
+			return true;
+	} elseif ( isset($_SERVER['SERVER_PORT']) && ( '443' == $_SERVER['SERVER_PORT'] ) ) {
+		return true;
+	}
+	return false;
+}
+
+/**
  * Guess the URL for the site.
  *
  * Will remove wp-admin links to retrieve only return URLs not in the wp-admin
