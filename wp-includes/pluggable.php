cee4b0075fef0a7bf26505a0d5d09cc9704cf9af
Kite <ixkaito@gmail.com>
2016-09-29 16:55:30 +0900

    Initialize wp-includes/pluggable.php

--- wp-includes/pluggable.php
+++ wp-includes/pluggable.php
@@ -5,3 +5,118 @@
  *
  * @package WordPress
  */
+
+if ( !function_exists('wp_set_current_user') ) :
+endif;
+
+if ( !function_exists('wp_get_current_user') ) :
+endif;
+
+if ( !function_exists('get_currentuserinfo') ) :
+endif;
+
+if ( !function_exists('get_userdata') ) :
+endif;
+
+if ( !function_exists('get_user_by') ) :
+endif;
+
+if ( !function_exists('cache_users') ) :
+endif;
+
+if ( !function_exists( 'wp_mail' ) ) :
+endif;
+
+if ( !function_exists('wp_authenticate') ) :
+endif;
+
+if ( !function_exists('wp_logout') ) :
+endif;
+
+if ( !function_exists('wp_validate_auth_cookie') ) :
+endif;
+
+if ( !function_exists('wp_generate_auth_cookie') ) :
+endif;
+
+if ( !function_exists('wp_parse_auth_cookie') ) :
+endif;
+
+if ( !function_exists('wp_set_auth_cookie') ) :
+endif;
+
+if ( !function_exists('wp_clear_auth_cookie') ) :
+endif;
+
+if ( !function_exists('is_user_logged_in') ) :
+endif;
+
+if ( !function_exists('auth_redirect') ) :
+endif;
+
+if ( !function_exists('check_admin_referer') ) :
+endif;
+
+if ( !function_exists('check_ajax_referer') ) :
+endif;
+
+if ( !function_exists('wp_redirect') ) :
+endif;
+
+if ( !function_exists('wp_sanitize_redirect') ) :
+endif;
+
+if ( !function_exists('wp_safe_redirect') ) :
+endif;
+
+if ( !function_exists('wp_validate_redirect') ) :
+endif;
+
+if ( ! function_exists('wp_notify_postauthor') ) :
+endif;
+
+if ( !function_exists('wp_notify_moderator') ) :
+endif;
+
+if ( !function_exists('wp_password_change_notification') ) :
+endif;
+
+if ( !function_exists('wp_new_user_notification') ) :
+endif;
+
+if ( !function_exists('wp_nonce_tick') ) :
+endif;
+
+if ( !function_exists('wp_verify_nonce') ) :
+endif;
+
+if ( !function_exists('wp_create_nonce') ) :
+endif;
+
+if ( !function_exists('wp_salt') ) :
+endif;
+
+if ( !function_exists('wp_hash') ) :
+endif;
+
+if ( !function_exists('wp_hash_password') ) :
+endif;
+
+if ( !function_exists('wp_check_password') ) :
+endif;
+
+if ( !function_exists('wp_generate_password') ) :
+endif;
+
+if ( !function_exists('wp_rand') ) :
+endif;
+
+if ( !function_exists('wp_set_password') ) :
+endif;
+
+if ( !function_exists( 'get_avatar' ) ) :
+endif;
+
+if ( !function_exists( 'wp_text_diff' ) ) :
+endif;
+
