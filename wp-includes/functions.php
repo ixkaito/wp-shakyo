021819d677266877be91914985bd72a60cb1c59b
Kite <ixkaito@gmail.com>
2016-09-30 20:20:01 +0900

    Add `absint()`

--- wp-includes/functions.php
+++ wp-includes/functions.php
@@ -294,6 +294,18 @@ function wp_ob_end_flush_all() {
 }
 
 /**
+ * Convert a value to non-negative integer.
+ *
+ * @since 2.5.0
+ *
+ * @param mixed $maybeint Data you wish to have converted to a non-negative integer.
+ * @return int A non-negative integer.
+ */
+function absint( $maybeint ) {
+	return abs( intval( $maybeint ) );
+}
+
+/**
  * Determine if SSL is used.
  *
  * @since 2.6.0
