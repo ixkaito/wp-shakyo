b2a554e54b057f040af67dd4bb05f418778973cf
Kite <ixkaito@gmail.com>
2016-10-02 07:14:47 +0900

    Add actions in wp-includes/theme.php

--- wp-includes/theme.php
+++ wp-includes/theme.php
@@ -5,3 +5,13 @@
  * @package WordPress
  * @subpackage Theme
  */
+
+add_action('setup_theme', 'preview_theme');
+
+add_action( 'wp_loaded', '_custom_header_background_just_in_time' );
+
+add_action( 'delete_attachment', '_delete_attachment_theme_mod' );
+
+add_action( 'plugins_loaded', '_wp_customize_include' );
+
+add_action( 'admin_enqueue_scripts', '_wp_customize_loader_settings' );
