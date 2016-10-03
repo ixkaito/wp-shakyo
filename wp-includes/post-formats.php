6d8846eaaf197a7970322c85c3d9bf783fd4e412
Kite <ixkaito@gmail.com>
2016-10-03 23:46:04 +0900

    Add filters in wp-includes/post-formats.php

--- wp-includes/post-formats.php
+++ wp-includes/post-formats.php
@@ -5,3 +5,13 @@
  * @package WordPress
  * @subpackage Post
  */
+
+add_filter( 'request', '_post_format_request' );
+
+add_filter( 'term_link', '_post_format_link', 10, 3 );
+
+add_filter( 'get_post_format', '_post_format_get_term' );
+
+add_filter( 'get_terms', '_post_format_get_terms', 10, 3 );
+
+add_filter( 'wp_get_object_terms', '_post_format_wp_get_object_terms' );
