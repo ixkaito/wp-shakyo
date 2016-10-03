d2ea278ecfeb968674719bfde83f1af830ecacaf
Kite <ixkaito@gmail.com>
2016-10-03 23:30:38 +0900

    Add action in wp-includes/general-template.php

--- wp-includes/general-template.php
+++ wp-includes/general-template.php
@@ -5,3 +5,8 @@
  * @package WordPress
  * @subpackage Template
  */
+
+add_action( 'save_post', 'delete_get_calendar_cache' );
+add_action( 'delete_post', 'delete_get_calendar_cache' );
+add_action( 'update_option_start_of_week', 'delete_get_calendar_cache' );
+add_action( 'update_option_gmt_offset', 'delete_get_calendar_cache' );
