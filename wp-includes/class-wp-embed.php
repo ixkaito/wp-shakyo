d1394068d80a0d6830fc5383d9dc2e28456ce8c9
Kite <ixkaito@gmail.com>
2016-10-05 04:56:22 +0900

    Init wp-includes/class-wp-embed.php

---
+++ wp-includes/class-wp-embed.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * API for easily embedding rich media such as videos and images into content.
+ *
+ * @package WordPress
+ * @subpackage Embed
+ * @since 2.9.0
+ */
+
+$GLOBALS['wp_embed'] = new WP_Embed();
