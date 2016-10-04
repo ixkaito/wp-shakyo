d077823323b431b61e869ceb6150ac9eeb3d3703
Kite <ixkaito@gmail.com>
2016-10-05 04:24:05 +0900

    Add wp-includes/class.wp-styles.php

---
+++ wp-includes/class.wp-styles.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * BackPress Styles enqueue.
+ *
+ * These classes were refactored from the WordPress WP_Scripts and WordPress
+ * script enqueue API.
+ *
+ * @package BackPress
+ * @since r74
+ */
