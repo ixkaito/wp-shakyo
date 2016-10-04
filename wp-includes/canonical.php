48986c3f76a2d1e5af2836f2727dbbfecdffa728
Kite <ixkaito@gmail.com>
2016-10-05 04:30:55 +0900

    Add wp-includes/canonical.php

---
+++ wp-includes/canonical.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Canonical API to handle WordPress Redirecting
+ *
+ * Based on "Permalink Redirect" from Scott Yang and "Enforce www. Preference"
+ * by Mark Jaquith
+ *
+ * @package WordPress
+ * @since 2.3.0
+ */
