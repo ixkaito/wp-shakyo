8b0789535d4e398a6c6f153318b7b0e5018cce1d
Kite <ixkaito@gmail.com>
2016-10-06 04:55:56 +0900

    Add actions and shortcodes in wp-includes/media.php

--- wp-includes/media.php
+++ wp-includes/media.php
@@ -5,3 +5,18 @@
  * @package WordPress
  * @subpackage Media
  */
+
+add_shortcode('wp_caption', 'img_caption_shortcode');
+add_shortcode('caption', 'img_caption_shortcode');
+
+add_shortcode('gallery', 'gallery_shortcode');
+
+add_action( 'wp_playlist_scripts', 'wp_playlist_scripts' );
+
+add_shortcode( 'playlist', 'wp_playlist_shortcode' );
+
+add_shortcode( 'audio', 'wp_audio_shortcode' );
+
+add_shortcode( 'video', 'wp_video_shortcode' );
+
+add_action( 'customize_controls_enqueue_scripts', 'wp_plupload_default_settings' );
