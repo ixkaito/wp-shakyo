62a5dc844adb958ade5cbc29da3a48070063a5a9
Kite <ixkaito@gmail.com>
2016-10-06 04:58:36 +0900

    Add wp-includes/widgets.php

---
+++ wp-includes/widgets.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * API for creating dynamic sidebar without hardcoding functionality into
+ * themes. Includes both internal WordPress routines and theme use routines.
+ *
+ * This functionality was found in a plugin before WordPress 2.2 release which
+ * included it in the core from that point on.
+ *
+ * @link http://codex.wordpress.org/Plugins/WordPress_Widgets WordPress Widgets
+ * @link http://codex.wordpress.org/Plugins/WordPress_Widgets_Api Widgets API
+ *
+ * @package WordPress
+ * @subpackage Widgets
+ */
