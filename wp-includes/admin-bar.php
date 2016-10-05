327c6230bb5eaeca0585be9944d496c0446740c6
Kite <ixkaito@gmail.com>
2016-10-06 05:04:30 +0900

    Init wp-includes/admin-bar.php

--- wp-includes/admin-bar.php
+++ wp-includes/admin-bar.php
@@ -4,3 +4,10 @@
  *
  * This code handles the building and rendering of the press bar.
  */
+
+// Don't remove. Wrong way to disable.
+add_action( 'template_redirect', '_wp_admin_bar_init', 0 );
+add_action( 'admin_init', '_wp_admin_bar_init' );
+
+add_action( 'wp_footer', 'wp_admin_bar_render', 1000 );
+add_action( 'in_admin_header', 'wp_admin_bar_render', 0 );
