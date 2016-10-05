d04deadac0d99f7f19170b4041b437634c27ba82
Kite <ixkaito@gmail.com>
2016-10-06 04:56:16 +0900

    Add wp-includes/http.php

---
+++ wp-includes/http.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Simple and uniform HTTP request API.
+ *
+ * Will eventually replace and standardize the WordPress HTTP requests made.
+ *
+ * @link http://trac.wordpress.org/ticket/4779 HTTP API Proposal
+ *
+ * @package WordPress
+ * @subpackage HTTP
+ * @since 2.7.0
+ */
