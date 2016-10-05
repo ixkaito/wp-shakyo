bb8afa41b4b7672f14ab01b81fed2ce9811d14f4
Kite <ixkaito@gmail.com>
2016-10-06 04:57:25 +0900

    Add wp-includes/class-http.php

---
+++ wp-includes/class-http.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Simple and uniform HTTP request API.
+ *
+ * Standardizes the HTTP requests for WordPress. Handles cookies, gzip encoding and decoding, chunk
+ * decoding, if HTTP 1.1 and various other difficult HTTP protocol implementations.
+ *
+ * @link http://trac.wordpress.org/ticket/4779 HTTP API Proposal
+ *
+ * @package WordPress
+ * @subpackage HTTP
+ * @since 2.7.0
+ */
