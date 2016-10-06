03c98829b79c1a77d7da20cbc8c3b55b01dba0e3
Kite <ixkaito@gmail.com>
2016-10-07 05:30:45 +0900

    Add `get_translations_for_domain()`

--- wp-includes/l10n.php
+++ wp-includes/l10n.php
@@ -47,3 +47,21 @@ function translate( $text, $domain = 'default' ) {
 function __( $text, $domain = 'default' ) {
 	return translate( $text, $domain );
 }
+
+/**
+ * Return the Translations instance for a text domain.
+ *
+ * If there isn't one, returns empty Translations instance.
+ *
+ * @since 2.8.0
+ *
+ * @param string $domain Text domain. Unique identifier for retrieving translated strings.
+ * @return Translations A Translations instance.
+ */
+function get_translations_for_domain( $domain ) {
+	global $l10n;
+	if ( !isset( $l10n[$domain] ) ) {
+		$l10n[$domain] = new NOOP_Translations;
+	}
+	return $l10n[$domain];
+}
