b01fa183c33fef8b78f8a9dfd90a3625d0595b5d
Kite <ixkaito@gmail.com>
2016-10-07 05:19:39 +0900

    Add `__()`

--- wp-includes/l10n.php
+++ wp-includes/l10n.php
@@ -5,3 +5,17 @@
  * @package WordPress
  * @subpackage i18n
  */
+
+/**
+ * Retrieve the translation of $text. If there is no translation,
+ * or the text domain isn't loaded, the original text is returned.
+ *
+ * @since 2.1.0
+ *
+ * @param string $text   Text to translate.
+ * @param string $domain Optional. Text domain. Unique identifier for retrieving translated strings.
+ * @return string Translated text.
+ */
+function __( $text, $domain = 'default' ) {
+	return translate( $text, $domain );
+}
