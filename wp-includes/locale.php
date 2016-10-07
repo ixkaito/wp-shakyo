21929bfb501ddb4b3ce32853480884ece1b6500b
Kite <ixkaito@gmail.com>
2016-10-08 07:46:09 +0900

    Add `is_rtl()`

--- wp-includes/locale.php
+++ wp-includes/locale.php
@@ -5,3 +5,14 @@
  * @package WordPress
  * @subpackage i18n
  */
+
+/**
+ * Checks if current locale is RTL.
+ *
+ * @since 3.0.0
+ * @return bool Whether locale is RTL.
+ */
+function is_rtl() {
+	global $wp_locale;
+	return $wp_locale->is_rtl();
+}
