f061d7c0aa4959b78062fd638c3e5adf9a1b286c
Kite <ixkaito@gmail.com>
2016-10-18 00:40:22 +0900

    Add `_x()`

--- wp-includes/l10n.php
+++ wp-includes/l10n.php
@@ -61,6 +61,26 @@ function _e( $text, $domain = 'default' ) {
 }
 
 /**
+ * Retrieve translated string with gettext context.
+ *
+ * Quite a few times, there will be collisions with similar translatable text
+ * found in more than two places, but with different translated context.
+ *
+ * By including the context in the pot file, translators can translate the two
+ * strings differently.
+ *
+ * @since 2.8.0
+ *
+ * @param string $text    Text to translate.
+ * @param string $context Context information for the translators.
+ * @param string $domain  Optional. Text domain. Unique identifier for retrieving translated strings.
+ * @return string Translated context string without pipe.
+ */
+function _x( $text, $context, $domain = 'default' ) {
+	return translate_with_gettext_context( $text, $context, $domain );
+}
+
+/**
  * Return the Translations instance for a text domain.
  *
  * If there isn't one, returns empty Translations instance.
