afd85fbcb697f0548b9c003a864f41c2768ec2fa
Kite <ixkaito@gmail.com>
2016-10-20 09:26:06 +0900

    Initialize wp-includes/ms-deprecated.php

---
+++ wp-includes/ms-deprecated.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Deprecated functions from WordPress MU and the multisite feature. You shouldn't
+ * use these functions and look for the alternatives instead. The functions will be
+ * removed in a later version.
+ *
+ * @package WordPress
+ * @subpackage Deprecated
+ * @since 3.0.0
+ */
+
+/*
+ * Deprecated functions come here to die.
+ */
