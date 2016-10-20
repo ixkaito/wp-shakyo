dd8202279582dc398f6bf8f4fe5b27ce68b33d53
Kite <ixkaito@gmail.com>
2016-10-21 02:32:47 +0900

    Add `maybe_unserialize()`

--- wp-includes/functions.php
+++ wp-includes/functions.php
@@ -8,6 +8,20 @@
 require( ABSPATH . WPINC . '/option.php' );
 
 /**
+ * Unserialize value only if it was serialized.
+ *
+ * @since 2.0.0
+ *
+ * @param string $original Maybe unserialized original, if is needed.
+ * @return mixed Unserialized data can be any type.
+ */
+function maybe_unserialize( $original ) {
+	if ( is_serialized( $original ) ) // don't attempt to unserialize data that wasn't serialized going in
+		return @unserialize( $original );
+	return $original;
+}
+
+/**
  * Retrieve the description for the HTTP status.
  *
  * @since 2.3.0
