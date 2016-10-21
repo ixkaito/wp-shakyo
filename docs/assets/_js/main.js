9b53b440b5bd45d497094048e2c391b7e2b85ddc
Kite <ixkaito@gmail.com>
2016-10-22 08:54:09 +0900

    Add toggle all button

--- docs/assets/_js/main.js
+++ docs/assets/_js/main.js
@@ -10,3 +10,7 @@ $('blockquote').prev('p').append('<i class="fa fa-caret-down toggler"></i>');
 $('.toggler').on('click', function() {
   $(this).toggleClass('fa-caret-up').parent().next('blockquote').toggle();
 });
+
+$('.js-toggle-all').on('click', function() {
+  $('p + blockquote').show();
+});
