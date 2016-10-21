77434a31c17e6bbfb2a8365aa076fc001950a28a
Kite <ixkaito@gmail.com>
2016-10-22 07:51:07 +0900

    Make each file and funciton togglable

--- docs/assets/_js/main.js
+++ docs/assets/_js/main.js
@@ -2,3 +2,9 @@
 
 global.jQuery = require('jquery');
 global.$ = jQuery;
+
+$('p + blockquote').hide();
+
+$('blockquote').prev('p').on('click', function() {
+  $(this).next('blockquote').toggle();
+});
