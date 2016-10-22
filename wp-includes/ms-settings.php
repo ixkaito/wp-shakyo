2a69c924c28ba284ee923c9d63d0d8a9cfef747c
Kite <ixkaito@gmail.com>
2016-10-23 08:57:57 +0900

    Init wp-includes/ms-settings.php

---
+++ wp-includes/ms-settings.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Used to set up and fix common variables and include
+ * the Multisite procedural and class library.
+ *
+ * Allows for some configuration in wp-config.php (see ms-default-constants.php)
+ *
+ * @package WordPress
+ * @subpackage Multisite
+ * @since 3.0.0
+ */
