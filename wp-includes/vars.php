202578e133975af2d27e9d96504a5f2e73423145
Kite <ixkaito@gmail.com>
2016-10-26 10:00:12 +0900

    Simple browser detection

--- wp-includes/vars.php
+++ wp-includes/vars.php
@@ -46,3 +46,45 @@ if ( is_admin() ) {
 		$pagenow = 'index.php';
 }
 unset($self_matches);
+
+// Simple browser detection
+$is_lynx = $is_gecko = $is_winIE = $is_macIE = $is_opera = $is_NS4 = $is_safari = $is_chrome = $is_iphone = false;
+
+if ( isset($_SERVER['HTTP_USER_AGENT']) ) {
+	if ( strpos($_SERVER['HTTP_USER_AGENT'], 'Lynx') !== false ) {
+		$is_lynx = true;
+	} elseif ( stripos($_SERVER['HTTP_USER_AGENT'], 'chrome') !== false ) {
+		if ( stripos( $_SERVER['HTTP_USER_AGENT'], 'chromeframe' ) !== false ) {
+			$is_admin = is_admin();
+			/**
+			 * Filter whether Google Chrome Frame should be used, if available.
+			 *
+			 * @since 3.2.0
+			 *
+			 * @param bool $is_admin Whether to use the Google Chrome Frame. Default is the value of is_admin().
+			 */
+			if ( $is_chrome = apply_filters( 'use_google_chrome_frame', $is_admin ) )
+				header( 'X-UA-Compatible: chrome=1' );
+			$is_winIE = ! $is_chrome;
+		} else {
+			$is_chrome = true;
+		}
+	} elseif ( stripos($_SERVER['HTTP_USER_AGENT'], 'safari') !== false ) {
+		$is_safari = true;
+	} elseif ( ( strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== false || strpos($_SERVER['HTTP_USER_AGENT'], 'Trident') !== false ) && strpos($_SERVER['HTTP_USER_AGENT'], 'Win') !== false ) {
+		$is_winIE = true;
+	} elseif ( strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== false && strpos($_SERVER['HTTP_USER_AGENT'], 'Mac') !== false ) {
+		$is_macIE = true;
+	} elseif ( strpos($_SERVER['HTTP_USER_AGENT'], 'Gecko') !== false ) {
+		$is_gecko = true;
+	} elseif ( strpos($_SERVER['HTTP_USER_AGENT'], 'Opera') !== false ) {
+		$is_opera = true;
+	} elseif ( strpos($_SERVER['HTTP_USER_AGENT'], 'Nav') !== false && strpos($_SERVER['HTTP_USER_AGENT'], 'Mozilla/4.') !== false ) {
+		$is_NS4 = true;
+	}
+}
+
+if ( $is_safari && stripos($_SERVER['HTTP_USER_AGENT'], 'mobile') !== false )
+	$is_iphone = true;
+
+$is_IE = ( $is_macIE || $is_winIE );
