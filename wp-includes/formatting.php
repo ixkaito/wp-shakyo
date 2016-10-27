7177dd269af5b617c4b771fcd15497de37437662
Kite <ixkaito@gmail.com>
2016-10-27 20:09:11 +0900

    Add `wp_parse_str()`

--- wp-includes/formatting.php
+++ wp-includes/formatting.php
@@ -31,3 +31,28 @@ function _deep_replace( $search, $subject ) {
 
 	return $subject;
 }
+
+/**
+ * Parses a string into variables to be stored in an array.
+ *
+ * Uses {@link http://www.php.net/parse_str parse_str()} and stripslashes if
+ * {@link http://www.php.net/magic_quotes magic_quotes_gpc} is on.
+ *
+ * @since 2.2.1
+ *
+ * @param string $string The string to be parsed.
+ * @param array $array Variables will be stored in this array.
+ */
+function wp_parse_str( $string, &$array ) {
+	parse_str( $string, $array );
+	if ( get_magic_quotes_gpc() )
+		$array = stripslashes_deep( $array );
+	/**
+	 * Filter the array of variables derived from a parsed string.
+	 *
+	 * @since 2.3.0
+	 *
+	 * @param array $array The array populated with variables.
+	 */
+	$array = apply_filters( 'wp_parse_str', $array );
+}
