16f4e963169cfd24c90fc459323b76d7f08641f3
Kite <ixkaito@gmail.com>
2016-10-28 18:09:53 +0900

    Add `current_theme_supports()`

--- wp-includes/theme.php
+++ wp-includes/theme.php
@@ -10,6 +10,73 @@ add_action('setup_theme', 'preview_theme');
 
 add_action( 'wp_loaded', '_custom_header_background_just_in_time' );
 
+/**
+ * Checks a theme's support for a given feature
+ *
+ * @since 2.9.0
+ * @param string $feature the feature being checked
+ * @return boolean
+ */
+function current_theme_supports( $feature ) {
+	global $_wp_theme_features;
+
+	if ( 'custom-header-uploads' == $feature )
+		return current_theme_supports( 'custom-header', 'uploads' );
+
+	if ( !isset( $_wp_theme_features[$feature] ) )
+		return false;
+
+	// If no args passed then no extra checks need be performed
+	if ( func_num_args() <= 1 )
+		return true;
+
+	$args = array_slice( func_get_args(), 1 );
+
+	switch ( $feature ) {
+		case 'post-thumbnails':
+			// post-thumbnails can be registered for only certain content/post types by passing
+			// an array of types to add_theme_support(). If no array was passed, then
+			// any type is accepted
+			if ( true === $_wp_theme_features[$feature] )  // Registered for all types
+				return true;
+			$content_type = $args[0];
+			return in_array( $content_type, $_wp_theme_features[$feature][0] );
+
+		case 'html5':
+		case 'post-formats':
+			// specific post formats can be registered by passing an array of types to
+			// add_theme_support()
+
+			// Specific areas of HTML5 support *must* be passed via an array to add_theme_support()
+
+			$type = $args[0];
+			return in_array( $type, $_wp_theme_features[$feature][0] );
+
+		case 'custom-header':
+		case 'custom-background' :
+			// specific custom header and background capabilities can be registered by passing
+			// an array to add_theme_support()
+			$header_support = $args[0];
+			return ( isset( $_wp_theme_features[$feature][0][$header_support] ) && $_wp_theme_features[$feature][0][$header_support] );
+	}
+
+	/**
+	 * Filter whether the current theme supports a specific feature.
+	 *
+	 * The dynamic portion of the hook name, $feature, refers to
+	 * the specific theme feature. Possible values include 'post-formats',
+	 * 'post-thumbnails', 'custom-background', 'custom-header', 'menus',
+	 * 'automatic-feed-links', and 'html5'.
+	 *
+	 * @since 3.4.0
+	 *
+	 * @param bool   true     Whether the current theme supports the given feature. Default true.
+	 * @param array  $args    Array of arguments for the feature.
+	 * @param string $feature The theme feature.
+	 */
+	return apply_filters( "current_theme_supports-{$feature}", true, $args, $_wp_theme_features[$feature] );
+}
+
 add_action( 'delete_attachment', '_delete_attachment_theme_mod' );
 
 add_action( 'plugins_loaded', '_wp_customize_include' );
