7c8044450f3163a56d8f8622bdb5b030f851c5ba
Kite <ixkaito@gmail.com>
2016-11-05 06:26:53 +0900

    Add `WP_Embed::register_handler()`

--- wp-includes/class-wp-embed.php
+++ wp-includes/class-wp-embed.php
@@ -34,6 +34,22 @@ class WP_Embed {
 		// After a post is saved, cache oEmbed items via AJAX
 		add_action( 'edit_form_advanced', array( $this, 'maybe_run_ajax_cache' ) );
 	}
+
+	/**
+	 * Register an embed handler. Do not use this function directly, use {@link wp_embed_register_handler()} instead.
+	 * This function should probably also only be used for sites that do not support oEmbed.
+	 *
+	 * @param string $id An internal ID/name for the handler. Needs to be unique.
+	 * @param string $regex The regex that will be used to see if this handler should be used for a URL.
+	 * @param callback $callback The callback function that will be called if the regex is matched.
+	 * @param int $priority Optional. Used to specify the order in which the registered handlers will be tested (default: 10). Lower numbers correspond with earlier testing, and handlers with the same priority are tested in the order in which they were added to the action.
+	 */
+	public function register_handler( $id, $regex, $callback, $priority = 10 ) {
+		$this->handlers[$priority][$id] = array(
+			'regex'    => $regex,
+			'callback' => $callback,
+		);
+	}
 }
 
 $GLOBALS['wp_embed'] = new WP_Embed();
