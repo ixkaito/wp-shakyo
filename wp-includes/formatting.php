d647ad95e5d5a1d829bfb96fca0c2c54e0cee662
Kite <ixkaito@gmail.com>
2016-11-07 19:14:18 +0900

    Add `esc_attr()`

--- wp-includes/formatting.php
+++ wp-includes/formatting.php
@@ -193,6 +193,31 @@ function _deep_replace( $search, $subject ) {
 }
 
 /**
+ * Escaping for HTML attributes.
+ *
+ * @since 2.8.0
+ *
+ * @param string $text
+ * @return string
+ */
+function esc_attr( $text ) {
+	$safe_text = wp_check_invalid_utf8( $text );
+	$safe_text = _wp_specialchars( $safe_text, ENT_QUOTES );
+	/**
+	 * Filter a string cleaned and escaped for output in an HTML attribute.
+	 *
+	 * Text passed to esc_attr() is stripped of invalid or special characters
+	 * before output.
+	 *
+	 * @since 2.0.6
+	 *
+	 * @param string $safe_text The text after it has been escaped.
+ 	 * @param string $text      The text prior to being escaped.
+	 */
+	return apply_filters( 'attribute_escape', $safe_text, $text );
+}
+
+/**
  * Parses a string into variables to be stored in an array.
  *
  * Uses {@link http://www.php.net/parse_str parse_str()} and stripslashes if
