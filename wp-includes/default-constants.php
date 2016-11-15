4618684ba6809431a8088124da375ef406256d73
Kite <ixkaito@gmail.com>
2016-11-15 20:47:34 +0900

    Add `wp_templating_constants()`

--- wp-includes/default-constants.php
+++ wp-includes/default-constants.php
@@ -292,3 +292,32 @@ function wp_functionality_constants() {
 	if ( !defined( 'WP_CRON_LOCK_TIMEOUT' ) )
 		define('WP_CRON_LOCK_TIMEOUT', 60);  // In seconds
 }
+
+/**
+ * Defines templating related WordPress constants
+ *
+ * @since 3.0.0
+ */
+function wp_templating_constants() {
+	/**
+	 * Filesystem path to the current active template directory
+	 * @since 1.5.0
+	 */
+	define('TEMPLATEPATH', get_template_directory());
+
+	/**
+	 * Filesystem path to the current active template stylesheet directory
+	 * @since 2.1.0
+	 */
+	define('STYLESHEETPATH', get_stylesheet_directory());
+
+	/**
+	 * Slug of the default theme for this install.
+	 * Used as the default theme when installing new sites.
+	 * Will be used as the fallback if the current theme doesn't exist.
+	 * @since 3.0.0
+	 */
+	if ( !defined('WP_DEFAULT_THEME') )
+		define( 'WP_DEFAULT_THEME', 'twentyfourteen' );
+
+}
