16824ccaf034e99426a4552441ca0781a9776bca
Kite <ixkaito@gmail.com>
2016-11-18 03:59:29 +0900

    Add `validate_file()`

--- wp-includes/functions.php
+++ wp-includes/functions.php
@@ -495,6 +495,36 @@ function _doing_it_wrong( $function, $message, $version ) {
 }
 
 /**
+ * File validates against allowed set of defined rules.
+ *
+ * A return value of '1' means that the $file contains either '..' or './'. A
+ * return value of '2' means that the $file contains ':' after the first
+ * character. A return value of '3' means that the file is not in the allowed
+ * files list.
+ *
+ * @since 1.2.0
+ *
+ * @param string $file File path.
+ * @param array $allowed_files List of allowed files.
+ * @return int 0 means nothing is wrong, greater than 0 means something was wrong.
+ */
+function validate_file( $file, $allowed_files = '' ) {
+	if ( false !== strpos( $file, '..' ) )
+		return 1;
+
+	if ( false !== strpos( $file, './' ) )
+		return 1;
+
+	if ( ! empty( $allowed_files ) && ! in_array( $file, $allowed_files ) )
+		return 3;
+
+	if (':' == substr( $file, 1, 1 ) )
+		return 2;
+
+	return 0;
+}
+
+/**
  * Determine if SSL is used.
  *
  * @since 2.6.0
