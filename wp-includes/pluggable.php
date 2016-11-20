31012f651216bb2157475f7f08b656d282cd9c82
Kite <ixkaito@gmail.com>
2016-11-20 15:43:56 +0900

    Add `get_userdata()`

--- wp-includes/pluggable.php
+++ wp-includes/pluggable.php
@@ -126,6 +126,17 @@ function get_currentuserinfo() {
 endif;
 
 if ( !function_exists('get_userdata') ) :
+/**
+ * Retrieve user info by user ID.
+ *
+ * @since 0.71
+ *
+ * @param int $user_id User ID
+ * @return WP_User|bool WP_User object on success, false on failure.
+ */
+function get_userdata( $user_id ) {
+	return get_user_by( 'id', $user_id );
+}
 endif;
 
 if ( !function_exists('get_user_by') ) :
