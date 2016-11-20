76689512e05caff2e8b4333786bc059bb4a81f11
Kite <ixkaito@gmail.com>
2016-11-20 15:50:06 +0900

    Add `current_user_can()`

--- wp-includes/capabilities.php
+++ wp-includes/capabilities.php
@@ -300,3 +300,23 @@ class WP_User {
 		return $user;
 	}
 }
+
+/**
+ * Whether current user has capability or role.
+ *
+ * @since 2.0.0
+ *
+ * @param string $capability Capability or role name.
+ * @return bool
+ */
+function current_user_can( $capability ) {
+	$current_user = wp_get_current_user();
+
+	if ( empty( $current_user ) )
+		return false;
+
+	$args = array_slice( func_get_args(), 1 );
+	$args = array_merge( array( $capability ), $args );
+
+	return call_user_func_array( array( $current_user, 'has_cap' ), $args );
+}
