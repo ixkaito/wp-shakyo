a4a0de11b39601bc5949fe67ae33cebcd96ffaca
Kite <ixkaito@gmail.com>
2016-11-20 15:44:18 +0900

    Add `get_user_by()`

--- wp-includes/pluggable.php
+++ wp-includes/pluggable.php
@@ -140,6 +140,26 @@ function get_userdata( $user_id ) {
 endif;
 
 if ( !function_exists('get_user_by') ) :
+/**
+ * Retrieve user info by a given field
+ *
+ * @since 2.8.0
+ *
+ * @param string $field The field to retrieve the user with. id | slug | email | login
+ * @param int|string $value A value for $field. A user ID, slug, email address, or login name.
+ * @return WP_User|bool WP_User object on success, false on failure.
+ */
+function get_user_by( $field, $value ) {
+	$userdata = WP_User::get_data_by( $field, $value );
+
+	if ( !$userdata )
+		return false;
+
+	$user = new WP_User;
+	$user->init( $userdata );
+
+	return $user;
+}
 endif;
 
 if ( !function_exists('cache_users') ) :
