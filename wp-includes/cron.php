06ec652bf087210a749be817658f75dee9f38562
Kite <ixkaito@gmail.com>
2016-11-23 06:21:36 +0900

    Add `wp_cron()`

--- wp-includes/cron.php
+++ wp-includes/cron.php
@@ -4,3 +4,36 @@
  *
  * @package WordPress
  */
+
+/**
+ * Run scheduled callbacks or spawn cron for all scheduled events.
+ *
+ * @since 2.1.0
+ *
+ * @return null When doesn't need to run Cron.
+ */
+function wp_cron() {
+
+	// Prevent infinite loops caused by lack of wp-cron.php
+	if ( strpos($_SERVER['REQUEST_URI'], '/wp-cron.php') !== false || ( defined('DISABLE_WP_CRON') && DISABLE_WP_CRON ) )
+		return;
+
+	if ( false === $crons = _get_cron_array() )
+		return;
+
+	$gmt_time = microtime( true );
+	$keys = array_keys( $crons );
+	if ( isset($keys[0]) && $keys[0] > $gmt_time )
+		return;
+
+	$schedules = wp_get_schedules();
+	foreach ( $crons as $timestamp => $cronhooks ) {
+		if ( $timestamp > $gmt_time ) break;
+		foreach ( (array) $cronhooks as $hook => $args ) {
+			if ( isset($schedules[$hook]['callback']) && !call_user_func( $schedules[$hook]['callback'] ) )
+				continue;
+			spawn_cron( $gmt_time );
+			break 2;
+		}
+	}
+}
