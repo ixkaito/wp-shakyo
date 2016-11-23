b3503b9f6efe04e81f9d27bccf800ec08dc58b12
Kite <ixkaito@gmail.com>
2016-11-24 03:30:26 +0900

    Add `_show_post_preview()`

--- wp-includes/revision.php
+++ wp-includes/revision.php
@@ -5,3 +5,21 @@
  * @package WordPress
  * @subpackage Post_Revisions
  */
+
+/**
+ * Filters the latest content for preview from the post autosave.
+ *
+ * @since 2.7.0
+ * @access private
+ */
+function _show_post_preview() {
+
+	if ( isset($_GET['preview_id']) && isset($_GET['preview_nonce']) ) {
+		$id = (int) $_GET['preview_id'];
+
+		if ( false == wp_verify_nonce( $_GET['preview_nonce'], 'post_preview_' . $id ) )
+			wp_die( __('You do not have permission to preview drafts.') );
+
+		add_filter('the_preview', '_set_preview');
+	}
+}
