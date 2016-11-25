2a175501b3a51332ecf580fffc8f6b98a338457b
Kite <ixkaito@gmail.com>
2016-11-26 01:04:48 +0900

    Load text domain in wp-admin/includes/admin.php

--- wp-admin/includes/admin.php
+++ wp-admin/includes/admin.php
@@ -5,3 +5,14 @@
  * @package WordPress
  * @subpackage Administration
  */
+
+if ( ! defined('WP_ADMIN') ) {
+	/*
+	 * This file is being included from a file other than wp-admin/admin.php, so
+	 * some setup was skipped. Make sure the admin message catalog is loaded since
+	 * load_default_textdomain() will not have done so in this context.
+	 */
+	load_textdomain( 'default', WP_LANG_DIR . '/admin-' . get_locale() . '.mo' );
+}
+
+var_dump(__FILE__);
