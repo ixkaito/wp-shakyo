57ce24c39f2c3e0c97d2c3c85c9bfdd7611ee96d
Kite <ixkaito@gmail.com>
2016-11-28 13:07:05 +0900

    Add wp-admin/includes/file.php

---
+++ wp-admin/includes/file.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Functions for reading, writing, modifying, and deleting files on the file system.
+ * Includes functionality for theme-specific files as well as operations for uploading,
+ * archiving, and rendering output when necessary.
+ *
+ * @package WordPress
+ * @subpackage Administration
+ */
