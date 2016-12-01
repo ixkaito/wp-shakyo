a5c380fdc58d034dd61d3a9bea9731044e67d816
Kite <ixkaito@gmail.com>
2016-12-01 13:46:37 +0900

    Require WordPress List Table Administration API and base class

--- wp-admin/includes/admin.php
+++ wp-admin/includes/admin.php
@@ -51,4 +51,8 @@ require_once(ABSPATH . 'wp-admin/includes/taxonomy.php');
 /** WordPress Template Administration API */
 require_once(ABSPATH . 'wp-admin/includes/template.php');
 
+/** WordPress List Table Administration API and base class */
+require_once(ABSPATH . 'wp-admin/includes/class-wp-list-table.php');
+require_once(ABSPATH . 'wp-admin/includes/list-table.php');
+
 var_dump(__FILE__);
