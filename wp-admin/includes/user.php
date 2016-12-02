976dfb6bc5213a3accee648a31056d53078718de
Kite <ixkaito@gmail.com>
2016-12-02 13:53:58 +0900

    Init wp-admin/includes/user.php

--- wp-admin/includes/user.php
+++ wp-admin/includes/user.php
@@ -5,3 +5,9 @@
  * @package WordPress
  * @subpackage Administration
  */
+
+add_action('admin_init', 'default_password_nag_handler');
+
+add_action('profile_update', 'default_password_nag_edit_user', 10, 2);
+
+add_action('admin_notices', 'default_password_nag');
