22962347945747d6acf80bce5f1ab807ee85b433
Kite <ixkaito@gmail.com>
2016-12-02 13:56:43 +0900

    Require WordPress Deprecated Administration API

---
+++ wp-admin/includes/deprecated.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Deprecated admin functions from past WordPress versions. You shouldn't use these
+ * functions and look for the alternatives instead. The functions will be removed
+ * in a later version.
+ *
+ * @package WordPress
+ * @subpackage Deprecated
+ */
