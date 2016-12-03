9592c7e980fb06aff7782e33e9e661f5febce6a7
Kite <ixkaito@gmail.com>
2016-12-03 14:02:06 +0900

    Init wp-admin/includes/ms-deprecated.php

---
+++ wp-admin/includes/ms-deprecated.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Deprecated multisite admin functions from past WordPress versions and WordPress MU.
+ * You shouldn't use these functions and look for the alternatives instead. The functions
+ * will be removed in a later version.
+ *
+ * @package WordPress
+ * @subpackage Deprecated
+ * @since 3.0.0
+ */
