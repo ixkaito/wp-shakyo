bb3f9b21c932caf7e2c793d54e6f12d191732bf6
Kite <ixkaito@gmail.com>
2016-12-07 14:00:11 +0900

    Add wp-admin/includes/class-wp-upgrader.php

---
+++ wp-admin/includes/class-wp-upgrader.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * A File upgrader class for WordPress.
+ *
+ * This set of classes are designed to be used to upgrade/install a local set of files on the filesystem via the Filesystem Abstraction classes.
+ *
+ * @link http://trac.wordpress.org/ticket/7875 consolidate plugin/theme/core upgrade/install functions
+ *
+ * @package WordPress
+ * @subpackage Upgrader
+ * @since 2.8.0
+ */
