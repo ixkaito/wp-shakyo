44e8ddbd54f1f01c9c9a3b47192b6f3bbffce74e
Kite <ixkaito@gmail.com>
2016-12-28 14:29:44 +0900

    Add `site_url()`

--- wp-includes/link-template.php
+++ wp-includes/link-template.php
@@ -5,3 +5,22 @@
  * @package WordPress
  * @subpackage Template
  */
+
+/**
+ * Retrieve the site url for the current site.
+ *
+ * Returns the 'site_url' option with the appropriate protocol, 'https' if
+ * is_ssl() and 'http' otherwise. If $scheme is 'http' or 'https', is_ssl() is
+ * overridden.
+ *
+ * @since 3.0.0
+ *
+ * @uses get_site_url()
+ *
+ * @param string $path Optional. Path relative to the site url.
+ * @param string $scheme Optional. Scheme to give the site url context. See set_url_scheme().
+ * @return string Site url link with optional path appended.
+*/
+function site_url( $path = '', $scheme = null ) {
+	return get_site_url( null, $path, $scheme );
+}
