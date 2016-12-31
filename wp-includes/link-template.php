b1f8edf637a64826b8e7e438ce721778f0b31fcd
Kite <ixkaito@gmail.com>
2017-01-01 07:02:05 +0900

    Add `set_url_scheme()`

--- wp-includes/link-template.php
+++ wp-includes/link-template.php
@@ -66,3 +66,48 @@ function get_site_url( $blog_id = null, $path = '', $scheme = null ) {
 	 */
 	return apply_filters( 'site_url', $url, $path, $scheme, $blog_id );
 }
+
+/**
+ * Set the scheme for a URL
+ *
+ * @since 3.4.0
+ *
+ * @param string $url Absolute url that includes a scheme
+ * @param string $scheme Optional. Scheme to give $url. Currently 'http', 'https', 'login', 'login_post', 'admin', or 'relative'.
+ * @return string $url URL with chosen scheme.
+ */
+function set_url_scheme( $url, $scheme = null ) {
+	$orig_scheme = $scheme;
+
+	if ( ! $scheme ) {
+		$scheme = is_ssl() ? 'https' : 'http';
+	} elseif ( $scheme === 'admin' || $scheme === 'login' || $scheme === 'login_post' || $scheme === 'rpc' ) {
+		$scheme = is_ssl() || force_ssl_admin() ? 'https' : 'http';
+	} elseif ( $scheme !== 'http' && $scheme !== 'https' && $scheme !== 'relative' ) {
+		$scheme = is_ssl() ? 'https' : 'http';
+	}
+
+	$url = trim( $url );
+	if ( substr( $url, 0, 2 ) === '//' )
+		$url = 'http:' . $url;
+
+	if ( 'relative' == $scheme ) {
+		$url = ltrim( preg_replace( '#^\w+://[^/]*#', '', $url ) );
+		if ( $url !== '' && $url[0] === '/' )
+			$url = '/' . ltrim($url , "/ \t\n\r\0\x0B" );
+	} else {
+		$url = preg_replace( '#^\w+://#', $scheme . '://', $url );
+	}
+
+	/**
+	 * Filter the resulting URL after setting the scheme.
+	 *
+	 * @since 3.4.0
+	 *
+	 * @param string $url         The complete URL including scheme and path.
+	 * @param string $scheme      Scheme applied to the URL. One of 'http', 'https', or 'relative'.
+	 * @param string $orig_scheme Scheme requested for the URL. One of 'http', 'https', 'login',
+	 *                            'login_post', 'admin', 'rpc', or 'relative'.
+	 */
+	return apply_filters( 'set_url_scheme', $url, $scheme, $orig_scheme );
+}
