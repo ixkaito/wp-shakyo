67b23b50d24ff03d154f9909246f580e1a45c499
Kite <ixkaito@gmail.com>
2017-01-01 07:26:22 +0900

    Add `_WP_Dependency::__construct()`

--- wp-includes/class.wp-dependencies.php
+++ wp-includes/class.wp-dependencies.php
@@ -422,72 +422,72 @@ class WP_Dependencies {
  * @since 2.6.0
  */
 class _WP_Dependency {
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	/**
+	 * The handle name.
+	 *
+	 * @access public
+	 * @since 2.6.0
+	 * @var null
+	 */
+	public $handle;
+
+	/**
+	 * The handle source.
+	 *
+	 * @access public
+	 * @since 2.6.0
+	 * @var null
+	 */
+	public $src;
+
+	/**
+	 * An array of handle dependencies.
+	 *
+	 * @access public
+	 * @since 2.6.0
+	 * @var array
+	 */
+	public $deps = array();
+
+	/**
+	 * The handle version.
+	 *
+	 * Used for cache-busting.
+	 *
+	 * @access public
+	 * @since 2.6.0
+	 * @var bool|string
+	 */
+	public $ver = false;
+
+	/**
+	 * Additional arguments for the handle.
+	 *
+	 * @access public
+	 * @since 2.6.0
+	 * @var null
+	 */
+	public $args = null;  // Custom property, such as $in_footer or $media.
+
+	/**
+	 * Extra data to supply to the handle.
+	 *
+	 * @access public
+	 * @since 2.6.0
+	 * @var array
+	 */
+	public $extra = array();
+
+	/**
+	 * Setup dependencies.
+	 *
+	 * @since 2.6.0
+	 */
+	public function __construct() {
+		@list( $this->handle, $this->src, $this->deps, $this->ver, $this->args ) = func_get_args();
+		if ( ! is_array($this->deps) )
+			$this->deps = array();
+	}
 
 
 
