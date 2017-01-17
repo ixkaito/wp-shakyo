b7f88790fb96934fc1da4d09fc9278046b134d25
Kite <ixkaito@gmail.com>
2017-01-18 06:42:49 +0900

    Add `WP_Filesystem_Base` class

--- wp-admin/includes/class-wp-filesystem-base.php
+++ wp-admin/includes/class-wp-filesystem-base.php
@@ -7,22 +7,9 @@
  */
 
 /**
- * WordPress Filesystem Class for direct PHP file and folder manipulation.
+ * Base WordPress Filesystem class for which Filesystem implementations extend
  *
  * @since 2.5.0
- * @package WordPress
- * @subpackage Filesystem
- * @uses WP_Filesystem_Base Extends class
  */
-class WP_Filesystem_Direct extends WP_Filesystem_Base {
-
-	/**
-	 * constructor
-	 *
-	 * @param mixed $arg ignored argument
-	 */
-	public function __construct($arg) {
-		$this->method = 'direct';
-		$this->errors = new WP_Error();
-	}
-}
+class WP_Filesystem_Base {
+} // WP_Filesystem_Base
--- wp-admin/includes/class-wp-filesystem-direct.php
+++ wp-admin/includes/class-wp-filesystem-direct.php
@@ -5,3 +5,24 @@
  * @package WordPress
  * @subpackage Filesystem
  */
+
+/**
+ * WordPress Filesystem Class for direct PHP file and folder manipulation.
+ *
+ * @since 2.5.0
+ * @package WordPress
+ * @subpackage Filesystem
+ * @uses WP_Filesystem_Base Extends class
+ */
+class WP_Filesystem_Direct extends WP_Filesystem_Base {
+
+	/**
+	 * constructor
+	 *
+	 * @param mixed $arg ignored argument
+	 */
+	public function __construct($arg) {
+		$this->method = 'direct';
+		$this->errors = new WP_Error();
+	}
+}
