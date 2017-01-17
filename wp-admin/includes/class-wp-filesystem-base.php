1f0f390ccaf682e81d24d521c2f61f308a34ca97
Kite <ixkaito@gmail.com>
2017-01-18 06:43:38 +0900

    Declare some variables of class `WP_Filesystem_Base`

--- wp-admin/includes/class-wp-filesystem-base.php
+++ wp-admin/includes/class-wp-filesystem-base.php
@@ -12,4 +12,31 @@
  * @since 2.5.0
  */
 class WP_Filesystem_Base {
+	/**
+	 * Whether to display debug data for the connection.
+	 *
+	 * @access public
+	 * @since 2.5.0
+	 * @var bool
+	 */
+	public $verbose = false;
+
+	/**
+	 * Cached list of local filepaths to mapped remote filepaths.
+	 *
+	 * @access private
+	 * @since 2.7.0
+	 * @var array
+	 */
+	private $cache = array();
+
+	/**
+	 * The Access method of the current connection, Set automatically.
+	 *
+	 * @access public
+	 * @since 2.5.0
+	 * @var string
+	 */
+	public $method = '';
+
 } // WP_Filesystem_Base
