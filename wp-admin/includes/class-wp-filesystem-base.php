c0c526888d9a09495a080c65442fdcced401e654
Kite <ixkaito@gmail.com>
2017-01-23 18:31:46 +0900

    Add `WP_Filesystem_Base::find_folder()`

--- wp-admin/includes/class-wp-filesystem-base.php
+++ wp-admin/includes/class-wp-filesystem-base.php
@@ -207,73 +207,73 @@ class WP_Filesystem_Base {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	/**
+	 * Locate a folder on the remote filesystem.
+	 *
+	 * Assumes that on Windows systems, Stripping off the Drive
+	 * letter is OK Sanitizes \\ to / in windows filepaths.
+	 *
+	 * @access public
+	 * @since 2.7.0
+	 *
+	 * @param string $folder the folder to locate.
+	 * @return string The location of the remote path.
+	 */
+	public function find_folder( $folder ) {
+
+		if ( isset( $this->cache[ $folder ] ) )
+			return $this->cache[ $folder ];
+
+		if ( stripos($this->method, 'ftp') !== false ) {
+			$constant_overrides = array(
+				'FTP_BASE' => ABSPATH,
+				'FTP_CONTENT_DIR' => WP_CONTENT_DIR,
+				'FTP_PLUGIN_DIR' => WP_PLUGIN_DIR,
+				'FTP_LANG_DIR' => WP_LANG_DIR
+			);
+
+			// Direct matches ( folder = CONSTANT/ )
+			foreach ( $constant_overrides as $constant => $dir ) {
+				if ( ! defined( $constant ) )
+					continue;
+				if ( $folder === $dir )
+					return trailingslashit( constant( $constant ) );
+			}
+
+			// Prefix Matches ( folder = CONSTANT/subdir )
+			foreach ( $constant_overrides as $constant => $dir ) {
+				if ( ! defined( $constant ) )
+					continue;
+				if ( 0 === stripos( $folder, $dir ) ) { // $folder starts with $dir
+					$potential_folder = preg_replace( '#^' . preg_quote( $dir, '#' ) . '/#i', trailingslashit( constant( $constant ) ), $folder );
+					$potential_folder = trailingslashit( $potential_folder );
+
+					if ( $this->is_dir( $potential_folder ) ) {
+						$this->cache[ $folder ] = $potential_folder;
+						return $potential_folder;
+					}
+				}
+			}
+		} elseif ( 'direct' == $this->method ) {
+			$folder = str_replace('\\', '/', $folder); // Windows path sanitisation
+			return trailingslashit($folder);
+		}
+
+		$folder = preg_replace('|^([a-z]{1}):|i', '', $folder); // Strip out windows drive letter if it's there.
+		$folder = str_replace('\\', '/', $folder); // Windows path sanitisation
+
+		if ( isset($this->cache[ $folder ] ) )
+			return $this->cache[ $folder ];
+
+		if ( $this->exists($folder) ) { // Folder exists at that absolute path.
+			$folder = trailingslashit($folder);
+			$this->cache[ $folder ] = $folder;
+			return $folder;
+		}
+		if ( $return = $this->search_for_folder($folder) )
+			$this->cache[ $folder ] = $return;
+		return $return;
+	}
 
 
 
