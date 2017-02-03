fe61408cf1a7152290bd52a6b062e7937523d034
Kite <ixkaito@gmail.com>
2017-02-04 05:47:03 +0900

    Add `get_home_url()`

--- wp-includes/link-template.php
+++ wp-includes/link-template.php
@@ -2422,55 +2422,55 @@ function home_url( $path = '', $scheme = null ) {
 	return get_home_url( null, $path, $scheme );
 }
 
+/**
+ * Retrieve the home url for a given site.
+ *
+ * Returns the 'home' option with the appropriate protocol, 'https' if
+ * is_ssl() and 'http' otherwise. If $scheme is 'http' or 'https', is_ssl() is
+ * overridden.
+ *
+ * @since 3.0.0
+ *
+ * @param  int $blog_id   (optional) Blog ID. Defaults to current blog.
+ * @param  string $path   (optional) Path relative to the home url.
+ * @param  string $scheme (optional) Scheme to give the home url context. Currently 'http', 'https', or 'relative'.
+ * @return string Home url link with optional path appended.
+*/
+function get_home_url( $blog_id = null, $path = '', $scheme = null ) {
+	$orig_scheme = $scheme;
 
+	if ( empty( $blog_id ) || !is_multisite() ) {
+		$url = get_option( 'home' );
+	} else {
+		switch_to_blog( $blog_id );
+		$url = get_option( 'home' );
+		restore_current_blog();
+	}
 
+	if ( ! in_array( $scheme, array( 'http', 'https', 'relative' ) ) ) {
+		if ( is_ssl() && ! is_admin() && 'wp-login.php' !== $GLOBALS['pagenow'] )
+			$scheme = 'https';
+		else
+			$scheme = parse_url( $url, PHP_URL_SCHEME );
+	}
 
+	$url = set_url_scheme( $url, $scheme );
 
+	if ( $path && is_string( $path ) )
+		$url .= '/' . ltrim( $path, '/' );
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	/**
+	 * Filter the home URL.
+	 *
+	 * @since 3.0.0
+	 *
+	 * @param string      $url         The complete home URL including scheme and path.
+	 * @param string      $path        Path relative to the home URL. Blank string if no path is specified.
+	 * @param string|null $orig_scheme Scheme to give the home URL context. Accepts 'http', 'https', 'relative' or null.
+	 * @param int|null    $blog_id     Blog ID, or null for the current blog.
+	 */
+	return apply_filters( 'home_url', $url, $path, $orig_scheme, $blog_id );
+}
 
 /**
  * Retrieve the site url for the current site.
