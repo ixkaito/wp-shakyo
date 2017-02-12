067c0ddc427bba86323b1b71e10b2364599628cf
Kite <ixkaito@gmail.com>
2017-02-13 05:02:48 +0900

    Add `wp_remote_retrieve_body()`

--- wp-includes/http.php
+++ wp-includes/http.php
@@ -272,20 +272,20 @@ function wp_remote_post($url, $args = array()) {
 
 
 
+/**
+ * Retrieve only the body from the raw response.
+ *
+ * @since 2.7.0
+ *
+ * @param array $response HTTP response.
+ * @return string The body of the response. Empty string if no body or incorrect parameter given.
+ */
+function wp_remote_retrieve_body( $response ) {
+	if ( is_wp_error($response) || ! isset($response['body']) )
+		return '';
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+	return $response['body'];
+}
 
 /**
  * Determines if there is an HTTP Transport that can process this request.
