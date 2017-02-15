447fe882f175fc6ccdfd76441e1e8567c6cce6d8
Kite <ixkaito@gmail.com>
2017-02-16 03:22:49 +0900

    Add `esc_html()`

--- wp-includes/formatting.php
+++ wp-includes/formatting.php
@@ -3143,30 +3143,30 @@ function esc_url( $url, $protocols = null, $_context = 'display' ) {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/**
+ * Escaping for HTML blocks.
+ *
+ * @since 2.8.0
+ *
+ * @param string $text
+ * @return string
+ */
+function esc_html( $text ) {
+	$safe_text = wp_check_invalid_utf8( $text );
+	$safe_text = _wp_specialchars( $safe_text, ENT_QUOTES );
+	/**
+	 * Filter a string cleaned and escaped for output in HTML.
+	 *
+	 * Text passed to esc_html() is stripped of invalid or special characters
+	 * before output.
+	 *
+	 * @since 2.8.0
+	 *
+	 * @param string $safe_text The text after it has been escaped.
+ 	 * @param string $text      The text prior to being escaped.
+	 */
+	return apply_filters( 'esc_html', $safe_text, $text );
+}
 
 /**
  * Escaping for HTML attributes.
