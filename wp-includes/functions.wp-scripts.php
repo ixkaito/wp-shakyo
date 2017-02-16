fcfde4e99e3e8a7926a557bd138310621f4fda27
Kite <ixkaito@gmail.com>
2017-02-17 08:14:46 +0900

    Add `wp_localize_script()`

--- wp-includes/functions.wp-scripts.php
+++ wp-includes/functions.wp-scripts.php
@@ -48,3 +48,77 @@ function wp_print_scripts( $handles = false ) {
 
 	return $wp_scripts->do_items( $handles );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * Localize a script.
+ *
+ * Works only if the script has already been added.
+ *
+ * Accepts an associative array $l10n and creates a JavaScript object:
+ * <code>
+ * "$object_name" = {
+ *       key: value,
+ *       key: value,
+ *       ...
+ * }
+ * </code>
+ *
+ * @see WP_Dependencies::localize()
+ * @link http://core.trac.wordpress.org/ticket/11520
+ * @global WP_Scripts $wp_scripts The WP_Scripts object for printing scripts.
+ *
+ * @since 2.6.0
+ *
+ * @param string $handle      Script handle the data will be attached to.
+ * @param string $object_name Name for the JavaScript object. Passed directly, so it should be qualified JS variable.
+ *                            Example: '/[a-zA-Z0-9_]+/'.
+ * @param array $l10n         The data itself. The data can be either a single or multi-dimensional array.
+ * @return bool True if the script was successfully localized, false otherwise.
+ */
+function wp_localize_script( $handle, $object_name, $l10n ) {
+	global $wp_scripts;
+	if ( ! is_a( $wp_scripts, 'WP_Scripts' ) ) {
+		if ( ! did_action( 'init' ) )
+			_doing_it_wrong( __FUNCTION__, sprintf( __( 'Scripts and styles should not be registered or enqueued until the %1$s, %2$s, or %3$s hooks.' ),
+				'<code>wp_enqueue_scripts</code>', '<code>admin_enqueue_scripts</code>', '<code>login_enqueue_scripts</code>' ), '3.3' );
+
+		return false;
+	}
+
+	return $wp_scripts->localize( $handle, $object_name, $l10n );
+}
