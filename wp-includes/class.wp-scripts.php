20609dd4b5e632a1ab9d9b9d2cccdef0e831c812
Kite <ixkaito@gmail.com>
2017-02-19 18:53:34 +0900

    Add `WP_Scripts` class

--- wp-includes/class.wp-scripts.php
+++ wp-includes/class.wp-scripts.php
@@ -8,3 +8,12 @@
  * @package BackPress
  * @since r16
  */
+
+/**
+ * BackPress Scripts enqueue class.
+ *
+ * @package BackPress
+ * @uses WP_Dependencies
+ * @since r16
+ */
+class WP_Scripts extends WP_Dependencies {}
