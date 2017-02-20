2a4a5ce5f96021ea380524fc20c87937da11dc05
Kite <ixkaito@gmail.com>
2017-02-20 18:59:20 +0900

    Add `WP_Scripts::__construct()`

--- wp-includes/class.wp-scripts.php
+++ wp-includes/class.wp-scripts.php
@@ -16,4 +16,22 @@
  * @uses WP_Dependencies
  * @since r16
  */
-class WP_Scripts extends WP_Dependencies {}
+class WP_Scripts extends WP_Dependencies {
+	public $base_url; // Full URL with trailing slash
+	public $content_url;
+	public $default_version;
+	public $in_footer = array();
+	public $concat = '';
+	public $concat_version = '';
+	public $do_concat = false;
+	public $print_html = '';
+	public $print_code = '';
+	public $ext_handles = '';
+	public $ext_version = '';
+	public $default_dirs;
+
+	public function __construct() {
+		$this->init();
+		add_action( 'init', array( $this, 'init' ), 0 );
+	}
+}
