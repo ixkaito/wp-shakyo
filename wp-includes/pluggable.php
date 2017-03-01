702f94ee75649da7318adfad39a607fbcb1e6373
Kite <ixkaito@gmail.com>
2017-03-01 18:16:59 +0900

    Add `wp_create_nonce()`

--- wp-includes/pluggable.php
+++ wp-includes/pluggable.php
@@ -462,6 +462,27 @@ if ( !function_exists('wp_verify_nonce') ) :
 endif;
 
 if ( !function_exists('wp_create_nonce') ) :
+/**
+ * Creates a cryptographic token tied to a specific action, user, and window of time.
+ *
+ * @since 2.0.3
+ *
+ * @param string $action Scalar value to add context to the nonce.
+ * @return string The token.
+ */
+function wp_create_nonce($action = -1) {
+	$user = wp_get_current_user();
+	$uid = (int) $user->ID;
+	if ( ! $uid ) {
+		/** This filter is documented in wp-includes/pluggable.php */
+		$uid = apply_filters( 'nonce_user_logged_out', $uid, $action );
+	}
+
+	$token = wp_get_session_token();
+	$i = wp_nonce_tick();
+
+	return substr( wp_hash( $i . '|' . $action . '|' . $uid . '|' . $token, 'nonce' ), -12, 10 );
+}
 endif;
 
 if ( !function_exists('wp_salt') ) :
