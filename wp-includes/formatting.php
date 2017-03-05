c6a7aeb5fbb15757875bc9d32160edc5f2263b18
Kite <ixkaito@gmail.com>
2017-03-05 23:17:29 +0900

    Add `stripslashes_deep()`

--- wp-includes/formatting.php
+++ wp-includes/formatting.php
@@ -1690,31 +1690,31 @@ function untrailingslashit( $string ) {
 
 
 
+/**
+ * Navigates through an array and removes slashes from the values.
+ *
+ * If an array is passed, the array_map() function causes a callback to pass the
+ * value back to the function. The slashes from this value will removed.
+ *
+ * @since 2.0.0
+ *
+ * @param mixed $value The value to be stripped.
+ * @return mixed Stripped value.
+ */
+function stripslashes_deep($value) {
+	if ( is_array($value) ) {
+		$value = array_map('stripslashes_deep', $value);
+	} elseif ( is_object($value) ) {
+		$vars = get_object_vars( $value );
+		foreach ($vars as $key=>$data) {
+			$value->{$key} = stripslashes_deep( $data );
+		}
+	} elseif ( is_string( $value ) ) {
+		$value = stripslashes($value);
+	}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	return $value;
+}
 
 /**
  * Navigates through an array and encodes the values to be used in a URL.
