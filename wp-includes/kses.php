5b2e21790804eba77e384295efda0333f81d3842
Kite <ixkaito@gmail.com>
2018-02-06 02:26:48 +0900

    Add `wp_kses_split()`

--- wp-includes/kses.php
+++ wp-includes/kses.php
@@ -583,24 +583,24 @@ function wp_kses_hook( $string, $allowed_html, $allowed_protocols ) {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/**
+ * Searches for HTML tags, no matter how malformed.
+ *
+ * It also matches stray ">" characters.
+ *
+ * @since 1.0.0
+ *
+ * @param string $string Content to filter
+ * @param array $allowed_html Allowed HTML elements
+ * @param array $allowed_protocols Allowed protocols to keep
+ * @return string Content with fixed HTML tags
+ */
+function wp_kses_split( $string, $allowed_html, $allowed_protocols ) {
+	global $pass_allowed_html, $pass_allowed_protocols;
+	$pass_allowed_html = $allowed_html;
+	$pass_allowed_protocols = $allowed_protocols;
+	return preg_replace_callback( '%(<!--.*?(-->|$))|(<[^>]*(>|$)|>)%', '_wp_kses_split_callback', $string );
+}
 
 
 
