c2e78a7046e065cd1a4883f2e4eacb7759ac4a2e
Kite <ixkaito@gmail.com>
2018-02-12 05:21:15 +0900

    Add `remove_action()`

--- wp-includes/plugin.php
+++ wp-includes/plugin.php
@@ -604,23 +604,23 @@ function do_action_ref_array($tag, $args) {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/**
+ * Removes a function from a specified action hook.
+ *
+ * This function removes a function attached to a specified action hook. This
+ * method can be used to remove default functions attached to a specific filter
+ * hook and possibly replace them with a substitute.
+ *
+ * @since 1.2.0
+ *
+ * @param string   $tag                The action hook to which the function to be removed is hooked.
+ * @param callback $function_to_remove The name of the function which should be removed.
+ * @param int      $priority           Optional. The priority of the function. Default 10.
+ * @return boolean Whether the function is removed.
+ */
+function remove_action( $tag, $function_to_remove, $priority = 10 ) {
+	return remove_filter( $tag, $function_to_remove, $priority );
+}
 
 
 
