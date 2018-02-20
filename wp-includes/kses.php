12e4edfc83c67763cfda263506a55caaeffe24e7
Kite <ixkaito@gmail.com>
2018-02-21 02:44:31 +0900

    Add `wp_kses_post()`

--- wp-includes/kses.php
+++ wp-includes/kses.php
@@ -1321,20 +1321,20 @@ function wp_filter_kses( $data ) {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/**
+ * Sanitize content for allowed HTML tags for post content.
+ *
+ * Post content refers to the page contents of the 'post' type and not $_POST
+ * data from forms.
+ *
+ * @since 2.9.0
+ *
+ * @param string $data Post content to filter
+ * @return string Filtered post content with allowed HTML tags and attributes intact.
+ */
+function wp_kses_post($data) {
+	return wp_kses( $data , 'post' );
+}
 
 
 
