9fa956170b892c695e195019f87f702995af6102
Kite <ixkaito@gmail.com>
2018-02-21 02:39:39 +0900

    Add `populate_roles_260()`

--- wp-admin/includes/schema.php
+++ wp-admin/includes/schema.php
@@ -766,3 +766,17 @@ function populate_roles_250() {
 		$role->add_cap( 'edit_dashboard' );
 	}
 }
+
+/**
+ * Create and modify WordPress roles for WordPress 2.6.
+ *
+ * @since 2.6.0
+ */
+function populate_roles_260() {
+	$role = get_role( 'administrator' );
+
+	if ( !empty( $role ) ) {
+		$role->add_cap( 'update_plugins' );
+		$role->add_cap( 'delete_plugins' );
+	}
+}
