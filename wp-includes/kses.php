a992e352e6d8d9ce0d0f6fee9e35cee2413eba08
Kite <ixkaito@gmail.com>
2018-02-23 15:24:11 +0900

    Add `wp_kses_stripslashes()`

--- wp-includes/kses.php
+++ wp-includes/kses.php
@@ -1007,21 +1007,21 @@ function wp_kses_no_null($string) {
 	return $string;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/**
+ * Strips slashes from in front of quotes.
+ *
+ * This function changes the character sequence \" to just ". It leaves all
+ * other slashes alone. It's really weird, but the quoting from
+ * preg_replace(//e) seems to require this.
+ *
+ * @since 1.0.0
+ *
+ * @param string $string String to strip slashes
+ * @return string Fixed string with quoted slashes
+ */
+function wp_kses_stripslashes($string) {
+	return preg_replace('%\\\\"%', '"', $string);
+}
 
 
 
