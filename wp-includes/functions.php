db69000acc25b650ceb0d185b19fe3f66af24b61
Kite <ixkaito@gmail.com>
2018-03-05 00:08:03 +0900

    Add `_config_wp_siteurl()`

--- wp-includes/functions.php
+++ wp-includes/functions.php
@@ -2681,26 +2681,26 @@ function is_blog_installed() {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/**
+ * Retrieve the WordPress site URL.
+ *
+ * If the constant named 'WP_SITEURL' is defined, then the value in that
+ * constant will always be returned. This can be used for debugging a site
+ * on your localhost while not having to change the database to your URL.
+ *
+ * @since 2.2.0
+ * @access private
+ *
+ * @see WP_SITEURL
+ *
+ * @param string $url URL to set the WordPress site location.
+ * @return string The WordPress Site URL.
+ */
+function _config_wp_siteurl( $url = '' ) {
+	if ( defined( 'WP_SITEURL' ) )
+		return untrailingslashit( WP_SITEURL );
+	return $url;
+}
 
 
 
