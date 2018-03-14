7a8816a631b047dc5d5476f2aaf43828cdf29a37
Kite <ixkaito@gmail.com>
2018-03-15 01:02:29 +0900

    Add `PasswordHash::HashPassword()`

--- wp-includes/class-phpass.php
+++ wp-includes/class-phpass.php
@@ -38,6 +38,45 @@ class PasswordHash {
 	var $iteration_count_log2;
 	var $portable_hashes;
 	var $random_state;
+
+	function HashPassword($password)
+	{
+		if ( strlen( $password ) > 4096 ) {
+			return '*';
+		}
+
+		$random = '';
+
+		if (CRYPT_BLOWFISH == 1 && !$this->portable_hashes) {
+			$random = $this->get_random_bytes(16);
+			$hash =
+			    crypt($password, $this->gensalt_blowfish($random));
+			if (strlen($hash) == 60)
+				return $hash;
+		}
+
+		if (CRYPT_EXT_DES == 1 && !$this->portable_hashes) {
+			if (strlen($random) < 3)
+				$random = $this->get_random_bytes(3);
+			$hash =
+			    crypt($password, $this->gensalt_extended($random));
+			if (strlen($hash) == 20)
+				return $hash;
+		}
+
+		if (strlen($random) < 6)
+			$random = $this->get_random_bytes(6);
+		$hash =
+		    $this->crypt_private($password,
+		    $this->gensalt_private($random));
+		if (strlen($hash) == 34)
+			return $hash;
+
+		# Returning '*' on error is safe here, but would _not_ be safe
+		# in a crypt(3)-like function used _both_ for generating new
+		# hashes and for validating passwords against existing hashes.
+		return '*';
+	}
 }
 
 ?>
