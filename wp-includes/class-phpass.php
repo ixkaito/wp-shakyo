87d04a216e8bc1ade1b437a82900c121fafab2de
Kite <ixkaito@gmail.com>
2018-03-19 06:26:36 +0900

    Add `PasswordHash::encode64()`

--- wp-includes/class-phpass.php
+++ wp-includes/class-phpass.php
@@ -75,6 +75,29 @@ class PasswordHash {
 		return $output;
 	}
 
+	function encode64($input, $count)
+	{
+		$output = '';
+		$i = 0;
+		do {
+			$value = ord($input[$i++]);
+			$output .= $this->itoa64[$value & 0x3f];
+			if ($i < $count)
+				$value |= ord($input[$i]) << 8;
+			$output .= $this->itoa64[($value >> 6) & 0x3f];
+			if ($i++ >= $count)
+				break;
+			if ($i < $count)
+				$value |= ord($input[$i]) << 16;
+			$output .= $this->itoa64[($value >> 12) & 0x3f];
+			if ($i++ >= $count)
+				break;
+			$output .= $this->itoa64[($value >> 18) & 0x3f];
+		} while ($i < $count);
+
+		return $output;
+	}
+
 	function gensalt_extended($input)
 	{
 		$count_log2 = min($this->iteration_count_log2 + 8, 24);
