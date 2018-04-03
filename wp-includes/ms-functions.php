9582906aac3a8244f19d69f78f5e05af12f59a3d
Kite <ixkaito@gmail.com>
2018-04-03 17:02:59 +0900

    Add `get_user_count()`

--- wp-includes/ms-functions.php
+++ wp-includes/ms-functions.php
@@ -116,18 +116,18 @@ add_filter( 'site_option_welcome_user_email', 'welcome_user_msg_filter' );
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+/**
+ * The number of active users in your installation.
+ *
+ * The count is cached and updated twice daily. This is not a live count.
+ *
+ * @since MU 2.7
+ *
+ * @return int
+ */
+function get_user_count() {
+	return get_site_option( 'user_count' );
+}
 
 
 
