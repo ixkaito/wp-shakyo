5d0b5d757de1ba96d3ff2abd9d4c6ec82698478a
Kite <ixkaito@gmail.com>
2018-05-06 04:11:47 +0900

    Remove reject

--- vue-app/src/main.js
+++ vue-app/src/main.js
@@ -3,13 +3,13 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
-Vue.component('example2', function(resolve, reject) {
+Vue.component('example2', function(resolve) {
   setTimeout(function() {
     resolve(require('./components/Example2.vue'))
   }, 5000)
 })
 
-const app = new Vue({
+new Vue({
   el: '#app',
   render: h => h(App)
 })
