5351ab5975ddf796a1afb99cab0616da19458ee0
Kite <ixkaito@gmail.com>
2018-05-07 01:15:05 +0900

    Remove setTimeout

--- vue-app/src/main.js
+++ vue-app/src/main.js
@@ -4,9 +4,7 @@ import App from './App.vue'
 Vue.config.productionTip = false
 
 Vue.component('example2', function(resolve) {
-  setTimeout(function() {
-    resolve(require('./components/Example2.vue'))
-  }, 5000)
+  resolve(require('./components/Example2.vue'))
 })
 
 new Vue({
