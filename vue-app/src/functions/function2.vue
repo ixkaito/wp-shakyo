983b56d489bf3a79747324e190f4759a346d950c
Kite <ixkaito@gmail.com>
2018-05-18 03:39:27 +0900

    Update the app

--- vue-app/src/functions/function2.vue
+++ vue-app/src/functions/function2.vue
@@ -1,3 +1,28 @@
 <template>
-  <li>function2()</li>
+  <li>function2() <button type="button" @click="expand">Expand</button>
+    <ul>
+      <function3 v-if="expanded" />
+    </ul>
+  </li></li>
 </template>
+
+<script>
+  import function3 from './function3.vue'
+
+  export default {
+    name: 'app',
+    components: {
+      function3
+    },
+    data() {
+      return {
+        expanded: false
+      }
+    },
+    methods: {
+      expand() {
+        this.expanded = true
+      }
+    }
+  }
+</script>
