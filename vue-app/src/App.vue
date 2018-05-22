f9cf4df781d5b85f6fb1814d8df4a81f56bc82b2
Kite <ixkaito@gmail.com>
2018-05-23 05:11:39 +0900

    Remove comments

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -4,41 +4,5 @@
       <function1 />
       <function2 />
     </ul>
-
-<!--     <ul>
-      wp-blog-header.php <button type="button" @click="expand">Expand</button>
-      <WpBlogHeader v-if="expanded" />
-    </ul>
- -->  </div>
+  </div>
 </template>
-
-<script>
-  // import function1 from './functions/function1.vue'
-  // import function2 from './functions/function2.vue'
-
-  // export default {
-  //   name: 'app',
-  //   components: {
-  //     function1,
-  //     function2
-  //   }
-  // }
-  // import WpBlogHeader from './files/wp-blog-header.php.vue'
-
-  // export default {
-  //   name: 'app',
-  //   components: {
-  //     WpBlogHeader
-  //   },
-  //   data() {
-  //     return {
-  //       expanded: false
-  //     }
-  //   },
-  //   methods: {
-  //     expand() {
-  //       this.expanded = true
-  //     }
-  //   }
-  // }
-</script>
