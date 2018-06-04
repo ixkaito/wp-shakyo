2b7007b1d7c4a1cf642e4a3388c14a18e4fe100c
Kite <ixkaito@gmail.com>
2018-06-05 02:33:12 +0900

    Change the title

--- nuxt/nuxt.config.js
+++ nuxt/nuxt.config.js
@@ -3,7 +3,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'nuxt',
+    title: 'WP Shakyo',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
