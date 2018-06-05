107ef7767f72fdffaf2b84316eea29c8e6ef8f4b
Kite <ixkaito@gmail.com>
2018-06-06 05:50:25 +0900

    Test axios

---
+++ nuxt/pages/user/_id.vue
@@ -0,0 +1,36 @@
+<template>
+  <section class="container">
+    <img :src="`https://github.com/${user.login}.png`" :alt="user.name">
+    <h1>{{user.name}}</h1>
+  </section>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  async asyncData ({params}) {
+    const { data: user } = await axios.get(`https://api.github.com/users/${params.id}`)
+    return {
+      user
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  flex-direction: column;
+}
+
+.container img{
+  margin-bottom: 20px;
+  border-radius: 50%;
+  overflow: hidden;
+}
+</style>
