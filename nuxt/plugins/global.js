ca2e1911cd275330f2ffa28cec169c07080816bc
Kite <ixkaito@gmail.com>
2018-06-14 03:36:30 +0900

    Remove comments

--- nuxt/plugins/global.js
+++ nuxt/plugins/global.js
@@ -1,11 +1,6 @@
 import Vue from 'vue'
 import upperFirst from 'lodash/upperFirst'
 import camelCase from 'lodash/camelCase'
-// import WpBlogHeaderPhp from '@/pages/wp-blog-header.php.vue'
-// import WpLoadPhp from '@/pages/wp-load.php.vue'
-
-// Vue.component('wp-blog-header.php', WpBlogHeaderPhp)
-// Vue.component('wp-load.php', WpLoadPhp)
 
 const requireComponent = require.context(
   // The relative path of the components folder
