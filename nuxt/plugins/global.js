0d470b37da79f65ff8e30fd925497e0a1654dffc
Kite <ixkaito@gmail.com>
2018-06-15 04:43:10 +0900

    Register component names with '/' and '.php'

--- nuxt/plugins/global.js
+++ nuxt/plugins/global.js
@@ -16,32 +16,14 @@ requireComponent.keys().forEach(fileName => {
   const componentConfig = requireComponent(fileName)
 
   // Strip the leading `./` and extension from the filename
-  fileName = fileName.replace(/^\.\/(.*)\.\w+$/, '$1')
-
-  console.log(fileName)
-
-  // Get extension (e.g. .php)
-  const match = fileName.match(/(.*)(\.\w+$)/)
-  const extension = match ? match[2] : ''
-  fileName = match ? match[1] : fileName
-
-  // Get PascalCase name of component
-  const componentName = upperFirst(
-    camelCase(
-      // Filename without extension
-      fileName
-    )
-  // Add the extension
-  ) + extension
-
-  // console.log(componentName)
+  componentName = fileName.replace(/^\.\/(.*)\.\w+$/, '$1')
 
   // Register component globally
-  // Vue.component(
-  //   componentName,
-  //   // Look for the component options on `.default`, which will
-  //   // exist if the component was exported with `export default`,
-  //   // otherwise fall back to module's root.
-  //   componentConfig.default || componentConfig
-  // )
+  Vue.component(
+    componentName,
+    // Look for the component options on `.default`, which will
+    // exist if the component was exported with `export default`,
+    // otherwise fall back to module's root.
+    componentConfig.default || componentConfig
+  )
 })
