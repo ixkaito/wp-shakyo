14d6f6a1a0980ce19adb2cd52efc92fc5910f7c0
Kite <ixkaito@gmail.com>
2018-06-23 04:47:14 +0900

    Test template

--- nuxt/pages/index.php.vue
+++ nuxt/pages/index.php.vue
@@ -1,6 +1,6 @@
 <template>
-  <blockquote>
+  <div class="content">
     <nuxt-link class="file" to="/wp-blog-header.php">wp-blog-header.php</nuxt-link>
     <wp-blog-header.php />
-  </blockquote>
+  </div>
 </template>
