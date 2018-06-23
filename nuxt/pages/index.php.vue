3a561018369cdbd813a71a4eb5aa5a1c0cf7b734
Kite <ixkaito@gmail.com>
2018-06-24 03:59:38 +0900

    Import all components in ~/compoents dir

--- nuxt/pages/index.php.vue
+++ nuxt/pages/index.php.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="content">
     <nuxt-link class="file" to="/wp-blog-header.php">wp-blog-header.php</nuxt-link>
-    <wp-blog-header.php />
+    <include />
+    <!-- <wp-blog-header.php /> -->
   </div>
 </template>
--- nuxt/plugins/global.js
+++ nuxt/plugins/global.js
@@ -1,28 +1,34 @@
 import Vue from 'vue'
 import axios from 'axios'
 
-const requireComponent = require.context(
-  // The relative path of the components folder
-  '@/pages',
-  // Whether or not to look in subfolders
-  true,
-  // The regular expression used to match base component filenames
-  /[a-z]\w+\.(vue|js)$/
-)
+// const requireComponent = require.context(
+//   // The relative path of the components folder
+//   '~/pages',
+//   // Whether or not to look in subfolders
+//   true,
+//   // The regular expression used to match base component filenames
+//   /[a-z]\w+\.(vue|js)$/
+// )
 
-requireComponent.keys().forEach(fileName => {
-  // Get component config
-  const componentConfig = requireComponent(fileName)
+function requireAll(requireComponent) {
 
-  // Strip the leading `./` and extension from the filename
-  const componentName = fileName.replace(/^\.\/(.*)\.\w+$/, '$1')
+  requireComponent.keys().forEach(fileName => {
+    // Get component config
+    const componentConfig = requireComponent(fileName)
 
-  // Register component globally
-  Vue.component(
-    componentName,
-    // Look for the component options on `.default`, which will
-    // exist if the component was exported with `export default`,
-    // otherwise fall back to module's root.
-    componentConfig.default || componentConfig
-  )
-})
+    // Strip the leading `./` and extension from the filename
+    const componentName = fileName.replace(/^\.\/(.*)\.\w+$/, '$1')
+
+    // Register component globally
+    Vue.component(
+      componentName,
+      // Look for the component options on `.default`, which will
+      // exist if the component was exported with `export default`,
+      // otherwise fall back to module's root.
+      componentConfig.default || componentConfig
+    )
+  })
+}
+
+requireAll(require.context('~/pages', true, /[a-z]\w+\.(vue|js)$/))
+requireAll(require.context('~/components', true, /[a-z]\w+\.(vue|js)$/))
