f03c0db3c4a04ba5706e63dd273476065965cbab
Kite <ixkaito@gmail.com>
2018-06-26 01:42:45 +0900

    Add external links

--- nuxt/pages/index.php.vue
+++ nuxt/pages/index.php.vue
@@ -6,7 +6,7 @@
       <blockquote>
         <p class="include">wp-config.php</p>
         <blockquote>
-          <p class="include">wp-settings.php</p>
+          <p class="include"><a href="https://developer.wordpress.org/reference/files/wp-settings.php/" target="_blank">wp-settings.php</a></p>
         </blockquote>
       </blockquote>
     </blockquote>
