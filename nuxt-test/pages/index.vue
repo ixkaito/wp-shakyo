f9df49d54e1940b6203f5a834b81b7a090ad985e
Kite <ixkaito@gmail.com>
2018-07-07 03:09:58 +0900

    Update index.vue

--- nuxt-test/pages/index.vue
+++ nuxt-test/pages/index.vue
@@ -1,16 +1,36 @@
 <template>
-	<div>
-		<h1>Welcome!</h1>
-		<NLink to="/about">
-			About Page
-		</NLink>
-	</div>
+  <div class="container">
+    <h1>Home page 🚀</h1>
+    <NLink to="/about">
+      About Page
+    </NLink>
+  </div>
 </template>
 
 <script>
 export default {
-	head: {
-		title: 'Home page'
-	}
+  head: {
+    title: 'Home page 🚀',
+    meta: [
+      { hid: 'description', name: 'description', content: 'Home page description' }
+    ],
+    noscript: [
+      { innerHTML: 'Body No Scripts', body: true }
+    ],
+    script: [
+      { src: '/head.js' },
+      // Supported since 1.0
+      { src: '/body.js', body: true },
+      { src: 'defer.js', defer: '' }
+    ]
+  }
 }
 </script>
+
+<style>
+.container {
+  text-align: center;
+  margin-top: 150px;
+  font-size: 20px;
+}
+</style>
