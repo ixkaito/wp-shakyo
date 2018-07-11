3a0b6db672cc541756370e8ef733cb321089c464
Kite <ixkaito@gmail.com>
2018-07-11 17:15:39 +0900

    Layouts

--- nuxt-test/pages/about.vue
+++ nuxt-test/pages/about.vue
@@ -1,17 +1,19 @@
 <template>
-  <div class="content">
-    <h1 class="title">
-      Another Page
-    </h1>
-    <p>
-      <n-link to="/" class="button is-medium is-info hvr-wobble-vertical">
-        Another button
-      </n-link>
-    </p>
-    <p>
-      <n-link to="/">
-        Back home
-      </n-link>
-    </p>
+  <div>
+    <p>Hi from {{ name }}</p>
+    <n-link to="/">
+      Home page
+    </n-link>
   </div>
 </template>
+
+<script>
+export default {
+  layout: 'dark',
+  asyncData ({ req }) {
+    return {
+      name: req ? 'server' : 'client'
+    }
+  }
+}
+</script>
--- nuxt-test/pages/index.vue
+++ nuxt-test/pages/index.vue
@@ -1,17 +1,8 @@
 <template>
-  <div class="content">
-    <h1 class="title">
-      Custom CSS!
-    </h1>
-    <p>
-      <n-link to="/about" class="button is-medium is-primary hvr-float-shadow">
-        I am a button
-      </n-link>
-    </p>
-    <p>
-      <n-link to="/about">
-        About page
-      </n-link>
-    </p>
+  <div class="container">
+    <h1>Welcome!</h1>
+    <n-link to="/about">
+      About page
+    </n-link>
   </div>
 </template>
